731f1a5f8d38971f1cead34e0407f12e18951bf6
Terah <terah.dev@gmail.com>
2018-05-29 20:25:51 +0200

    Fix frontend compilation

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,9 +1,4 @@
-extern crate worldgen;
-extern crate worldsim;
-
-use worldgen::MacroWorld;
 
 fn main() {
-    let mut mw = MacroWorld::new(1337, 1024);
-    worldsim::simulate(&mut mw, 1);
+    println!("Frontend works !")
 }
