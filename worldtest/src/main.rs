adaac91ee4775f9fd1c7c0ea06de26562e3669c8
Joshua Barretto <joshua.s.barretto@gmail.com>
2018-05-29 02:16:16 +0100

    Fixed worldtest source

--- worldtest/src/main.rs
+++ worldtest/src/main.rs
@@ -1,9 +1,46 @@
+extern crate sfml;
 extern crate worldgen;
-extern crate worldsim;
+
+use sfml::system::Vector2f;
+use sfml::window::{ContextSettings, VideoMode, Event, Style};
+use sfml::graphics::{RectangleShape, Color, RenderTarget, RenderWindow, Shape, Transformable};
 
 use worldgen::MacroWorld;
 
+const WORLD_SEED: u32 = 1337;
+const WORLD_SIZE: u32 = 800;
+
 fn main() {
-    let mut mw = MacroWorld::new(1337, 1024);
-    worldsim::simulate(&mut mw, 1);
+    let mw = MacroWorld::new(WORLD_SEED, WORLD_SIZE);
+
+    let mut win = RenderWindow::new(
+        VideoMode::new(mw.size(), mw.size(), 32),
+        "Veloren World Test",
+        Style::CLOSE,
+        &ContextSettings::default()
+    );
+
+    loop {
+        loop {
+            match win.poll_event() {
+                Some(Event::Closed) => return,
+                None => break,
+                _ => {},
+            }
+        }
+
+        win.clear(&Color::rgb(0, 0, 0));
+
+        let mut pixel = RectangleShape::with_size(Vector2f::new(1., 1.));
+        for x in 0..mw.size() {
+            for y in 0..mw.size() {
+                let alt = mw.get(x, y).unwrap().altitude() as u8;
+                pixel.set_fill_color(&Color::rgb(alt, alt, alt));
+                pixel.set_position(Vector2f::new(x as f32, y as f32));
+                win.draw(&pixel);
+            }
+        }
+
+        win.display();
+    }
 }
