097215b39e9823696746cdc06d70859ff68eb6a1
Joshua Barretto <joshua.s.barretto@gmail.com>
2018-05-29 21:09:52 +0100

    Added Server struct, allows server-cli to create it

--- server-cli/src/main.rs
+++ server-cli/src/main.rs
@@ -1,5 +1,10 @@
 extern crate server;
 
+use server::Server;
+
 fn main() {
-    
+    println!("Started server-cli...");
+
+    let mut server = Server::new(1227, 1024);
+    while server.next_tick() {}
 }
--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,9 +1,25 @@
 extern crate worldgen;
 extern crate worldsim;
 
+use std::{thread, time};
 use worldgen::MacroWorld;
 
-fn server() {
-    let mut mw = MacroWorld::new(1337, 1024);
-    worldsim::simulate(&mut mw, 1);
+pub struct Server {
+    mw: MacroWorld,
+}
+
+impl Server {
+    pub fn new(seed: u32, world_size: u32) -> Server {
+        Server {
+            mw: MacroWorld::new(seed, world_size),
+        }
+    }
+
+    pub fn next_tick(&mut self) -> bool {
+        worldsim::simulate(&mut self.mw, 1);
+
+        thread::sleep(time::Duration::from_millis(20));
+
+        true
+    }
 }
