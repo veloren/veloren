237a458e9cc75aa55d2a62944056334925c95a2c
Joshua Barretto <joshua.s.barretto@gmail.com>
2018-05-29 23:37:10 +0100

    Added headless frontend, began implementing client crate

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,1 +1,30 @@
-// Nothing yet
+extern crate network;
+
+use network::ClientConn;
+
+#[derive(Debug)]
+pub enum Error {
+    ConnectionErr,
+}
+
+pub enum ClientMode {
+    Game,
+    Headless,
+}
+
+pub struct Client {
+    conn: ClientConn,
+}
+
+impl Client {
+    pub fn new(mode: ClientMode, bind_addr: &str, remote_addr: &str) -> Result<Client, Error> {
+        let conn = match ClientConn::new(bind_addr, remote_addr) {
+            Ok(conn) => conn,
+            Err(_) => return Err(Error::ConnectionErr),
+        };
+
+        Ok(Client {
+            conn,
+        })
+    }
+}
