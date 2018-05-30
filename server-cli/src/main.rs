0b2d5be449de051452861d2abc4da887186a3209
Joshua Barretto <joshua.s.barretto@gmail.com>
2018-05-30 21:22:19 +0100

    Fixed server ToSocketAddrs code

--- server-cli/src/main.rs
+++ server-cli/src/main.rs
@@ -6,6 +6,6 @@ use server::Server;
 fn main() {
     println!("Started server-cli...");
 
-    let mut server = Server::new("127.0.0.1:1337", 1227, 1024).expect("Could not create server");
+    let mut server = Server::new("127.0.0.1:59003", 1227, 1024).expect("Could not create server");
     while server.next_tick() {}
 }
