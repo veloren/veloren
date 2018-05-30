9538fdceee48982781269e5535be49fb4f895b81
Joshua Barretto <joshua.s.barretto@gmail.com>
2018-05-30 21:42:55 +0100

    Fixed workspace TOML and added automatic IP discovery

--- server-cli/src/main.rs
+++ server-cli/src/main.rs
@@ -1,11 +1,18 @@
+extern crate local_ip;
 extern crate server;
 
-use std::net::ToSocketAddrs;
+use std::net::SocketAddr;
 use server::Server;
+
+const PORT: u16 = 59003;
 
 fn main() {
     println!("Started server-cli...");
 
-    let mut server = Server::new("127.0.0.1:59003", 1227, 1024).expect("Could not create server");
+    let ip = local_ip::get().unwrap();
+
+    println!("Hosting on {}:{}...", ip.to_string(), PORT);
+
+    let mut server = Server::new(SocketAddr::new(ip, PORT), 1227, 1024).expect("Could not create server");
     while server.next_tick() {}
 }
