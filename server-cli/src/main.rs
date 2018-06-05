4471aa72590ce6544e53394088c69e9d1f96de5e
Joshua Barretto <joshua.s.barretto@gmail.com>
2018-06-06 00:35:05 +0100

    Added IP choice to server-cli

--- server-cli/src/main.rs
+++ server-cli/src/main.rs
@@ -5,6 +5,7 @@ extern crate pretty_env_logger;
 extern crate server;
 extern crate get_if_addrs;
 
+use std::io;
 use std::net::SocketAddr;
 use server::ServerHandle;
 
@@ -14,9 +15,24 @@ fn main() {
     pretty_env_logger::init();
     info!("Started server-cli...");
 
-    let ip = get_if_addrs::get_if_addrs().unwrap()[0].ip();
+    let ifaces = get_if_addrs::get_if_addrs().unwrap();
+    for (i, iface) in ifaces.iter().enumerate() {
+        println!("[{}] {}", i, iface.ip().to_string());
+    }
+    let ip = loop {
+        let mut ip_index = String::new();
+        println!("Enter the number of the IP address you wish to choose.");
+        io::stdin().read_line(&mut ip_index).unwrap();
 
-    info!("Hosting on {}:{}...", ip.to_string(), PORT);
+        if let Ok(index) = ip_index.trim().parse::<usize>() {
+            if let Some(iface) = ifaces.get(index) {
+                break iface.ip();
+            }
+        }
+        println!("Invalid number!");
+    };
+
+    println!("Hosting on {}:{}...", ip.to_string(), PORT);
 
     let mut server = ServerHandle::new(SocketAddr::new(ip, PORT), 1227, 1024)
         .expect("Could not create server");
