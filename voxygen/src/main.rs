f8786cbb68d549e1becaf74eccbbbf34a44bb59d
Kekker_ <calvinjwilliams97@gmail.com>
2018-06-06 10:33:32 -0400

    Properly redirect all clients from loopback to 0:0:0:0

--- voxygen/src/main.rs
+++ voxygen/src/main.rs
@@ -32,7 +32,23 @@ use game::Game;
 fn main() {
     println!("Starting Voxygen...");
 
-    let ip = get_if_addrs::get_if_addrs().unwrap()[0].ip();
+    let ip = std::net::IpAddr::V4(std::net::Ipv4Addr::new(0,0,0,0));
+    // let ifaces = get_if_addrs::get_if_addrs().unwrap();
+    // for (i, iface) in ifaces.iter().enumerate() {
+    //     println!("[{}] {}", i, iface.ip().to_string());
+    // }
+    // let ip = loop {
+    //     let mut ip_index = String::new();
+    //     println!("Enter the number of the IP address you wish to choose.");
+    //     io::stdin().read_line(&mut ip_index).unwrap();
+
+    //     if let Ok(index) = ip_index.trim().parse::<usize>() {
+    //         if let Some(iface) = ifaces.get(index) {
+    //             break iface.ip();
+    //         }
+    //     }
+    //     println!("Invalid number!");
+    // };
     let port: u16 = 59001;
 
     println!("Binding local port to {}:{}...", ip.to_string(), port);
