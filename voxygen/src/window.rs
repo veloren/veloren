4975d47ca9b036870e741167b358a0e5feae7a20
Tom Richardson <tgrkzus@gmail.com>
2018-07-05 04:42:42 +1000

    properly release/grab cursor on unfocus/focus window event

--- voxygen/src/window.rs
+++ voxygen/src/window.rs
@@ -128,6 +128,16 @@ impl RenderWindow {
                         }
                     },
                     WindowEvent::CloseRequested => func(Event::CloseRequest),
+
+                    WindowEvent::Focused(is_focused) => {
+                        if is_focused {
+                            // Window was changed to focused, ensure the cursor is trapped.
+                            self.cursor_trapped.store(true, Ordering::Relaxed);
+                        } else {
+                            // Window was changed to unfocused, ensure the cursor is not trapped
+                            self.cursor_trapped.store(false, Ordering::Relaxed);
+                        }
+                    }
                     _ => {},
                 },
                 _ => {},
