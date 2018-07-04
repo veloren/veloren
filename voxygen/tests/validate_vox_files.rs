0f29a8bf89dbf623eb2e6189b404314f0badd185
treenix <28811732+treenix@users.noreply.github.com>
2018-07-04 11:21:30 +0200

    Check if vox folder is empty
    If the vox folder is empty, the game is not really playable.

--- voxygen/tests/validate_vox_files.rs
+++ voxygen/tests/validate_vox_files.rs
@@ -4,10 +4,15 @@ use std::fs;
 #[test]
 fn validate_vox_files() {
     let paths = fs::read_dir("./vox").unwrap();
-    
+    let mut files_checked = 0;
+
     for path in paths {
         let path_string = path.unwrap().path().into_os_string().into_string().unwrap();
-        let vox = dot_vox::load(&path_string);
+        let vox = dot_vox::load(&path_string);        
         assert_eq!(true, vox.is_ok(), "Failed to validate file '{:?}'", path_string);
+
+        files_checked += 1;
     }
+
+    assert_ne!(0, files_checked, "No files found in voxygen/vox");
 }
