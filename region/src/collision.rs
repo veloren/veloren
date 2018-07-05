ed509e8d44909c67f75ddf7666aa05815c5d95f0
Marcel Märtens <marcel.cochem@googlemail.com>
2018-07-05 19:03:56 +0200

    remove old collision coding

--- region/src/collision.rs
+++ region/src/collision.rs
@@ -8,9 +8,9 @@ pub struct Cuboid {
 }
 
 pub struct CollisionResolution {
-    collision: Vec3<f64>,
-    a_correction: Vec3<f64>,
-    b_correction: Vec3<f64>,
+    pub collision: Vec3<f64>,
+    pub a_correction: Vec3<f64>,
+    pub b_correction: Vec3<f64>,
 }
 
 pub enum Collidable {
@@ -58,151 +58,9 @@ fn cuboid_cuboid_col(a: Cuboid, b: Cuboid) -> Option<CollisionResolution> {
             // we collide
             return Some(CollisionResolution{
                 collision: vec3!(0.0, 0.0, 0.0),
-                a_correction: vec3!(0.0, 0.0, 0.0001), // hack this stuff for now
-                b_correction: vec3!(0.0, 0.0, 0.0001),
+                a_correction: vec3!(0.0, 0.0, 0.001), // hack this stuff for now
+                b_correction: vec3!(0.0, 0.0, 0.001),
             });
         };
     None
-}
-
-/*
-
-Sphere collision is quite easy, we have a look at the middle and their distance and the added radius of those
-This way we can also test Sheres with an offset
-    _     _
-   / \   / \
-  | X \ | X \
-   \ /   \ /
-   ``    ``
-
-*/
-
-pub trait SphereColission {
-    fn middle(&self) -> Vec3<f64>;
-    fn radius(&self) -> f64;
-}
-
-struct Sphere {
-    middle: Vec3<f64>,
-    radius: f64,
-}
-
-impl SphereColission for Sphere {
-    fn middle(&self) -> Vec3<f64> {
-        return self.middle;
-    }
-    fn radius(&self) -> f64 {
-        return self.radius;
-    }
-}
-
-pub fn do_colide_sphere<V: SphereColission, W: SphereColission>(e1: V, e2: W) -> bool {
-    //fast check via sphere collision
-    let m1 = e1.middle();
-    let m2 = e2.middle();
-    let r1 = e1.radius();
-    let r2 = e2.radius();
-    let mdiff = m2 - m1;
-    let radd = r1  + r2;
-    // compare the squares instead of calulate the squareroot of mlen
-    let radd = radd * radd;
-    let mlen = mdiff.x*mdiff.x + mdiff.y*mdiff.y + mdiff.z*mdiff.z;
-    return radd >= mlen;
-}
-
-/*
-
- CuboidColission 2D representation of 3D cuboid
-
- +-----+ +-----+
- |     | |     |
- |  X  | |  X  |
- |     | |     |
- +-----+ +-----+
-
- +-----+-----+
- |     |     |
- |  X  |  X  |
- |     |     |
- +-----+-----+
-
-  /\ +-----+
- /  \|     |
-/    \     |
-\  X  \ X  |
- \   /     |
-  \ /|     |
-   - +-----+
-
-Cuboid Colission is quite difficult, because they have a rotation. Our approach will be:
-    - take all 8 edges of cuboid1
-    - calculate distance to cuboid2 middle
-    - check the thickness of cuboid2 towards this point( see below X marks the point, and ``_O marks the thickness)
-    - if thickness is bigger or equals to distance from middle2 to point we are good.
-
-          +-----X
-+------+   |     |
-|     _O   |  X  |
-|  X`` |   |     |
-|      |   +-----+
-+------+
-
-Flaws:
-    - actually there is a flaw if objects do not collide in their edges, but this is extremly uncommon, e.g:
-
-  +-+
-  | |
-+--------------------------------+
-| | |           X                |
-+--------------------------------+
-  | |
-  |X|
-  | |
-  | |
-  | |
-  +-+
-
-*/
-
-pub trait CuboidColission {
-    fn middle(&self) -> Vec3<f64>;
-    fn radius(&self) -> Vec3<f64>;
-    fn rotation(&self) -> Vec3<f64>;
-}
-
-impl<V: Volume> CuboidColission for V {
-    fn middle(&self) -> Vec3<f64> {
-        let o = self.offset();
-        let s = self.size();
-        let o = Vec3::new(o.x as f64, o.y as f64, o.z as f64);
-        let s = Vec3::new(s.x as f64, s.y as f64, s.z as f64);
-        let bl = o + s / 2.0;
-        return bl * self.scale();
-    }
-    // radius of a inner Sphere, or a/2, outer radius would be radius * SQRT(3)
-    fn radius(&self) -> Vec3<f64> {
-        let s = self.size();
-        let s = Vec3::new(s.x as f64 / 2.0, s.y as f64 / 2.0, s.z as f64 / 2.0);
-        return s * self.scale();
-    }
-    fn rotation(&self) -> Vec3<f64> {
-        return self.rotation();
-    }
-}
-pub fn might_colide<V: CuboidColission, W: CuboidColission>(e1: V, e2: W) -> bool {
-    //fast check via sphere collision
-    const SQRT3 : f64 = 1.73205080758;
-    let r1 = e1.radius();
-    let r2 = e2.radius();
-    let biggest_r1 = r1.x.max(r1.y.max(r1.z)) * SQRT3;
-    let biggest_r2 = r2.x.max(r2.y.max(r2.z)) * SQRT3;
-    let s1 = Sphere{middle: e1.middle(), radius: biggest_r1};
-    let s2 = Sphere{middle: e2.middle(), radius: biggest_r2};
-    return do_colide_sphere(s1,s2);
-}
-
-pub fn do_colide<V: CuboidColission, W: CuboidColission>(e1: V, e2: W) -> bool {
-    //do what i told above
-    // make it a simple AABB for now
-    panic!("s");
 }
--- region/src/tests.rs
+++ region/src/tests.rs
@@ -10,92 +10,24 @@ use coord::prelude::*;
 
 // Parent
 use super::{Volume, Voxel, Model};
-use super::collision::{might_colide, do_colide, CuboidColission};
+use super::collision::{resolve_collision, Collidable, Cuboid};
 
-fn newmodel(size: Vec3<i64>, offset: Vec3<i64>, rotation: Vec3<f64>) -> Model {
-    let mut model = Model::new();
-    model.set_size(size);
-    model.set_offset(offset);
-    model.set_rotation(rotation);
-    return model;
-}
-
-#[test]
-fn middle1() {
-    let m = newmodel(vec3!(1, 1, 1), vec3!(0, 0, 0), vec3!(0.0, 0.0, 0.0));
-    assert_eq!(m.middle(),vec3!(0.5, 0.5, 0.5));
-    let m = newmodel(vec3!(1, 1, 1), vec3!(2, 0, 0), vec3!(0.0, 0.0, 0.0));
-    assert_eq!(m.middle(),vec3!(2.5, 0.5, 0.5));
-    let m = newmodel(vec3!(1, 1, 1), vec3!(2, 0, 30), vec3!(0.0, 0.0, 0.0));
-    assert_eq!(m.middle(),vec3!(2.5, 0.5, 30.5));
-    let m = newmodel(vec3!(10, 10, 10), vec3!(2, 4, 0), vec3!(0.0, 0.0, 0.0));
-    assert_eq!(m.middle(),vec3!(7.0, 9.0, 5.0));
-    let m = newmodel(vec3!(10, 10, 10), vec3!(2, 4, 0), vec3!(2.0, 0.0, 0.0));
-    assert_eq!(m.middle(),vec3!(7.0, 9.0, 5.0));
-    let m = newmodel(vec3!(10, 10, 10), vec3!(2, 4, 0), vec3!(0.0, 1.0, 0.5));
-    assert_eq!(m.middle(),vec3!(7.0, 9.0, 5.0));
-    let m = newmodel(vec3!(10, 10, 10), vec3!(2, 4, 0), vec3!(0.2, 0.4, 0.1));
-    assert_eq!(m.middle(),vec3!(7.0, 9.0, 5.0));
-}
-
-#[test]
-fn radius1() {
-    let m = newmodel(vec3!(1, 1, 1), vec3!(0, 0, 0), vec3!(0.0, 0.0, 0.0));
-    assert_eq!(m.radius(),vec3!(0.5, 0.5, 0.5));
-    let m = newmodel(vec3!(1, 3, 1), vec3!(2, 0, 30), vec3!(0.0, 0.0, 0.0));
-    assert_eq!(m.radius(),vec3!(0.5, 1.5, 0.5));
-    let m = newmodel(vec3!(10, 10, 4), vec3!(2, 4, 0), vec3!(0.0, 0.0, 0.0));
-    assert_eq!(m.radius(),vec3!(5.0, 5.0, 2.0));
-    let m = newmodel(vec3!(5, 10, 10), vec3!(2, 4, 0), vec3!(0.2, 0.4, 0.1));
-    assert_eq!(m.radius(),vec3!(2.5, 5.0, 5.0));
-}
-
-#[test]
-fn rotation1() {
-    let m = newmodel(vec3!(1, 1, 1), vec3!(0, 0, 0), vec3!(0.0, 0.0, 0.0));
-    assert_eq!(<Model as CuboidColission>::rotation(&m),vec3!(0.0, 0.0, 0.0));
-    let m = newmodel(vec3!(5, 10, 10), vec3!(2, 4, 0), vec3!(0.2, 0.4, 0.1));
-    assert_eq!(<Model as CuboidColission>::rotation(&m),vec3!(0.2, 0.4, 0.1));
+fn newmodel(middle: Vec3<f64>, size: Vec3<f64>) -> Collidable {
+    let col = Collidable::Cuboid{ cuboid: Cuboid::new(middle, size) };
+    return col;
 }
 
 #[test]
 fn might_colide_simple() {
-    let m1 = newmodel(vec3!(1, 1, 1), vec3!(0, 0, 0), vec3!(0.0, 0.0, 0.0));
-    let m2 = newmodel(vec3!(1, 1, 1), vec3!(0, 0, 0), vec3!(0.0, 0.0, 0.0));
-    assert!(might_colide(m1, m2));
-    let m1 = newmodel(vec3!(10, 10, 10), vec3!(0, 0, 0), vec3!(0.0, 0.0, 0.0));
-    let m2 = newmodel(vec3!(10, 10, 10), vec3!(10, 0, 0), vec3!(0.0, 0.0, 0.0));
-    assert!(might_colide(m1, m2)); //should exactly colide
-    let m1 = newmodel(vec3!(10, 10, 10), vec3!(0, 0, 0), vec3!(0.0, 0.0, 0.0));
-    let m2 = newmodel(vec3!(10, 10, 10), vec3!(11, 0, 0), vec3!(0.0, 0.0, 0.0));
-    assert!(might_colide(m1, m2)); //colide because could be rotated
-    let m1 = newmodel(vec3!(10, 10, 10), vec3!(0, 0, 0), vec3!(0.0, 0.0, 0.0));
-    let m2 = newmodel(vec3!(10, 10, 10), vec3!(18, 0, 0), vec3!(0.0, 0.0, 0.0));
-    assert!(!might_colide(m1, m2)); // no longer collide
-    let m1 = newmodel(vec3!(1, 1, 1), vec3!(0, 0, 0), vec3!(0.0, 0.0, 0.0));
-    let m2 = newmodel(vec3!(1, 1, 1), vec3!(2, 0, 0), vec3!(0.0, 0.0, 0.0));
-    assert!(!might_colide(m1, m2));
-}
+    let m1 = newmodel(vec3!(0.5, 0.5, 0.5), vec3!(1.0, 1.0, 1.0));
+    let m2 = newmodel(vec3!(1.5, 0.5, 0.5), vec3!(1.0, 1.0, 1.0));
+    let res = resolve_collision(m1, m2).unwrap();
+    assert_eq!(res.collision, vec3!(0.0, 0.0, 0.0));
+    assert_eq!(res.a_correction, vec3!(0.0, 0.0, 0.001));
+    assert_eq!(res.b_correction, vec3!(0.0, 0.0, 0.001));
 
-#[test]
-fn might_colide_maxradius() {
-    let m1 = newmodel(vec3!(1, 1, 1), vec3!(0, 0, 0), vec3!(0.0, 0.0, 0.0));
-    let m2 = newmodel(vec3!(1, 1, 1), vec3!(1, 0, 0), vec3!(0.0, 0.0, 0.0));
-    assert!(might_colide(m1, m2));
-    let m1 = newmodel(vec3!(1, 2, 1), vec3!(0, 0, 0), vec3!(0.0, 0.0, 0.0));
-    let m2 = newmodel(vec3!(1, 1, 1), vec3!(1, 0, 0), vec3!(0.0, 0.0, 0.0));
-    assert!(might_colide(m1, m2));
-}
-
-#[test]
-fn might_colide3() {
-    let m1 = newmodel(vec3!(1, 1, 1), vec3!(0, 0, 0), vec3!(0.0, 0.0, 0.0));
-    let m2 = newmodel(vec3!(1, 1, 1), vec3!(0, 0, 0), vec3!(0.0, 0.0, 0.0));
-    assert!(might_colide(m1, m2));
-    let m1 = newmodel(vec3!(1, 1, 1), vec3!(0, 0, 0), vec3!(0.0, 0.0, 0.0));
-    let m2 = newmodel(vec3!(2, 2, 2), vec3!(1, 1, 0), vec3!(0.0, 0.0, 0.0));
-    assert!(might_colide(m1, m2));
-    let m1 = newmodel(vec3!(1, 1, 1), vec3!(0, 0, 0), vec3!(0.0, 0.0, 0.0));
-    let m2 = newmodel(vec3!(2, 2, 2), vec3!(2, 2, 0), vec3!(0.0, 0.0, 0.0));
-    assert!(!might_colide(m1, m2));
+    let m1 = newmodel(vec3!(0.5, 0.5, 0.5), vec3!(1.0, 1.0, 1.0));
+    let m2 = newmodel(vec3!(2.5, 0.5, 0.5), vec3!(1.0, 1.0, 1.0));
+    let res = resolve_collision(m1, m2);
+    assert!(res.is_none());
 }
