628a91af00967891f3bd8e8e932ddf32025c2fa3
Imberflur <hank.aa7@gmail.com>
2019-02-11 23:14:55 -0500

    move conrod ui rendering and setup into ui::Ui
    Former-commit-id: 1dc43fb7d73671d0b798238fecfcbf8d07ffbfcb

--- voxygen/src/menu/title.rs
+++ voxygen/src/menu/title.rs
@@ -1,12 +1,8 @@
 // Library
 use vek::*;
 use image;
-use conrod_core::Ui;
-use conrod_core::UiBuilder;
 use conrod_core::widget::image::Image as ImageWidget;
-use conrod_core::image::Map as ImageMap;
-use conrod_core::{Positionable, Sizeable};
-use conrod_core::Widget;
+use conrod_core::{Positionable, Sizeable, Widget};
 
 // Crate
 use crate::{
@@ -19,33 +15,31 @@ use crate::{
         Consts,
         UiLocals,
         Renderer,
-        Texture,
-        UiPipeline,
-        create_ui_quad_mesh,
+    },
+    ui::{
+        Ui
     },
 };
 
 pub struct TitleState {
-    ui: Ui,
-    image_map: ImageMap<Texture<UiPipeline>>,
+    ui: Ui
 }
 
 impl TitleState {
     /// Create a new `TitleState`
     pub fn new(renderer: &mut Renderer) -> Self {
-        let mut ui = UiBuilder::new([500.0, 500.0]).build();
-        let widget_id = ui.widget_id_generator().next();
-        let mut image_map = ImageMap::new();
-        let img = image::open(concat!(env!("CARGO_MANIFEST_DIR"), "/test_assets/test.png")).unwrap();
-        let img = renderer.create_texture(&img).unwrap();
-        let img_id = image_map.insert(img);
-        ImageWidget::new(img_id)
-            .x_y(0.0, 0.0)
-            .w_h(500.0,500.0)
-            .set(widget_id, &mut ui.set_widgets());
+        let mut ui = Ui::new(renderer, [500.0, 500.0]).unwrap();
+        let widget_id = ui.new_widget();
+        let image = image::open(concat!(env!("CARGO_MANIFEST_DIR"), "/test_assets/test.png")).unwrap();
+        let title_img = ui.new_image(renderer, &image).unwrap();
+        ui.set_widgets(|ui_cell| {
+            ImageWidget::new(title_img)
+                .x_y(0.0, 0.0)
+                .w_h(500.0,500.0)
+                .set(widget_id, ui_cell);
+        });
         Self {
-            ui,
-            image_map
+            ui
         }
     }
 }
@@ -70,43 +64,19 @@ impl PlayState for TitleState {
             }
 
 
+            global_state.window.renderer_mut().clear(BG_COLOR);
+
             // Maintain the UI
             //self.ui.maintain(global_state.window.renderer_mut());
 
             // Draw the UI to the screen
-            //self.ui.render(global_state.window.renderer_mut());
-            if let Some(mut primitives) = Some(self.ui.draw()){//_if_changed() {
-                // Clear the screen
-                global_state.window.renderer_mut().clear(BG_COLOR);
+            self.ui.render(global_state.window.renderer_mut());
 
-                //render the primatives one at a time
-                while let Some(prim) = primitives.next() {
-                    let mut renderer = global_state.window.renderer_mut();
-                    use conrod_core::render::{Primitive, PrimitiveKind};
-                    let Primitive {kind, scizzor, rect, ..} = prim;
-                    match kind {
-                        PrimitiveKind::Image { image_id, color, source_rect } => {
-
-                            let mut locals = renderer.create_consts(&[UiLocals::default()]).unwrap();
-                            renderer.update_consts(&mut locals, &[UiLocals::new(
-                                    [0.0, 0.0, 1.0, 1.0],
-                                )]);
-                            let model = renderer.create_model(&create_ui_quad_mesh()).unwrap();
-                            global_state.window.renderer_mut().render_ui_element(&model, &locals, self.image_map.get(&image_id).unwrap())
-                        }
-                        _ => {println!("should not reach here");}
-                    }
-                }
-
-
-                // Finish the frame
-                global_state.window.renderer_mut().flush();
-                global_state.window
-                    .swap_buffers()
-                    .expect("Failed to swap window buffers");
-            }
-
-
+            // Finish the frame
+            global_state.window.renderer_mut().flush();
+            global_state.window
+            .swap_buffers()
+            .expect("Failed to swap window buffers");
 
         }
     }
