ac94020b48cf418a432443252ac341a8037adce8
Joshua Barretto <joshua.s.barretto@gmail.com>
2019-02-25 16:45:54 +0000

    Netcode usability improvements
    Former-commit-id: a0e0d0b6fd503b4de9c60d2fcb02562def98c75e

--- common/src/net/test.rs
+++ common/src/net/test.rs
@@ -1,69 +1,78 @@
-use std::io::Write;
-use std::net::SocketAddr;
+use std::{
+    io::Write,
+    str::FromStr,
+    net::SocketAddr,
+    thread,
+    time::Duration,
+};
 
 use mio::{net::TcpStream, Events, Poll, PollOpt, Ready, Token};
 
 use super::{error::PostError, PostBox, PostOffice};
 
+fn new_local_addr(n: u16) -> SocketAddr {
+    SocketAddr::from(([127, 0, 0, 1], 12345 + n))
+}
+
 #[test]
 fn basic_run() {
-    let listen_addr = SocketAddr::from(([0, 0, 0, 0], 12345u16));
-    let conn_addr = SocketAddr::from(([127, 0, 0, 1], 12345u16));
-    let server: PostOffice<String, String> = PostOffice::new(&listen_addr).unwrap();
-    let client: PostBox<String, String> = PostBox::to_server(&conn_addr).unwrap();
+    let srv_addr = new_local_addr(0);
+    let mut server: PostOffice<String, String> = PostOffice::new(srv_addr).unwrap();
+    let mut client: PostBox<String, String> = PostBox::to_server(srv_addr).unwrap();
     std::thread::sleep(std::time::Duration::from_millis(10));
-    let scon = server.get_iter().unwrap().next().unwrap().unwrap();
+    let mut scon = server.new_connections().next().unwrap();
     std::thread::sleep(std::time::Duration::from_millis(10));
-    scon.send(String::from("foo"));
-    client.send(String::from("bar"));
+    scon.send(String::from("foo")).unwrap();
+    client.send(String::from("bar")).unwrap();
     std::thread::sleep(std::time::Duration::from_millis(10));
-    assert_eq!("foo", client.recv_iter().unwrap().next().unwrap().unwrap());
-    assert_eq!("bar", scon.recv_iter().unwrap().next().unwrap().unwrap());
+    assert_eq!("foo", client.recv_iter().next().unwrap());
+    assert_eq!("bar", scon.recv_iter().next().unwrap());
 }
 
 #[test]
 fn huge_size_header() {
-    let listen_addr = SocketAddr::from(([0, 0, 0, 0], 12346u16));
-    let conn_addr = SocketAddr::from(([127, 0, 0, 1], 12346u16));
-    let server: PostOffice<String, String> = PostOffice::new(&listen_addr).unwrap();
-    let mut client = TcpStream::connect(&conn_addr).unwrap();
+    let srv_addr = new_local_addr(1);
+
+    let mut server: PostOffice<String, String> = PostOffice::new(srv_addr).unwrap();
+    let mut client = TcpStream::connect(&srv_addr).unwrap();
     std::thread::sleep(std::time::Duration::from_millis(10));
-    let scon = server.get_iter().unwrap().next().unwrap().unwrap();
+    let mut scon = server.new_connections().next().unwrap();
     std::thread::sleep(std::time::Duration::from_millis(10));
     client.write(&[0xffu8; 64]).unwrap();
     std::thread::sleep(std::time::Duration::from_millis(10));
-    assert!(match scon.recv_iter().unwrap().next().unwrap() {
-        Err(PostError::MsgSizeLimitExceeded) => true,
-        _ => false,
-    });
+    assert_eq!(scon.recv_iter().next(), None);
 }
 
 #[test]
 fn disconnect() {
-    let listen_addr = SocketAddr::from(([0, 0, 0, 0], 12347u16));
-    let conn_addr = SocketAddr::from(([127, 0, 0, 1], 12347u16));
-    let server: PostOffice<String, String> = PostOffice::new(&listen_addr).unwrap();
+    let srv_addr = new_local_addr(2);
+
+    let mut server = PostOffice::<_, String>::new(srv_addr)
+        .unwrap();
+
+    // Create then close client
     {
-        #[allow(unused_variables)]
-        let client: PostBox<String, String> = PostBox::to_server(&conn_addr).unwrap();
+        PostBox::<String, String>::to_server(srv_addr).unwrap();
     }
-    std::thread::sleep(std::time::Duration::from_millis(10));
-    let scon = server.get_iter().unwrap().next().unwrap().unwrap();
-    scon.send(String::from("foo"));
+
     std::thread::sleep(std::time::Duration::from_millis(10));
 
-    match scon.recv_iter().unwrap().next().unwrap() {
-        Ok(_) => panic!("Didn't expect to receive anything"),
-        Err(err) => {
-            if !(match err {
-                PostError::Io(e) => e,
-                _ => panic!("PostError different than expected"),
-            }
-            .kind()
-                == std::io::ErrorKind::BrokenPipe)
-            {
-                panic!("Error different than disconnection")
-            }
-        }
+    let mut to_client = server
+        .new_connections()
+        .next()
+        .unwrap();
+
+    to_client.send(String::from("foo")).unwrap();
+
+    thread::sleep(Duration::from_millis(10));
+
+    match to_client.recv_iter().next() {
+        None => {},
+        _ => panic!("Unexpected message!"),
+    }
+
+    match to_client.status() {
+        Some(PostError::Disconnected) => {},
+        s => panic!("Did not expect {:?}", s),
     }
 }
