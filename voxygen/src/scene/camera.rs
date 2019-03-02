33b8922aa435aa20d228a986af013e62595afa49
Imbris <hank.aa7@gmail.com>
2019-03-02 14:43:51 -0500

    move/name velocity constant, rename move_vec, simplify cam rotate_by()
    Former-commit-id: 0835a9e91bd0133922f5d4e9731b9f6b6b547a0b

--- voxygen/src/scene/camera.rs
+++ voxygen/src/scene/camera.rs
@@ -53,13 +53,14 @@ impl Camera {
 
     /// Rotate the camera about its focus by the given delta, limiting the input accordingly.
     pub fn rotate_by(&mut self, delta: Vec3<f32>) {
-        self.ori += delta;
-        // Wrap camera roll
-        self.ori.x = self.ori.x % (2.0 * PI);
+        // Wrap camera yaw
+        self.ori.x = (self.ori.x + delta.x) % (2.0 * PI);
         // Clamp camera pitch to the vertical limits
-        self.ori.y = self.ori.y
+        self.ori.y = (self.ori.y + delta.y)
             .min(PI / 2.0)
             .max(-PI / 2.0);
+        // Wrap camera roll
+        self.ori.z = (self.ori.z + delta.z) % (2.0 * PI);
     }
 
     /// Zoom the camera by the given delta, limiting the input accordingly.
