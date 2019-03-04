b0aae7a8e6dd075996f3bee6cc7be29763f6cdaa
Joshua Barretto <joshua.s.barretto@gmail.com>
2019-03-04 00:06:01 +0000

    Added Uid marker component

--- common/src/comp/mod.rs
+++ common/src/comp/mod.rs
@@ -1,9 +1,12 @@
 pub mod phys;
+pub mod uid;
 
 // External
 use specs::World as EcsWorld;
 
 pub fn register_local_components(ecs_world: &mut EcsWorld) {
+    ecs_world.register::<uid::Uid>();
+
     ecs_world.register::<phys::Pos>();
     ecs_world.register::<phys::Vel>();
     ecs_world.register::<phys::Dir>();
