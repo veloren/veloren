2d6a2b42b45b6d1a6ffbc27f64c38b7e574746cf
timokoesters <timo@koesters.xyz>
2019-04-16 20:15:49 +0200

    Implement animation smoothing

--- voxygen/src/anim/character/idle.rs
+++ voxygen/src/anim/character/idle.rs
@@ -18,31 +18,34 @@ impl Animation for IdleAnimation {
     type Dependency = f64;
 
     fn update_skeleton(
-        skeleton: &mut Self::Skeleton,
+        skeleton: &Self::Skeleton,
         time: f64,
-    ) {
-        skeleton.head.offset = Vec3::unit_z() * 13.0 / 11.0;
-        skeleton.head.ori = Quaternion::rotation_z(0.0);
+    ) -> Self::Skeleton {
+        let mut next = (*skeleton).clone();
+        next.head.offset = Vec3::unit_z() * 13.0 / 11.0;
+        next.head.ori = Quaternion::rotation_z(0.0);
 
-        skeleton.chest.offset = Vec3::unit_z() * 9.0 / 11.0;
-        skeleton.chest.ori = Quaternion::rotation_z(0.0);
+        next.chest.offset = Vec3::unit_z() * 9.0 / 11.0;
+        next.chest.ori = Quaternion::rotation_z(0.0);
 
-        skeleton.belt.offset = Vec3::unit_z() * 7.0 / 11.0;
-        skeleton.belt.ori = Quaternion::rotation_z(0.0);
+        next.belt.offset = Vec3::unit_z() * 7.0 / 11.0;
+        next.belt.ori = Quaternion::rotation_z(0.0);
 
-        skeleton.shorts.offset = Vec3::unit_z() * 4.0 / 11.0;
-        skeleton.shorts.ori = Quaternion::rotation_z(0.0);
+        next.shorts.offset = Vec3::unit_z() * 4.0 / 11.0;
+        next.shorts.ori = Quaternion::rotation_z(0.0);
 
-        skeleton.l_hand.offset = Vec3::new(-8.0, 0.0, 9.0) / 11.0;
-        skeleton.r_hand.offset = Vec3::new(8.0, 0.0, 9.0 ) / 11.0;
+        next.l_hand.offset = Vec3::new(-8.0, 0.0, 9.0) / 11.0;
+        next.r_hand.offset = Vec3::new(8.0, 0.0, 9.0 ) / 11.0;
 
-        skeleton.l_foot.offset = Vec3::new(-3.5, 0.0, 3.0) / 11.0;
-        skeleton.l_foot.ori = Quaternion::rotation_x(0.0);
-        skeleton.r_foot.offset = Vec3::new(3.5, 0.0, 3.0) / 11.0;
-        skeleton.r_foot.ori = Quaternion::rotation_x(0.0);
+        next.l_foot.offset = Vec3::new(-3.5, 0.0, 3.0) / 11.0;
+        next.l_foot.ori = Quaternion::rotation_x(0.0);
+        next.r_foot.offset = Vec3::new(3.5, 0.0, 3.0) / 11.0;
+        next.r_foot.ori = Quaternion::rotation_x(0.0);
 
-        skeleton.back.offset = Vec3::new(-9.0, 5.0, 18.0);
-        skeleton.back.ori = Quaternion::rotation_y(2.5);
-        skeleton.back.scale = Vec3::one();
+        next.back.offset = Vec3::new(-9.0, 5.0, 18.0);
+        next.back.ori = Quaternion::rotation_y(2.5);
+        next.back.scale = Vec3::one();
+
+        next
     }
 }
