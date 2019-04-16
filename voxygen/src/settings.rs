5acd975f69f6c6d1c69315b0e56a4b9186bf95bc
Louis Pearson <desttinghimgame@gmail.com>
2019-04-16 02:56:47 -0600

    Add default keybindings
    Former-commit-id: c8886a2eb3326ba62e31ef84b93a29ee59f378b3

--- voxygen/src/settings.rs
+++ voxygen/src/settings.rs
@@ -6,11 +6,13 @@ use serde_derive::{Serialize, Deserialize};
 
 use glutin::VirtualKeyCode;
 
+/// Settings contains everything that can be configured in the Settings.toml file
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Settings {
     pub controls: ControlSettings,
 }
 
+/// ControlSettings contains keybindings
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct ControlSettings {
     pub toggle_cursor: VirtualKeyCode,
@@ -32,6 +34,28 @@ pub struct ControlSettings {
 }
 
 impl Settings {
+    pub fn default() -> Self {
+        Settings {
+            controls: ControlSettings {
+                toggle_cursor: VirtualKeyCode::Tab,
+                escape: VirtualKeyCode::Escape,
+                enter: VirtualKeyCode::Return,
+                move_forward: VirtualKeyCode::W,
+                move_left: VirtualKeyCode::A,
+                move_back: VirtualKeyCode::S,
+                move_right: VirtualKeyCode::D,
+                map: VirtualKeyCode::M,
+                bag: VirtualKeyCode::B,
+                quest_log: VirtualKeyCode::L,
+                character_window: VirtualKeyCode::C,
+                social: VirtualKeyCode::O,
+                spellbook: VirtualKeyCode::P,
+                settings: VirtualKeyCode::N,
+                help: VirtualKeyCode::F2,
+                interface: VirtualKeyCode::F2,
+            },
+        }
+    }
     pub fn load() -> Result<Self, ConfigError> {
         let mut config = Config::new();
         config.merge(config::File::with_name("Settings"))?;
