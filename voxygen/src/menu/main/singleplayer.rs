7ec6e093325a501330049a1f6fc01911acd54978
Louis Pearson <desttinghimgame@gmail.com>
2019-04-17 07:41:34 -0600

    Add basic singleplayer support
    Former-commit-id: 929b05faca5d2b95a96349dfc17a62b3165289a1

---
+++ voxygen/src/menu/main/singleplayer.rs
@@ -0,0 +1,36 @@
+use std::time::Duration;
+use log::info;
+use server::{Input, Event, Server};
+use common::clock::Clock;
+
+const TPS: u64 = 30;
+
+pub fn run_server() {
+    info!("Starting server-cli...");
+
+    // Set up an fps clock
+    let mut clock = Clock::new();
+
+    // Create server
+    let mut server = Server::new()
+        .expect("Failed to create server instance");
+
+    loop {
+        let events = server.tick(Input::default(), clock.get_last_delta())
+            .expect("Failed to tick server");
+
+        for event in events {
+            match event {
+                Event::ClientConnected { entity } => info!("Client connected!"),
+                Event::ClientDisconnected { entity } => info!("Client disconnected!"),
+                Event::Chat { entity, msg } => info!("[Client] {}", msg),
+            }
+        }
+
+        // Clean up the server after a tick
+        server.cleanup();
+
+        // Wait for the next tick
+        clock.tick(Duration::from_millis(1000 / TPS));
+    }
+}
