76adbec3bbce74fbf9e40ffd69fff4250dcf853e
Louis Pearson <desttinghimgame@gmail.com>
2019-04-17 08:57:23 -0600

    Add method to drop singleplayer thread
    Former-commit-id: b62a90728d4e83a005687538e134cee427491052

--- voxygen/src/singleplayer.rs
+++ voxygen/src/singleplayer.rs
@@ -2,10 +2,45 @@ use std::time::Duration;
 use log::info;
 use server::{Input, Event, Server};
 use common::clock::Clock;
+use std::{
+    thread,
+    thread::JoinHandle
+};
+use std::sync::mpsc::{
+    channel, Receiver, Sender
+};
 
 const TPS: u64 = 30;
 
-pub fn run_server() {
+enum Msg {
+    Stop,
+}
+
+pub struct Singleplayer {
+    server_thread: JoinHandle<()>,
+    sender: Sender<Msg>,
+}
+
+impl Singleplayer {
+    pub fn new() -> Self {
+        let (sender, reciever) = channel();
+        let thread = thread::spawn(move || {
+            run_server(reciever);
+        });
+        Singleplayer {
+            server_thread: thread,
+            sender,
+        }
+    }
+}
+
+impl Drop for Singleplayer {
+    fn drop(&mut self) {
+        self.sender.send(Msg::Stop);
+    }
+}
+
+fn run_server(rec: Receiver<Msg>) {
     info!("Starting server-cli...");
 
     // Set up an fps clock
@@ -29,6 +64,14 @@ pub fn run_server() {
 
         // Clean up the server after a tick
         server.cleanup();
+
+        match rec.try_recv() {
+            Ok(msg) => break,
+            Err(err) => match err {
+                Empty => (),
+                Disconnected => break,
+            },
+        }
 
         // Wait for the next tick
         clock.tick(Duration::from_millis(1000 / TPS));
