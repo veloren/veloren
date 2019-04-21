bb150e349b6bcab694957f7add8dc7cd069aafef
jshipsey <jshipsey18@gmail.com>
2019-04-21 13:35:43 -0400

    velocity tilt
    Former-commit-id: 7373827734c9a65813537d7a1cb49b400c7d6fb8

--- voxygen/src/anim/character/idle.rs
+++ voxygen/src/anim/character/idle.rs
@@ -32,31 +32,40 @@ impl Animation for IdleAnimation {
         let waveultracos_slow = (time as f32 * 1.0 + PI).cos();
         let wave_dip = (wave_slow.abs() - 0.5).abs();
 
-        next.head.offset = Vec3::new(0.0, 0.0, 12.0 + waveultra_slow * 0.4) / SCALE;
+        next.head.offset = Vec3::new(-1.0, 0.0, 12.0 + waveultra_slow * 0.4) / SCALE;
         next.head.ori = Quaternion::rotation_y(waveultra_slow * 0.05);
+        next.head.scale = Vec3::one() / SCALE;
 
-        next.chest.offset = Vec3::new(0.0, 0.0, 8.0 + waveultra_slow * 0.4) / SCALE;
+        next.chest.offset = Vec3::new(2.5, 0.0, 8.0 + waveultra_slow * 0.4) / SCALE;
         next.chest.ori = Quaternion::rotation_y(0.0);
+        next.chest.scale = Vec3::one() / SCALE;
 
-        next.belt.offset = Vec3::new(0.0, 0.0, 6.0 + waveultra_slow * 0.4) / SCALE;
+        next.belt.offset = Vec3::new(2.5, 0.0, 6.0 + waveultra_slow * 0.4) / SCALE;
         next.belt.ori = Quaternion::rotation_y(0.0);
+        next.belt.scale = Vec3::one() / SCALE;
 
-        next.shorts.offset = Vec3::new(0.0, 0.0, 3.0 + waveultra_slow * 0.4) / SCALE;
+        next.shorts.offset = Vec3::new(2.5, 0.0, 3.0 + waveultra_slow * 0.4) / SCALE;
         next.shorts.ori = Quaternion::rotation_y(0.0);
+        next.shorts.scale = Vec3::one() / SCALE;
 
-        next.l_hand.offset = Vec3::new(0.0 + waveultracos_slow * 0.3, 7.5, 11.0 + waveultra_slow * 1.1) / SCALE;
+        next.l_hand.offset = Vec3::new(2.0 + waveultracos_slow * 0.3, 7.5, 13.5 + waveultra_slow * 1.1) / SCALE;
         next.l_hand.ori = Quaternion::rotation_y(0.0 + waveultra_slow * 0.06);
-        next.r_hand.offset = Vec3::new(0.0 + waveultracos_slow * 0.3 , - 7.5, 11.0 + waveultra_slow * 1.1) / SCALE;
+        next.r_hand.offset = Vec3::new(2.0 + waveultracos_slow * 0.3 , - 7.5, 13.5 + waveultra_slow * 1.1) / SCALE;
         next.r_hand.ori = Quaternion::rotation_y(0.0 + waveultra_slow * 0.06);
 
-        next.l_foot.offset = Vec3::new(2.5, 3.4, 6.0) / SCALE;
+        next.l_foot.offset = Vec3::new(5.0, 3.4, 8.0) / SCALE;
         next.l_foot.ori = Quaternion::rotation_y(0.04 + waveultra_slow * 0.04);
-        next.r_foot.offset = Vec3::new(2.5, -3.4, 6.0) / SCALE;
+        next.r_foot.offset = Vec3::new(5.0, -3.4, 8.0) / SCALE;
         next.r_foot.ori = Quaternion::rotation_y(0.04 + waveultra_slow * 0.04);
 
-        next.back.offset = Vec3::new(-6.5, 16.0, 15.0);
+        next.back.offset = Vec3::new(-4.5, 12.0, 11.0);
         next.back.ori = Quaternion::rotation_x(2.5);
         next.back.scale = Vec3::one();
+
+
+        next.torso.offset = Vec3::new(0.0, 0.0, 0.0);
+        next.torso.ori = Quaternion::rotation_y(0.0);
+        next.torso.scale = Vec3::one();
 
         next
     }
