df7a145bbc372a0b6515a62fd3340685fe9c199b
timokoesters <timo@koesters.xyz>
2019-04-23 11:53:45 +0200

    Better formatting
    Former-commit-id: 4316514f4f58dca8ed21dae00fad7a6df36b9ff1

--- server/src/client.rs
+++ server/src/client.rs
@@ -1,11 +1,11 @@
-use std::collections::HashMap;
-use specs::Entity as EcsEntity;
+use crate::Error;
 use common::{
     comp,
-    msg::{ServerMsg, ClientMsg, ClientState, RequestStateError},
+    msg::{ClientMsg, ClientState, RequestStateError, ServerMsg},
     net::PostBox,
 };
-use crate::Error;
+use specs::Entity as EcsEntity;
+use std::collections::HashMap;
 
 pub struct Client {
     pub client_state: ClientState,
@@ -19,17 +19,16 @@ impl Client {
     }
     pub fn allow_state(&mut self, new_state: ClientState) {
         self.client_state = new_state;
-        self.postbox.send_message(ServerMsg::StateAnswer(
-                Ok(new_state)));
+        self.postbox
+            .send_message(ServerMsg::StateAnswer(Ok(new_state)));
     }
     pub fn error_state(&mut self, error: RequestStateError) {
-        self.postbox.send_message(ServerMsg::StateAnswer(
-                Err((error, self.client_state))));
+        self.postbox
+            .send_message(ServerMsg::StateAnswer(Err((error, self.client_state))));
     }
     pub fn force_state(&mut self, new_state: ClientState) {
         self.client_state = new_state;
-        self.postbox.send_message(ServerMsg::ForceState(
-                new_state));
+        self.postbox.send_message(ServerMsg::ForceState(new_state));
     }
 }
 
@@ -68,7 +67,9 @@ impl Clients {
 
     pub fn notify_ingame(&mut self, msg: ServerMsg) {
         for client in self.clients.values_mut() {
-            if client.client_state == ClientState::Spectator || client.client_state == ClientState::Character {
+            if client.client_state == ClientState::Spectator
+                || client.client_state == ClientState::Character
+            {
                 client.notify(msg.clone());
             }
         }
@@ -84,8 +85,10 @@ impl Clients {
 
     pub fn notify_ingame_except(&mut self, except_entity: EcsEntity, msg: ServerMsg) {
         for (entity, client) in self.clients.iter_mut() {
-            if (client.client_state == ClientState::Spectator || client.client_state == ClientState::Character)
-                && *entity != except_entity {
+            if (client.client_state == ClientState::Spectator
+                || client.client_state == ClientState::Character)
+                && *entity != except_entity
+            {
                 client.notify(msg.clone());
             }
         }
