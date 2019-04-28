9264a07271a63677f5784499f1fc890ebb52aa41
robojumper <robojumper@gmail.com>
2019-04-28 19:12:45 +0200

    Generalize meshing and AO code over terrain, figure

--- voxygen/src/mesh/segment.rs
+++ voxygen/src/mesh/segment.rs
@@ -3,43 +3,20 @@ use vek::*;
 
 // Project
 use common::{
-    vol::{
-        Vox,
-        SizedVol,
-        ReadVol,
-    },
     figure::Segment,
+    vol::{ReadVol, SizedVol, Vox},
 };
 
 // Crate
 use crate::{
-    mesh::Meshable,
-    render::{
-        self,
-        Mesh,
-        Quad,
-        FigurePipeline,
-    },
+    mesh::{ao, Meshable},
+    render::{self, FigurePipeline, Mesh, Quad},
 };
 
 type FigureVertex = <FigurePipeline as render::Pipeline>::Vertex;
 
-// Utility function
-// TODO: Evaluate how useful this is
-fn create_quad(
-    origin: Vec3<f32>,
-    unit_x: Vec3<f32>,
-    unit_y: Vec3<f32>,
-    norm: Vec3<f32>,
-    col: Rgb<f32>,
-    bone: u8,
-) -> Quad<FigurePipeline> {
-    Quad::new(
-        FigureVertex::new(origin, norm, col, bone),
-        FigureVertex::new(origin + unit_x, norm, col, bone),
-        FigureVertex::new(origin + unit_x + unit_y, norm, col, bone),
-        FigureVertex::new(origin + unit_y, norm, col, bone),
-    )
+fn create_vertex(origin: Vec3<f32>, norm: Vec3<f32>, col: Rgb<f32>) -> FigureVertex {
+    FigureVertex::new(origin, norm, col, 0)
 }
 
 impl Meshable for Segment {
@@ -50,97 +27,17 @@ impl Meshable for Segment {
         let mut mesh = Mesh::new();
 
         for pos in self.iter_positions() {
-            if let Some(col) = self
-                .get(pos)
-                .ok()
-                .and_then(|vox| vox.get_color())
-            {
+            if let Some(col) = self.get(pos).ok().and_then(|vox| vox.get_color()) {
                 let col = col.map(|e| e as f32 / 255.0);
 
-                // -x
-                if self.get(pos - Vec3::unit_x())
-                    .map(|v| v.is_empty())
-                    .unwrap_or(true)
-                {
-                    mesh.push_quad(create_quad(
-                        offs + pos.map(|e| e as f32) + Vec3::unit_y(),
-                        -Vec3::unit_y(),
-                        Vec3::unit_z(),
-                        -Vec3::unit_x(),
-                        col,
-                        0,
-                    ));
-                }
-                // +x
-                if self.get(pos + Vec3::unit_x())
-                    .map(|v| v.is_empty())
-                    .unwrap_or(true)
-                {
-                    mesh.push_quad(create_quad(
-                        offs + pos.map(|e| e as f32) + Vec3::unit_x(),
-                        Vec3::unit_y(),
-                        Vec3::unit_z(),
-                        Vec3::unit_x(),
-                        col,
-                        0,
-                    ));
-                }
-                // -y
-                if self.get(pos - Vec3::unit_y())
-                    .map(|v| v.is_empty())
-                    .unwrap_or(true)
-                {
-                    mesh.push_quad(create_quad(
-                        offs + pos.map(|e| e as f32),
-                        Vec3::unit_x(),
-                        Vec3::unit_z(),
-                        -Vec3::unit_y(),
-                        col,
-                        0,
-                    ));
-                }
-                // +y
-                if self.get(pos + Vec3::unit_y())
-                    .map(|v| v.is_empty())
-                    .unwrap_or(true)
-                {
-                    mesh.push_quad(create_quad(
-                        offs + pos.map(|e| e as f32) + Vec3::unit_y(),
-                        Vec3::unit_z(),
-                        Vec3::unit_x(),
-                        Vec3::unit_y(),
-                        col,
-                        0,
-                    ));
-                }
-                // -z
-                if self.get(pos - Vec3::unit_z())
-                    .map(|v| v.is_empty())
-                    .unwrap_or(true)
-                {
-                    mesh.push_quad(create_quad(
-                        offs + pos.map(|e| e as f32),
-                        Vec3::unit_y(),
-                        Vec3::unit_x(),
-                        -Vec3::unit_z(),
-                        col,
-                        0,
-                    ));
-                }
-                // +z
-                if self.get(pos + Vec3::unit_z())
-                    .map(|v| v.is_empty())
-                    .unwrap_or(true)
-                {
-                    mesh.push_quad(create_quad(
-                        offs + pos.map(|e| e as f32) + Vec3::unit_z(),
-                        Vec3::unit_x(),
-                        Vec3::unit_y(),
-                        Vec3::unit_z(),
-                        col,
-                        0,
-                    ));
-                }
+                ao::push_vox_verts_ao(
+                    &mut mesh,
+                    self,
+                    pos,
+                    offs + pos.map(|e| e as f32),
+                    col,
+                    create_vertex,
+                );
             }
         }
 
--- voxygen/src/mesh/terrain.rs
+++ voxygen/src/mesh/terrain.rs
@@ -10,40 +10,11 @@ use common::{
 
 // Crate
 use crate::{
-    mesh::Meshable,
+    mesh::{ao, Meshable},
     render::{self, Mesh, Quad, TerrainPipeline},
 };
 
 type TerrainVertex = <TerrainPipeline as render::Pipeline>::Vertex;
-
-// Utility function
-fn create_quad(
-    origin: Vec3<f32>,
-    unit_x: Vec3<f32>,
-    unit_y: Vec3<f32>,
-    norm: Vec3<f32>,
-    col: Rgb<f32>,
-    ao: Vec4<f32>,
-) -> Quad<TerrainPipeline> {
-    let ao_scale = 1.0;
-    let dark = col * (1.0 - ao_scale);
-
-    if ao[0] + ao[2] < ao[1] + ao[3] {
-        Quad::new(
-            TerrainVertex::new(origin + unit_y, norm, Rgb::lerp(dark, col, ao[3])),
-            TerrainVertex::new(origin, norm, Rgb::lerp(dark, col, ao[0])),
-            TerrainVertex::new(origin + unit_x, norm, Rgb::lerp(dark, col, ao[1])),
-            TerrainVertex::new(origin + unit_x + unit_y, norm, Rgb::lerp(dark, col, ao[2])),
-        )
-    } else {
-        Quad::new(
-            TerrainVertex::new(origin, norm, Rgb::lerp(dark, col, ao[0])),
-            TerrainVertex::new(origin + unit_x, norm, Rgb::lerp(dark, col, ao[1])),
-            TerrainVertex::new(origin + unit_x + unit_y, norm, Rgb::lerp(dark, col, ao[2])),
-            TerrainVertex::new(origin + unit_y, norm, Rgb::lerp(dark, col, ao[3])),
-        )
-    }
-}
 
 impl<M> Meshable for Dyna<Block, M> {
     type Pipeline = TerrainPipeline;
@@ -65,128 +36,7 @@ impl<M> Meshable for Dyna<Block, M> {
             if let Some(col) = self.get(pos).ok().and_then(|vox| vox.get_color()) {
                 let col = col.map(|e| e as f32 / 255.0);
 
-                let (x, y, z) = (Vec3::unit_x(), Vec3::unit_y(), Vec3::unit_z());
-
-                fn get_ao<M>(
-                    dyna: &Dyna<Block, M>,
-                    pos: Vec3<i32>,
-                    dirs: &[Vec3<i32>],
-                ) -> Vec4<f32> {
-                    dirs.windows(2)
-                        .map(|offs| {
-                            let (s1, s2) = (
-                                dyna.get(pos + offs[0])
-                                    .map(|v| v.is_empty() as i32)
-                                    .unwrap_or(1),
-                                dyna.get(pos + offs[1])
-                                    .map(|v| v.is_empty() as i32)
-                                    .unwrap_or(1),
-                            );
-
-                            if s1 == 0 && s2 == 0 {
-                                0
-                            } else {
-                                let corner = dyna
-                                    .get(pos + offs[0] + offs[1])
-                                    .map(|v| v.is_empty() as i32)
-                                    .unwrap_or(1);
-                                s1 + s2 + corner
-                            }
-                        })
-                        .map(|i| i as f32 / 3.0)
-                        .collect::<Vec4<f32>>()
-                }
-
-                // -x
-                if self
-                    .get(pos - Vec3::unit_x())
-                    .map(|v| v.is_empty())
-                    .unwrap_or(true)
-                {
-                    mesh.push_quad(create_quad(
-                        offs,
-                        Vec3::unit_z(),
-                        Vec3::unit_y(),
-                        -Vec3::unit_x(),
-                        col,
-                        get_ao(self, pos - Vec3::unit_x(), &[-z, -y, z, y, -z]),
-                    ));
-                }
-                // +x
-                if self
-                    .get(pos + Vec3::unit_x())
-                    .map(|v| v.is_empty())
-                    .unwrap_or(true)
-                {
-                    mesh.push_quad(create_quad(
-                        offs + Vec3::unit_x(),
-                        Vec3::unit_y(),
-                        Vec3::unit_z(),
-                        Vec3::unit_x(),
-                        col,
-                        get_ao(self, pos + Vec3::unit_x(), &[-y, -z, y, z, -y]),
-                    ));
-                }
-                // -y
-                if self
-                    .get(pos - Vec3::unit_y())
-                    .map(|v| v.is_empty())
-                    .unwrap_or(true)
-                {
-                    mesh.push_quad(create_quad(
-                        offs,
-                        Vec3::unit_x(),
-                        Vec3::unit_z(),
-                        -Vec3::unit_y(),
-                        col,
-                        get_ao(self, pos - Vec3::unit_y(), &[-x, -z, x, z, -x]),
-                    ));
-                }
-                // +y
-                if self
-                    .get(pos + Vec3::unit_y())
-                    .map(|v| v.is_empty())
-                    .unwrap_or(true)
-                {
-                    mesh.push_quad(create_quad(
-                        offs + Vec3::unit_y(),
-                        Vec3::unit_z(),
-                        Vec3::unit_x(),
-                        Vec3::unit_y(),
-                        col,
-                        get_ao(self, pos + Vec3::unit_y(), &[-z, -x, z, x, -z]),
-                    ));
-                }
-                // -z
-                if self
-                    .get(pos - Vec3::unit_z())
-                    .map(|v| v.is_empty())
-                    .unwrap_or(true)
-                {
-                    mesh.push_quad(create_quad(
-                        offs,
-                        Vec3::unit_y(),
-                        Vec3::unit_x(),
-                        -Vec3::unit_z(),
-                        col,
-                        get_ao(self, pos - Vec3::unit_z(), &[-y, -x, y, x, -y]),
-                    ));
-                }
-                // +z
-                if self
-                    .get(pos + Vec3::unit_z())
-                    .map(|v| v.is_empty())
-                    .unwrap_or(true)
-                {
-                    mesh.push_quad(create_quad(
-                        offs + Vec3::unit_z(),
-                        Vec3::unit_x(),
-                        Vec3::unit_y(),
-                        Vec3::unit_z(),
-                        col,
-                        get_ao(self, pos + Vec3::unit_z(), &[-x, -y, x, y, -x]),
-                    ));
-                }
+                ao::push_vox_verts_ao(&mut mesh, self, pos, offs, col, TerrainVertex::new);
             }
         }
 
