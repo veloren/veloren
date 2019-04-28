5d9e9823f7b63909ecd9b1234d29f25311f959c4
robojumper <robojumper@gmail.com>
2019-04-28 19:12:45 +0200

    Generalize meshing and AO code over terrain, figure
    Former-commit-id: f0a91173ec5a0a9c683e6d1c222cb74e858625b7

--- voxygen/src/mesh/mod.rs
+++ voxygen/src/mesh/mod.rs
@@ -1,11 +1,9 @@
+mod ao;
 pub mod segment;
 pub mod terrain;
 
 // Crate
-use crate::render::{
-    self,
-    Mesh,
-};
+use crate::render::{self, Mesh};
 
 pub trait Meshable {
     type Pipeline: render::Pipeline;
