08aa8875fda70c528edf569be82c2a8bfb23fb31
timokoesters <timo@koesters.xyz>
2019-05-03 18:56:18 +0200

    Separate settings and small windows and convert chat
    Former-commit-id: 847b0b8cf64b9d2de10b241d37bb7e21c126d314

--- voxygen/src/hud/chat.rs
+++ voxygen/src/hud/chat.rs
@@ -1,13 +1,15 @@
-use crate::ui::Ui;
-use super::img_ids::Imgs;
-use super::font_ids::Fonts;
 use conrod_core::{
-    color,
+    builder_methods, color,
     input::Key,
+    text::font,
+    widget::{self, Button, Id, List, Rectangle, Text, TextEdit},
     position::Dimension,
-    text::font::Id as FontId,
-    widget::{Button, Id, List, Rectangle, Text, TextEdit},
-    widget_ids, Colorable, Positionable, Sizeable, UiCell, Widget,
+    UiCell, widget_ids, Color, Colorable, Labelable, Positionable, Sizeable, Widget, WidgetCommon,
+};
+use super::{
+    img_ids::Imgs,
+    font_ids::Fonts,
+    TEXT_COLOR,
 };
 use std::collections::VecDeque;
 
@@ -20,49 +22,35 @@ widget_ids! {
         chat_arrow,
     }
 }
-// Chat Behaviour:
-// Input Window is only shown when the player presses Enter (graphical overlay to make it look better?)
-// Instead of a Scrollbar it could have 3 Arrows at it's left side
-// First two: Scroll the chat up and down
-// Last one: Gets back to the bottom of the chat
 
-// Consider making this a custom Widget
-pub struct Chat {
-    ids: Ids,
-    messages: VecDeque<String>,
-    input: String,
-    new_messages: bool,
+#[derive(WidgetCommon)]
+pub struct Chat<'a> {
+    new_messages: &'a mut VecDeque<String>,
+
+    imgs: &'a Imgs,
+    fonts: &'a Fonts,
+
+    #[conrod(common_builder)]
+    common: widget::CommonBuilder,
+    style: (),
 }
-impl Chat {
-    pub fn new(ui: &mut Ui) -> Self {
-        Chat {
-            ids: Ids::new(ui.id_generator()),
-            messages: VecDeque::new(),
-            input: String::new(),
-            new_messages: false,
+
+impl<'a> Chat<'a> {
+    pub fn new(new_messages: &'a mut VecDeque<String>, imgs: &'a Imgs, fonts: &'a Fonts) -> Self {
+        Self {
+            new_messages,
+            imgs,
+            fonts,
+            common: widget::CommonBuilder::default(),
+            style: (),
         }
     }
-    pub fn input_box_id(&self) -> Id {
-        self.ids.input
-    }
-    pub fn new_message(&mut self, msg: String) {
-        self.messages.push_back(msg);
-        self.new_messages = true;
-    }
-    // Determine if the message box is scrolled to the bottom
-    // (i.e. the player is viewing new messages)
-    // If so scroll down when new messages are added
-    fn scroll_new_messages(&self, ui_widgets: &mut UiCell) {
-        // If previously scrolled to the bottom stay there
-        if self.scrolled_to_bottom(ui_widgets) {
-            self.scroll_to_bottom(ui_widgets);
-        }
-    }
-    fn scrolled_to_bottom(&self, ui_widgets: &UiCell) -> bool {
+
+    fn scrolled_to_bottom(state: &State, ui: &UiCell) -> bool {
         // could be more efficient to cache result and update it when a scroll event has occurred instead of every frame
-        if let Some(scroll) = ui_widgets
+        if let Some(scroll) = ui
             .widget_graph()
-            .widget(self.ids.message_box)
+            .widget(state.ids.message_box)
             .and_then(|widget| widget.maybe_y_scroll_state)
         {
             scroll.offset >= scroll.offset_bounds.start
@@ -70,50 +58,77 @@ impl Chat {
             false
         }
     }
-    fn scroll_to_bottom(&self, ui_widgets: &mut UiCell) {
-        ui_widgets.scroll_widget(self.ids.message_box, [0.0, std::f64::MAX]);
+}
+
+pub struct State {
+    messages: VecDeque<String>,
+    input: String,
+
+    ids: Ids,
+}
+
+pub enum Event {
+    SendMessage(String),
+}
+
+impl<'a> Widget for Chat<'a> {
+    type State = State;
+    type Style = ();
+    type Event = Option<Event>;
+
+    fn init_state(&self, id_gen: widget::id::Generator) -> Self::State {
+        State {
+            messages: VecDeque::new(),
+            input: "".to_owned(),
+            ids: Ids::new(id_gen),
+        }
     }
-    pub(super) fn update_layout(
-        &mut self,
-        ui_widgets: &mut UiCell,
-        imgs: &Imgs,
-        fonts: &Fonts,
-    ) -> Option<String> {
+
+    fn style(&self) -> Self::Style {
+        ()
+    }
+
+    fn update(self, args: widget::UpdateArgs<Self>) -> Self::Event {
+        let widget::UpdateArgs {
+            id,
+            state,
+            ui,
+            style,
+            ..
+        } = args;
+
         // Maintain scrolling
-        if self.new_messages {
-            self.scroll_new_messages(ui_widgets);
-            self.new_messages = false;
+        if !self.new_messages.is_empty() {
+            state.update(|s| s.messages.extend(self.new_messages.drain(..)));
+            ui.scroll_widget(state.ids.message_box, [0.0, std::f64::MAX]);
         }
 
         // Only show if it has the keyboard captured
         // Chat input with rectangle as background
-        let keyboard_captured = ui_widgets
-            .global_input()
-            .current
-            .widget_capturing_keyboard
-            .map_or(false, |id| id == self.ids.input);
+        let keyboard_captured = ui.global_input().current.widget_capturing_keyboard == Some(id);
         if keyboard_captured {
-            let text_edit = TextEdit::new(&self.input)
+            let text_edit = TextEdit::new(&state.input)
                 .w(460.0)
                 .restrict_to_height(false)
                 .line_spacing(2.0)
                 .font_size(15)
-                .font_id(fonts.opensans);
-            let y = match text_edit.get_y_dimension(ui_widgets) {
+                .font_id(self.fonts.opensans);
+            let y = match text_edit.get_y_dimension(ui) {
                 Dimension::Absolute(y) => y + 6.0,
                 _ => 0.0,
             };
             Rectangle::fill([470.0, y])
                 .rgba(0.0, 0.0, 0.0, 0.8)
-                .bottom_left_with_margins_on(ui_widgets.window, 10.0, 10.0)
+                .bottom_left_with_margins_on(ui.window, 10.0, 10.0)
                 .w(470.0)
-                .set(self.ids.input_bg, ui_widgets);
+                .set(state.ids.input_bg, ui);
             if let Some(str) = text_edit
-                .top_left_with_margins_on(self.ids.input_bg, 1.0, 1.0)
-                .set(self.ids.input, ui_widgets)
+                .top_left_with_margins_on(state.ids.input_bg, 1.0, 1.0)
+                .set(state.ids.input, ui)
             {
-                self.input = str.to_string();
-                self.input.retain(|c| c != '\n');
+                let mut input = str.to_owned();
+                input.retain(|c| c != '\n');
+                state.update(|s| s.input = input);
             }
         }
 
@@ -127,23 +142,23 @@ impl Chat {
                     r.bottom_left_with_margins_on(ui_widgets.window, 10.0, 10.0)
                 }
             })
-            .set(self.ids.message_box_bg, ui_widgets);
-        let (mut items, _) = List::flow_down(self.messages.len() + 1)
-            .top_left_of(self.ids.message_box_bg)
+            .set(state.ids.message_box_bg, ui);
+        let (mut items, _) = List::flow_down(state.messages.len() + 1)
+            .top_left_of(state.ids.message_box_bg)
             .w_h(470.0, 174.0)
             .scroll_kids_vertically()
-            .set(self.ids.message_box, ui_widgets);
-        while let Some(item) = items.next(ui_widgets) {
+            .set(state.ids.message_box, ui);
+        while let Some(item) = items.next(ui) {
             // This would be easier if conrod used the v-metrics from rusttype
-            let widget = if item.i < self.messages.len() {
-                let text = Text::new(&self.messages[item.i])
+            let widget = if item.i < state.messages.len() {
+                let text = Text::new(&state.messages[item.i])
                     .font_size(15)
-                    .font_id(fonts.opensans)
+                    .font_id(self.fonts.opensans)
                     .w(470.0)
                     .rgba(1.0, 1.0, 1.0, 1.0)
                     .line_spacing(2.0);
                 // Add space between messages
-                let y = match text.get_y_dimension(ui_widgets) {
+                let y = match text.get_y_dimension(ui) {
                     Dimension::Absolute(y) => y + 2.0,
                     _ => 0.0,
                 };
@@ -151,40 +166,41 @@ impl Chat {
             } else {
                 // Spacer at bottom of the last message so that it is not cut off
                 // Needs to be larger than the space above
-                Text::new("").font_size(6).font_id(fonts.opensans).w(470.0)
+                Text::new("").font_size(6).font_id(self.fonts.opensans).w(470.0)
             };
-            item.set(widget, ui_widgets);
+            item.set(widget, ui);
         }
 
         // Chat Arrow
-        if !self.scrolled_to_bottom(ui_widgets) {
-            if Button::image(imgs.chat_arrow)
+        // Check if already at bottom
+        if !Self::scrolled_to_bottom(state, ui) {
+            if Button::image(self.imgs.chat_arrow)
                 .w_h(20.0, 20.0)
-                .hover_image(imgs.chat_arrow_mo)
-                .press_image(imgs.chat_arrow_press)
-                .bottom_right_with_margins_on(self.ids.message_box_bg, 0.0, -22.0)
-                .set(self.ids.chat_arrow, ui_widgets)
+                .hover_image(self.imgs.chat_arrow_mo)
+                .press_image(self.imgs.chat_arrow_press)
+                .bottom_right_with_margins_on(state.ids.message_box_bg, 0.0, -22.0)
+                .set(state.ids.chat_arrow, ui)
                 .was_clicked()
             {
-                self.scroll_to_bottom(ui_widgets);
+                ui.scroll_widget(state.ids.message_box, [0.0, std::f64::MAX]);
             }
         }
 
         // If enter is pressed and the input box is not empty send the current message
-        if ui_widgets
-            .widget_input(self.ids.input)
+        if ui
+            .widget_input(state.ids.input)
             .presses()
             .key()
             .any(|key_press| match key_press.key {
-                Key::Return if !self.input.is_empty() => true,
+                Key::Return if !state.input.is_empty() => true,
                 _ => false,
             })
         {
-            let new_message = self.input.clone();
-            self.input.clear();
-            Some(new_message)
-        } else {
-            None
+            let msg = state.input.clone();
+            state.update(|s| s.input.clear());
+            return Some(Event::SendMessage(msg))
         }
+
+        None
     }
 }
