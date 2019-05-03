0096947eae154441fb34ec6aa782f3ff633fc642
Pfauenauge <m.marzouq@gmx.de>
2019-05-03 13:02:03 +0200

    New Buttons and Input Frame
    Former-commit-id: cf05992eca9e6a75cedda8c873ef268e4bbaade8

--- voxygen/src/hud/mod.rs
+++ voxygen/src/hud/mod.rs
@@ -219,9 +219,9 @@ pub(self) struct Imgs {
     // Menu
     esc_bg: ImgId,
     fireplace: ImgId,
-    button_dark: ImgId,
-    button_dark_hover: ImgId,
-    button_dark_press: ImgId,
+    button: ImgId,
+    button_hover: ImgId,
+    button_press: ImgId,
 
     // MiniMap
     mmap_frame: ImgId,    
@@ -382,9 +382,9 @@ impl Imgs {
             // Esc-Menu
             esc_bg: load_img("element/frames/menu.png", ui),
             fireplace: load_vox("element/misc_bg/fireplace.vox", ui),
-            button_dark: load_vox("element/buttons/button_dark.vox", ui),
-            button_dark_hover: load_img("element/buttons/button_dark_hover.png", ui),
-            button_dark_press: load_img("element/buttons/button_dark_press.png", ui),
+            button: load_vox("element/buttons/button.vox", ui),
+            button_hover: load_img("element/buttons/button_hover.png", ui),
+            button_press: load_img("element/buttons/button_press.png", ui),
 
             // MiniMap
             mmap_frame: load_vox("element/frames/mmap.vox", ui),            
@@ -1300,7 +1300,7 @@ impl Hud {
                 .font_id(self.font_opensans)
                 .font_size(18)
                 .set(self.ids.controls_text, ui_widgets);
-                // TODO
+                // TODO: Replace with buttons that show the actual keybind and allow the user to change it.
                 Text::new(
                     "TAB\n\
                      F1\n\
@@ -1769,11 +1769,11 @@ impl Hud {
                 .set(self.ids.fireplace, ui_widgets);
 
             // Settings
-            if Button::image(self.imgs.button_dark)
+            if Button::image(self.imgs.button)
                 .mid_top_with_margin_on(self.ids.esc_bg, 115.0)
                 .w_h(170.0, 50.0)
-                .hover_image(self.imgs.button_dark_hover)
-                .press_image(self.imgs.button_dark_press)
+                .hover_image(self.imgs.button_hover)
+                .press_image(self.imgs.button_press)
                 .label("Settings")
                 .label_y(conrod_core::position::Relative::Scalar(2.0))
                 .label_color(TEXT_COLOR)
@@ -1785,11 +1785,11 @@ impl Hud {
                 self.open_windows = Windows::Settings;
             };
             // Controls
-            if Button::image(self.imgs.button_dark)
+            if Button::image(self.imgs.button)
                 .mid_top_with_margin_on(self.ids.esc_bg, 175.0)
                 .w_h(170.0, 50.0)
-                .hover_image(self.imgs.button_dark_hover)
-                .press_image(self.imgs.button_dark_press)
+                .hover_image(self.imgs.button_hover)
+                .press_image(self.imgs.button_press)
                 .label("Controls")
                 .label_y(conrod_core::position::Relative::Scalar(2.0))
                 .label_color(TEXT_COLOR)
@@ -1802,11 +1802,11 @@ impl Hud {
                 self.open_windows = Windows::Settings;
             };
             // Servers
-            if Button::image(self.imgs.button_dark)
+            if Button::image(self.imgs.button)
                 .mid_top_with_margin_on(self.ids.esc_bg, 235.0)
                 .w_h(170.0, 50.0)
-                .hover_image(self.imgs.button_dark_hover)
-                .press_image(self.imgs.button_dark_press)
+                .hover_image(self.imgs.button_hover)
+                .press_image(self.imgs.button_press)
                 .label("Servers")
                 .label_y(conrod_core::position::Relative::Scalar(2.0))
                 .label_color(TEXT_COLOR)
@@ -1817,11 +1817,11 @@ impl Hud {
                 //self.menu_open = false;
             };
             // Logout
-            if Button::image(self.imgs.button_dark)
+            if Button::image(self.imgs.button)
                 .mid_top_with_margin_on(self.ids.esc_bg, 295.0)
                 .w_h(170.0, 50.0)
-                .hover_image(self.imgs.button_dark_hover)
-                .press_image(self.imgs.button_dark_press)
+                .hover_image(self.imgs.button_hover)
+                .press_image(self.imgs.button_press)
                 .label("Logout")
                 .label_y(conrod_core::position::Relative::Scalar(2.0))
                 .label_color(TEXT_COLOR)
@@ -1832,11 +1832,11 @@ impl Hud {
                 events.push(Event::Logout);
             };
             // Quit
-            if Button::image(self.imgs.button_dark)
+            if Button::image(self.imgs.button)
                 .mid_top_with_margin_on(self.ids.esc_bg, 355.0)
                 .w_h(170.0, 50.0)
-                .hover_image(self.imgs.button_dark_hover)
-                .press_image(self.imgs.button_dark_press)
+                .hover_image(self.imgs.button_hover)
+                .press_image(self.imgs.button_press)
                 .label("Quit")
                 .label_y(conrod_core::position::Relative::Scalar(2.0))
                 .label_color(TEXT_COLOR)
