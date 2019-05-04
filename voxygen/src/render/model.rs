78fd9da4cec216287d1f0fd641a0903ebdf3b937
Imbris <hank.aa7@gmail.com>
2019-05-04 10:28:21 -0400

    Stop recreating vertex buffers for ui
    Former-commit-id: 6e9326d1bca942e4da70281e3d86b624f9d2504a

--- voxygen/src/render/model.rs
+++ voxygen/src/render/model.rs
@@ -1,8 +1,11 @@
-// Library
-use gfx::{self, traits::FactoryExt};
-
-// Local
-use super::{gfx_backend, mesh::Mesh, Pipeline};
+use super::{gfx_backend, mesh::Mesh, Pipeline, RenderError};
+use gfx::{
+    buffer::Role,
+    memory::{Bind, Usage},
+    traits::FactoryExt,
+    Factory,
+};
+use std::ops::Range;
 
 /// Represents a mesh that has been sent to the GPU.
 pub struct Model<P: Pipeline> {
@@ -22,5 +25,45 @@ impl<P: Pipeline> Model<P> {
                 buffer: gfx::IndexBuffer::Auto,
             },
         }
+    }
+}
+
+/// Represents a mesh on the GPU which can be updated dynamically
+pub struct DynamicModel<P: Pipeline> {
+    pub vbuf: gfx::handle::Buffer<gfx_backend::Resources, P::Vertex>,
+}
+
+impl<P: Pipeline> DynamicModel<P> {
+    pub fn new(factory: &mut gfx_backend::Factory, size: usize) -> Result<Self, RenderError> {
+        Ok(Self {
+            vbuf: factory
+                .create_buffer(size, Role::Vertex, Usage::Dynamic, Bind::empty())
+                .map_err(|err| RenderError::BufferCreationError(err))?,
+        })
+    }
+
+    /// Create a model with a slice of a portion of this model to send to the renderer
+    pub fn submodel(&self, range: Range<usize>) -> Model<P> {
+        Model {
+            vbuf: self.vbuf.clone(),
+            slice: gfx::Slice {
+                start: range.start as u32,
+                end: range.end as u32,
+                base_vertex: 0,
+                instances: None,
+                buffer: gfx::IndexBuffer::Auto,
+            },
+        }
+    }
+
+    pub fn update(
+        &self,
+        encoder: &mut gfx::Encoder<gfx_backend::Resources, gfx_backend::CommandBuffer>,
+        mesh: &Mesh<P>,
+        offset: usize,
+    ) -> Result<(), RenderError> {
+        encoder
+            .update_buffer(&self.vbuf, mesh.vertices(), offset)
+            .map_err(|err| RenderError::UpdateError(err))
     }
 }
