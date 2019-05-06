87e75644ec0be8f916173b5d12154d752500305e
timokoesters <timo@koesters.xyz>
2019-05-06 19:29:55 +0200

    Fix fonts
    Former-commit-id: c7929a0b540099ac67b93563d97be4b8c6639190

--- voxygen/src/hud/font_ids.rs
+++ voxygen/src/hud/font_ids.rs
@@ -2,7 +2,7 @@ use conrod_core::text::Font;
 
 font_ids! {
     pub struct Fonts {
-        opensans: "/voxygen/font/Metamorphous-Regular.ttf",
-        metamorph: "/voxygen/font/OpenSans-Regular.ttf",
+        opensans: "/voxygen/font/OpenSans-Regular.ttf",
+        metamorph: "/voxygen/font/Metamorphous-Regular.ttf",
     }
 }
