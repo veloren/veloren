0f287945473d64ac1cb1a8f3410e879c1b88d988
Joshua Barretto <joshua.s.barretto@gmail.com>
2019-05-09 18:12:24 +0100

    Smartened up terrain generation

--- voxygen/src/key_state.rs
+++ voxygen/src/key_state.rs
@@ -20,9 +20,15 @@ impl KeyState {
     }
 
     pub fn dir_vec(&self) -> Vec2<f32> {
-        Vec2::<f32>::new(
+        let dir = Vec2::<f32>::new(
             if self.right { 1.0 } else { 0.0 } + if self.left { -1.0 } else { 0.0 },
             if self.up { 1.0 } else { 0.0 } + if self.down { -1.0 } else { 0.0 },
-        )
+        );
+
+        if dir.magnitude_squared() == 0.0 {
+            dir
+        } else {
+            dir.normalized()
+        }
     }
 }
