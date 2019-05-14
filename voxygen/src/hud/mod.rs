5aae1cbe6d92e3a9da29d67192e21fa477ef221b
Pfauenauge <m.marzouq@gmx.de>
2019-05-14 14:57:25 +0200

    Changed font name for testing

--- voxygen/src/hud/mod.rs
+++ voxygen/src/hud/mod.rs
@@ -84,7 +84,7 @@ widget_ids! {
 font_ids! {
     pub struct Fonts {
         opensans: "/voxygen/font/OpenSans-Regular.ttf",
-        metamorph: "/voxygen/font/Metamorphous-Regular.ttf",
+        metamorph: "/voxygen/font/Metamorphous-Regular.ttf",        
     }
 }
 
