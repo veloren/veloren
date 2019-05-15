5092df0e7f0f21577311487dbbb611703c6f7d12
Joshua Barretto <joshua.s.barretto@gmail.com>
2019-05-15 17:06:58 +0100

    Improved quadruped origin
    Former-commit-id: 35a360bf285f2495dda7859cb8d376d5aa16a1af

--- voxygen/src/anim/quadruped/run.rs
+++ voxygen/src/anim/quadruped/run.rs
@@ -30,27 +30,27 @@ impl Animation for RunAnimation {
         let wavecos_slow = (anim_time as f32 * 8.0 + PI).cos();
         let wave_dip = (wave_slow.abs() - 0.5).abs();
 
-        next.pighead.offset = Vec3::new(0.0, 9.0, -1.5 + wave * 1.5) / 11.0;
+        next.pighead.offset = Vec3::new(0.0, 0.0, -1.5 + wave * 1.5) / 11.0;
         next.pighead.ori = Quaternion::rotation_x(0.2 + wave * 0.05) * Quaternion::rotation_y(wavecos * 0.03);
         next.pighead.scale = Vec3::one() / 10.5;
 
-        next.pigchest.offset = Vec3::new(0.0, 0.0, 1.5 + wavecos * 1.2) / 11.0;
+        next.pigchest.offset = Vec3::new(0.0, -9.0, 1.5 + wavecos * 1.2) / 11.0;
         next.pigchest.ori = Quaternion::rotation_x(wave * 0.1);
         next.pigchest.scale = Vec3::one() / 11.0;
 
-        next.piglf_leg.offset = Vec3::new(-4.5, 11.0 + wavequick * 0.8, 2.5 + wavequickcos * 1.5) / 11.0;
+        next.piglf_leg.offset = Vec3::new(-4.5, 2.0 + wavequick * 0.8, 2.5 + wavequickcos * 1.5) / 11.0;
         next.piglf_leg.ori = Quaternion::rotation_x(wavequick *  0.3);
         next.piglf_leg.scale = Vec3::one() / 11.0;
 
-        next.pigrf_leg.offset = Vec3::new(2.5, 11.0 - wavequickcos * 0.8, 2.5 + wavequick * 1.5) / 11.0;
+        next.pigrf_leg.offset = Vec3::new(2.5, 2.0 - wavequickcos * 0.8, 2.5 + wavequick * 1.5) / 11.0;
         next.pigrf_leg.ori = Quaternion::rotation_x(wavequickcos * -0.3);
         next.pigrf_leg.scale = Vec3::one() / 11.0;
 
-        next.piglb_leg.offset = Vec3::new(-4.5, 6.0 - wavequickcos * 0.8, 2.5 + wavequick * 1.5) / 11.0;
+        next.piglb_leg.offset = Vec3::new(-4.5, -3.0 - wavequickcos * 0.8, 2.5 + wavequick * 1.5) / 11.0;
         next.piglb_leg.ori = Quaternion::rotation_x(wavequickcos * -0.3);
         next.piglb_leg.scale = Vec3::one() / 11.0;
 
-        next.pigrb_leg.offset = Vec3::new(2.5, 6.0 + wavequick * 0.8, 2.5 + wavequickcos * 1.5) / 11.0;
+        next.pigrb_leg.offset = Vec3::new(2.5, -3.0 + wavequick * 0.8, 2.5 + wavequickcos * 1.5) / 11.0;
         next.pigrb_leg.ori = Quaternion::rotation_x(wavequick *  0.3);
         next.pigrb_leg.scale = Vec3::one() / 11.0;
 
