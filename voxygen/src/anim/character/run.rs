0068d6203b08b71e6dbb45b84da2ad3145cd2d15
jshipsey <jshipsey18@gmail.com>
2019-05-16 01:29:48 -0400

    popup origin fix
    Former-commit-id: 9a7ba69115e7b574bc2f3bf26afad71e3c682c38

--- voxygen/src/anim/character/run.rs
+++ voxygen/src/anim/character/run.rs
@@ -76,7 +76,7 @@ impl Animation for RunAnimation {
         next.torso.ori = Quaternion::rotation_x(-velocity * 0.05 - wavecos * 0.1);
         next.torso.scale = Vec3::one() / 11.0;
 
-        next.draw.offset = Vec3::new(0.0, 0.0, 0.0);
+        next.draw.offset = Vec3::new(13.5, 0.0, 0.0);
         next.draw.ori = Quaternion::rotation_y(0.0);
         next.draw.scale = Vec3::one() * 0.0;
 
