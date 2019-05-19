811978b744d075cd00e2b35dc68e95bf9970182c
timokoesters <timo@koesters.xyz>
2019-05-19 22:46:53 +0200

    Add knockback

--- common/src/sys/action.rs
+++ common/src/sys/action.rs
@@ -5,7 +5,7 @@ use vek::*;
 // Crate
 use crate::{
     comp::{
-        phys::{Pos, Vel},
+        phys::{ForceUpdate, Pos, Vel},
         Action, Actions, Control, Stats,
     },
     state::{DeltaTime, Time},
@@ -23,11 +23,12 @@ impl<'a> System<'a> for Sys {
         ReadStorage<'a, Pos>,
         WriteStorage<'a, Vel>,
         WriteStorage<'a, Stats>,
+        WriteStorage<'a, ForceUpdate>,
     );
 
     fn run(
         &mut self,
-        (entities, time, dt, mut actions, positions, mut velocities, mut stats): Self::SystemData,
+        (entities, time, dt, mut actions, positions, mut velocities, mut stats, mut force_updates): Self::SystemData,
     ) {
         for (a, actions_a, pos_a) in (&entities, &mut actions, &positions).join() {
             for event in actions_a.0.drain(..) {
@@ -40,7 +41,10 @@ impl<'a> System<'a> for Sys {
                                 continue;
                             }
                             if pos_a.0.distance_squared(pos_b.0) < 50.0 {
-                                stat_b.hp.change_by(-60); // TODO: variable damage
+                                stat_b.hp.change_by(-10); // TODO: variable damage
+                                vel_b.0 += (pos_b.0 - pos_a.0).normalized() * 20.0;
+                                vel_b.0.z = 20.0;
+                                force_updates.insert(b, ForceUpdate);
                             }
                         }
                     }
