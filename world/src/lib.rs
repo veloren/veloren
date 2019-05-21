91ce041c9116a5c9123ada43cd8da10057c9417c
Joshua Barretto <joshua.s.barretto@gmail.com>
2019-05-21 23:31:38 +0100

    fmt
    Former-commit-id: 1f7bec8da117091b3f2e8f58ce45b9caf1b6c89d

--- world/src/lib.rs
+++ world/src/lib.rs
@@ -1,15 +1,15 @@
 mod sim;
 
-use std::{
-    ops::{Add, Sub, Mul, Div, Neg},
-    time::Duration,
-};
-use noise::{NoiseFn, BasicMulti, Perlin, Seedable, MultiFractal};
-use vek::*;
 use common::{
     terrain::{Block, TerrainChunk, TerrainChunkMeta, TerrainChunkSize},
     vol::{SizedVol, VolSize, Vox, WriteVol},
 };
+use noise::{BasicMulti, MultiFractal, NoiseFn, Perlin, Seedable};
+use std::{
+    ops::{Add, Div, Mul, Neg, Sub},
+    time::Duration,
+};
+use vek::*;
 
 #[derive(Debug)]
 pub enum Error {
@@ -22,7 +22,9 @@ pub struct World {
 
 impl World {
     pub fn generate(seed: u32) -> Self {
-        Self { sim: sim::WorldSim::generate(seed) }
+        Self {
+            sim: sim::WorldSim::generate(seed),
+        }
     }
 
     pub fn sim(&self) -> &sim::WorldSim {
@@ -43,9 +45,7 @@ impl World {
         let sand = Block::new(4, Rgb::new(180, 150, 50));
         let water = Block::new(5, Rgb::new(100, 150, 255));
 
-        let warp_nz = BasicMulti::new()
-            .set_octaves(3)
-            .set_seed(self.sim.seed + 0);
+        let warp_nz = BasicMulti::new().set_octaves(3).set_seed(self.sim.seed + 0);
 
         let base_z = match self.sim.get_base_z(chunk_pos.map(|e| e as u32)) {
             Some(base_z) => base_z as i32,
@@ -56,30 +56,34 @@ impl World {
 
         for x in 0..TerrainChunkSize::SIZE.x as i32 {
             for y in 0..TerrainChunkSize::SIZE.y as i32 {
-                let wpos2d = Vec2::new(x, y) + Vec3::from(chunk_pos) * TerrainChunkSize::SIZE.map(|e| e as i32);
+                let wpos2d = Vec2::new(x, y)
+                    + Vec3::from(chunk_pos) * TerrainChunkSize::SIZE.map(|e| e as i32);
                 let wposf2d = wpos2d.map(|e| e as f64);
 
                 let sim::Sample {
                     alt,
                     chaos,
-                    surface_color
+                    surface_color,
                 } = if let Some(sample) = self.sim.sample(wpos2d) {
                     sample
                 } else {
-                    continue
+                    continue;
                 };
 
-                let max_z = self.sim
+                let max_z = self
+                    .sim
                     .get_interpolated(wpos2d, |chunk| chunk.get_max_z())
                     .unwrap_or(0.0) as i32;
 
                 for z in base_z..max_z {
                     let lpos = Vec3::new(x, y, z);
-                    let wpos = lpos
-                        + Vec3::from(chunk_pos) * TerrainChunkSize::SIZE.map(|e| e as i32);
+                    let wpos =
+                        lpos + Vec3::from(chunk_pos) * TerrainChunkSize::SIZE.map(|e| e as i32);
                     let wposf = wpos.map(|e| e as f64);
 
-                    let warp = (warp_nz.get((wposf.div(Vec3::new(120.0, 120.0, 150.0))).into_array()) as f32)
+                    let warp = (warp_nz
+                        .get((wposf.div(Vec3::new(120.0, 120.0, 150.0))).into_array())
+                        as f32)
                         .mul((chaos - 0.1).max(0.0))
                         .mul(90.0);
 
--- world/src/sim.rs
+++ world/src/sim.rs
@@ -1,13 +1,13 @@
+use common::{terrain::TerrainChunkSize, vol::VolSize};
+use noise::{
+    BasicMulti, HybridMulti, MultiFractal, NoiseFn, OpenSimplex, RidgedMulti, Seedable,
+    SuperSimplex,
+};
 use std::{
-    ops::{Add, Sub, Mul, Div, Neg},
     f32,
+    ops::{Add, Div, Mul, Neg, Sub},
 };
-use noise::{NoiseFn, BasicMulti, HybridMulti, RidgedMulti, SuperSimplex, OpenSimplex, Seedable, MultiFractal};
 use vek::*;
-use common::{
-    terrain::TerrainChunkSize,
-    vol::VolSize,
-};
 
 pub const WORLD_SIZE: Vec2<usize> = Vec2 { x: 1024, y: 1024 };
 
@@ -20,27 +20,17 @@ pub struct WorldSim {
 impl WorldSim {
     pub fn generate(seed: u32) -> Self {
         let mut gen_ctx = GenCtx {
-            turb_x_nz: BasicMulti::new()
-                .set_seed(seed + 0),
-            turb_y_nz: BasicMulti::new()
-                .set_seed(seed + 1),
-            chaos_nz: RidgedMulti::new()
-                .set_octaves(7)
-                .set_seed(seed + 2),
-            hill_nz: SuperSimplex::new()
-                .set_seed(seed + 3),
+            turb_x_nz: BasicMulti::new().set_seed(seed + 0),
+            turb_y_nz: BasicMulti::new().set_seed(seed + 1),
+            chaos_nz: RidgedMulti::new().set_octaves(7).set_seed(seed + 2),
+            hill_nz: SuperSimplex::new().set_seed(seed + 3),
             alt_nz: HybridMulti::new()
                 .set_octaves(7)
                 .set_persistence(0.1)
                 .set_seed(seed + 4),
-            temp_nz: SuperSimplex::new()
-                .set_seed(seed + 5),
-            small_nz: BasicMulti::new()
-                .set_octaves(2)
-                .set_seed(seed + 6),
-            rock_nz: HybridMulti::new()
-                .set_persistence(0.3)
-                .set_seed(seed + 7),
+            temp_nz: SuperSimplex::new().set_seed(seed + 5),
+            small_nz: BasicMulti::new().set_octaves(2).set_seed(seed + 6),
+            rock_nz: HybridMulti::new().set_persistence(0.3).set_seed(seed + 7),
         };
 
         let mut chunks = Vec::new();
@@ -58,7 +48,10 @@ impl WorldSim {
     }
 
     pub fn get(&self, chunk_pos: Vec2<u32>) -> Option<&SimChunk> {
-        if chunk_pos.map2(WORLD_SIZE, |e, sz| e < sz as u32).reduce_and() {
+        if chunk_pos
+            .map2(WORLD_SIZE, |e, sz| e < sz as u32)
+            .reduce_and()
+        {
             Some(&self.chunks[chunk_pos.y as usize * WORLD_SIZE.x + chunk_pos.x as usize])
         } else {
             None
@@ -66,25 +59,27 @@ impl WorldSim {
     }
 
     pub fn get_base_z(&self, chunk_pos: Vec2<u32>) -> Option<f32> {
-        self
-            .get(chunk_pos)
-            .and_then(|_| (0..2)
-                .map(|i| (0..2)
-                    .map(move |j| (i, j)))
+        self.get(chunk_pos).and_then(|_| {
+            (0..2)
+                .map(|i| (0..2).map(move |j| (i, j)))
                 .flatten()
-                .map(|(i, j)| self
-                    .get(chunk_pos + Vec2::new(i, j))
-                    .map(|c| c.get_base_z()))
+                .map(|(i, j)| {
+                    self.get(chunk_pos + Vec2::new(i, j))
+                        .map(|c| c.get_base_z())
+                })
                 .flatten()
-                .fold(None, |a: Option<f32>, x| a.map(|a| a.min(x)).or(Some(x))))
+                .fold(None, |a: Option<f32>, x| a.map(|a| a.min(x)).or(Some(x)))
+        })
     }
 
     pub fn get_interpolated<T, F>(&self, pos: Vec2<i32>, mut f: F) -> Option<T>
-        where
-            T: Copy + Default + Add<Output=T> + Mul<f32, Output=T>,
-            F: FnMut(&SimChunk) -> T,
+    where
+        T: Copy + Default + Add<Output = T> + Mul<f32, Output = T>,
+        F: FnMut(&SimChunk) -> T,
     {
-        let pos = pos.map2(TerrainChunkSize::SIZE.into(), |e, sz: u32| e as f64 / sz as f64);
+        let pos = pos.map2(TerrainChunkSize::SIZE.into(), |e, sz: u32| {
+            e as f64 / sz as f64
+        });
 
         let cubic = |a: T, b: T, c: T, d: T, x: f32| -> T {
             let x2 = x * x;
@@ -101,10 +96,11 @@ impl WorldSim {
         let mut y = [T::default(); 4];
 
         for (y_idx, j) in (-1..3).enumerate() {
-            let x0 = f(self.get(pos.map2(Vec2::new(-1, j), |e, q| (e.max(0.0) as i32 + q) as u32))?);
-            let x1 = f(self.get(pos.map2(Vec2::new( 0, j), |e, q| (e.max(0.0) as i32 + q) as u32))?);
-            let x2 = f(self.get(pos.map2(Vec2::new( 1, j), |e, q| (e.max(0.0) as i32 + q) as u32))?);
-            let x3 = f(self.get(pos.map2(Vec2::new( 2, j), |e, q| (e.max(0.0) as i32 + q) as u32))?);
+            let x0 =
+                f(self.get(pos.map2(Vec2::new(-1, j), |e, q| (e.max(0.0) as i32 + q) as u32))?);
+            let x1 = f(self.get(pos.map2(Vec2::new(0, j), |e, q| (e.max(0.0) as i32 + q) as u32))?);
+            let x2 = f(self.get(pos.map2(Vec2::new(1, j), |e, q| (e.max(0.0) as i32 + q) as u32))?);
+            let x3 = f(self.get(pos.map2(Vec2::new(2, j), |e, q| (e.max(0.0) as i32 + q) as u32))?);
 
             y[y_idx] = cubic(x0, x1, x2, x3, pos.x.fract() as f32);
         }
@@ -131,7 +127,9 @@ impl WorldSim {
             .mul(2.0);
 
         let alt = self.get_interpolated(pos, |chunk| chunk.alt)?
-            + self.gen_ctx.small_nz.get((wposf.div(128.0)).into_array()) as f32 * chaos.max(0.15) * 32.0
+            + self.gen_ctx.small_nz.get((wposf.div(128.0)).into_array()) as f32
+                * chaos.max(0.15)
+                * 32.0
             + rock * 15.0;
 
         // Colours
@@ -151,11 +149,7 @@ impl WorldSim {
             surface_color: Rgb::lerp(
                 sand,
                 // Land
-                Rgb::lerp(
-                    ground,
-                    cliff,
-                    (alt - SEA_LEVEL - 100.0) / 150.0
-                ),
+                Rgb::lerp(ground, cliff, (alt - SEA_LEVEL - 100.0) / 150.0),
                 // Beach
                 (alt - SEA_LEVEL - 2.0) / 5.0,
             ),
@@ -194,13 +188,11 @@ impl SimChunk {
     fn generate(pos: Vec2<u32>, gen_ctx: &mut GenCtx) -> Self {
         let wposf = (pos * Vec2::from(TerrainChunkSize::SIZE)).map(|e| e as f64);
 
-        let hill = (gen_ctx.hill_nz
-            .get((wposf.div(3500.0)).into_array()) as f32)
-            .max(0.0);
+        let hill = (gen_ctx.hill_nz.get((wposf.div(3500.0)).into_array()) as f32).max(0.0);
 
-        let chaos = (gen_ctx.chaos_nz
-            .get((wposf.div(3500.0)).into_array()) as f32)
-            .add(1.0).mul(0.5)
+        let chaos = (gen_ctx.chaos_nz.get((wposf.div(3500.0)).into_array()) as f32)
+            .add(1.0)
+            .mul(0.5)
             .powf(1.9)
             .add(0.25 * hill);
 
@@ -212,15 +204,21 @@ impl SimChunk {
 
         Self {
             chaos,
-            alt: SEA_LEVEL + (0.0
-                + alt_main
-                + gen_ctx.small_nz.get((wposf.div(300.0)).into_array()) as f32 * alt_main.max(0.05) * chaos * 1.3)
-                .add(1.0).mul(0.5)
-                .mul(chaos)
-                .add(alt_base)
-                .mul(750.0),
+            alt: SEA_LEVEL
+                + (0.0
+                    + alt_main
+                    + gen_ctx.small_nz.get((wposf.div(300.0)).into_array()) as f32
+                        * alt_main.max(0.05)
+                        * chaos
+                        * 1.3)
+                    .add(1.0)
+                    .mul(0.5)
+                    .mul(chaos)
+                    .add(alt_base)
+                    .mul(750.0),
             temp: (gen_ctx.temp_nz.get((wposf.div(48.0)).into_array()) as f32)
-                .add(1.0).mul(0.5),
+                .add(1.0)
+                .mul(0.5),
             rockiness: (gen_ctx.rock_nz.get((wposf.div(1024.0)).into_array()) as f32)
                 .sub(0.1)
                 .mul(1.2)
