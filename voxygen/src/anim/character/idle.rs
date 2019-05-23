40936e88a84c77d110deb0b96acedebc662399fd
jshipsey <jshipsey18@gmail.com>
2019-05-22 23:36:43 -0400

    cleaning up inconsistencies in animation code
    Former-commit-id: 31c8ae92c3ebf9bed01aba29f9a030de334df153

--- voxygen/src/anim/character/idle.rs
+++ voxygen/src/anim/character/idle.rs
@@ -39,19 +39,19 @@ impl Animation for IdleAnimation {
                 .sin()
                 * 0.25,
         );
-        next.head.offset = Vec3::new(5.5, 2.0, 11.0 + wave_ultra_slow * 0.3);
+        next.head.offset = Vec3::new(0.0, 2.0, 11.0 + wave_ultra_slow * 0.3);
         next.head.ori = Quaternion::rotation_z(head_look.x) * Quaternion::rotation_x(head_look.y);
         next.head.scale = Vec3::one();
 
-        next.chest.offset = Vec3::new(5.5, 0.0, 7.0 + wave_ultra_slow * 0.3);
+        next.chest.offset = Vec3::new(0.0, 0.0, 7.0 + wave_ultra_slow * 0.3);
         next.chest.ori = Quaternion::rotation_x(0.0);
         next.chest.scale = Vec3::one();
 
-        next.belt.offset = Vec3::new(5.5, 0.0, 5.0 + wave_ultra_slow * 0.3);
+        next.belt.offset = Vec3::new(0.0, 0.0, 5.0 + wave_ultra_slow * 0.3);
         next.belt.ori = Quaternion::rotation_x(0.0);
         next.belt.scale = Vec3::one();
 
-        next.shorts.offset = Vec3::new(5.5, 0.0, 2.0 + wave_ultra_slow * 0.3);
+        next.shorts.offset = Vec3::new(0.0, 0.0, 2.0 + wave_ultra_slow * 0.3);
         next.shorts.ori = Quaternion::rotation_x(0.0);
         next.shorts.scale = Vec3::one();
 
@@ -59,7 +59,6 @@ impl Animation for IdleAnimation {
             -7.5,
             -2.0 + wave_ultra_slow_cos * 0.15,
             8.0 + wave_ultra_slow * 0.5,
-            11.5 + wave_ultra_slow * 0.5,
         ) / 11.0;
 
         next.l_hand.ori = Quaternion::rotation_x(0.0 + wave_ultra_slow * 0.06);
@@ -74,15 +73,15 @@ impl Animation for IdleAnimation {
         next.r_hand.ori = Quaternion::rotation_x(0.0 + wave_ultra_slow * 0.06);
         next.r_hand.scale = Vec3::one() / 11.;
 
-        next.l_foot.offset = Vec3::new(-3.3, -0.1, 8.0);
+        next.l_foot.offset = Vec3::new(-3.4, -0.1, 8.0);
         next.l_foot.ori = Quaternion::identity();
         next.l_foot.scale = Vec3::one();
 
-        next.r_foot.offset = Vec3::new(4.1, -0.1, 8.0);
+        next.r_foot.offset = Vec3::new(3.4, -0.1, 8.0);
         next.r_foot.ori = Quaternion::identity();
         next.r_foot.scale = Vec3::one();
 
-        next.weapon.offset = Vec3::new(-9.0, -5.0, 15.0);
+        next.weapon.offset = Vec3::new(-7.0, -5.0, 15.0);
         next.weapon.ori = Quaternion::rotation_y(2.5);
         next.weapon.scale = Vec3::one();
 
@@ -94,21 +93,19 @@ impl Animation for IdleAnimation {
         next.r_shoulder.ori = Quaternion::rotation_x(0.0);
         next.r_shoulder.scale = Vec3::one() * 1.04;
 
-        next.draw.offset = Vec3::new(5.5, 0.0, 0.0);
+        next.draw.offset = Vec3::new(0.0, 5.0, 0.0);
         next.draw.ori = Quaternion::rotation_y(0.0);
         next.draw.scale = Vec3::one() * 0.0;
 
         next.left_equip.offset = Vec3::new(0.0, 0.0, 5.0) / 11.0;
         next.left_equip.ori = Quaternion::rotation_x(0.0);;
-        next.left_equip.scale = Vec3::one() *0.0;
-        
+        next.left_equip.scale = Vec3::one() * 0.0;
 
-        next.draw.offset = Vec3::new(13.5, 0.0, 0.0);
-        next.draw.ori = Quaternion::rotation_y(0.0);
-        next.draw.scale = Vec3::one() * 0.0;
-        
+        next.right_equip.offset = Vec3::new(0.0, 0.0, 5.0) / 11.0;
+        next.right_equip.ori = Quaternion::rotation_x(0.0);;
+        next.right_equip.scale = Vec3::one() * 0.0;
 
-        next.torso.offset = Vec3::new(-0.5, -0.2, 0.1);
+        next.torso.offset = Vec3::new(0.0, -0.2, 0.1);
         next.torso.ori = Quaternion::rotation_x(0.0);
         next.torso.scale = Vec3::one() / 11.0;
 
