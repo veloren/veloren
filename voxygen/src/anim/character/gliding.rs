f64c5ce307e3ff5e8995fa8fb048354b291fc50d
jshipsey <jshipsey18@gmail.com>
2019-05-22 21:48:13 -0400

    restored previous animations with new bone setup

--- voxygen/src/anim/character/gliding.rs
+++ voxygen/src/anim/character/gliding.rs
@@ -66,7 +66,7 @@ impl Animation for GlidingAnimation {
         next.l_hand.offset = Vec3::new(
             -10.0,
             6.0 - wave_very_slow * 1.50,
-            15.0 + wave_very_slow * 0.50,
+            15.0 + wave_very_slow * 0.50,   
         ) / 11.0;
         next.l_hand.ori = Quaternion::rotation_x(0.2 + wave_very_slow_cos * 0.05);
         next.l_hand.scale = Vec3::one() / 11.0;
@@ -91,22 +91,25 @@ impl Animation for GlidingAnimation {
         );
         next.r_foot.scale = Vec3::one();
 
-        next.weapon.offset = Vec3::new(-5.0, -5.0, 12.0);
+        next.weapon.offset = Vec3::new(-9.0, -5.0, 15.0);
         next.weapon.ori = Quaternion::rotation_y(2.5);
         next.weapon.scale = Vec3::one();
 
         next.l_shoulder.offset = Vec3::new(-10.0, -3.0, 2.5);
         next.l_shoulder.ori = Quaternion::rotation_x(0.0);
-        next.l_shoulder.scale = Vec3::one();
+        next.l_shoulder.scale = Vec3::one() * 1.04;
 
         next.r_shoulder.offset = Vec3::new(0.0, -3.0, 2.5);
         next.r_shoulder.ori = Quaternion::rotation_x(0.0);
-        next.r_shoulder.scale = Vec3::one();
+        next.r_shoulder.scale = Vec3::one() * 1.04;
         
         next.draw.offset = Vec3::new(5.5, -9.0 + wave_very_slow * 0.10, 18.0);
         next.draw.ori = Quaternion::rotation_x(0.95 - wave_very_slow * 0.15) * Quaternion::rotation_y(wave_very_slow_cos * 0.04);
         next.draw.scale = Vec3::one();
 
+        next.left_equip.offset = Vec3::new(0.0, 0.0, 5.0) / 11.0;
+        next.left_equip.ori = Quaternion::rotation_x(0.0);;
+        next.left_equip.scale = Vec3::one() *0.0;
 
         next.torso.offset = Vec3::new(-0.5, -0.2, 0.0);
         next.torso.ori = Quaternion::rotation_x(-0.8 + wave_very_slow * 0.10);
