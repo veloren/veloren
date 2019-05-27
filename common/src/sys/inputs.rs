f9e2828de53b853a59ee5792eb9bed3b2e2d1b64
jshipsey <jshipsey18@gmail.com>
2019-05-27 19:03:52 -0400

    swing animation changes
    Former-commit-id: 1bc70090e938c12d6e133683932b470be102aecd

--- common/src/sys/inputs.rs
+++ common/src/sys/inputs.rs
@@ -115,14 +115,13 @@ impl<'a> System<'a> for Sys {
             let animation = if on_ground {
                 if control.move_dir.magnitude() > 0.01 {
                     Animation::Run
-            } else if attackings.get(entity).is_some(){
-                Animation::Attack
+                } else if attackings.get(entity).is_some() {
+                    Animation::Attack
                 } else {
                     Animation::Idle
                 }
             } else if glidings.get(entity).is_some() {
                 Animation::Gliding
-
             } else {
                 Animation::Jump
             };
