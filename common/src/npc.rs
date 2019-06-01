384fbdc2a6ba1e8fd04b4c83b5995f4ba4803bc4
Ahmed Ihsan Tawfeeq <ahmed_ihsan_tawfeek@yahoo.com>
2019-06-01 19:24:17 +0000

    Added randomized NPC names veloren/veloren#133

---
+++ common/src/npc.rs
@@ -0,0 +1,34 @@
+use serde_json;
+use rand::seq::SliceRandom;
+use std::fs::File;
+
+pub enum NpcKind {
+    Wolf,
+    Pig
+}
+
+impl NpcKind {
+    fn as_str(&self) -> &'static str {
+        match *self {
+            NpcKind::Wolf => "wolf",
+            NpcKind::Pig => "pig"
+        }
+    }
+}
+
+pub fn get_npc_name(npc_type: NpcKind) -> String {
+    let file = File::open("common/assets/npc_names.json").expect("file should open read only");
+    let json: serde_json::Value =
+        serde_json::from_reader(file).expect("file should be proper JSON");
+    let npc_names = json
+        .get(npc_type.as_str())
+        .expect("accessing json using NPC type provided as key")
+        .as_array()
+        .expect("parsing accessed json value into an array");
+    let npc_name = npc_names
+        .choose(&mut rand::thread_rng())
+        .expect("getting a random NPC name")
+        .as_str()
+        .expect("parsing NPC name json value into a &str");
+    String::from(npc_name)
+}
