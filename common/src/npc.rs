a1ea354153691f719d0b3303abd85eff2f66d3ef
scorpion9979 <ahmed_ihsan_tawfeek@yahoo.com>
2019-06-01 22:49:34 +0300

    Cleaned code
    Refactored code for readability

--- common/src/npc.rs
+++ common/src/npc.rs
@@ -1,26 +1,30 @@
-use serde_json;
 use rand::seq::SliceRandom;
+use serde_json;
 use std::fs::File;
+use std::io::Error;
 
 pub enum NpcKind {
     Wolf,
-    Pig
+    Pig,
 }
 
 impl NpcKind {
     fn as_str(&self) -> &'static str {
         match *self {
             NpcKind::Wolf => "wolf",
-            NpcKind::Pig => "pig"
+            NpcKind::Pig => "pig",
         }
     }
 }
 
+const npc_names_dir: &str = "common/assets/npc_names.json";
+
 pub fn get_npc_name(npc_type: NpcKind) -> String {
-    let file = File::open("common/assets/npc_names.json").expect("file should open read only");
-    let json: serde_json::Value =
-        serde_json::from_reader(file).expect("file should be proper JSON");
-    let npc_names = json
+    let npc_names_file =
+        File::open(npc_names_dir).expect(&format!("opening {} in read-only mode", npc_names_dir));
+    let npc_names_json: serde_json::Value = serde_json::from_reader(npc_names_file)
+        .expect(&format!("reading json contents from {}", npc_names_dir));
+    let npc_names = npc_names_json
         .get(npc_type.as_str())
         .expect("accessing json using NPC type provided as key")
         .as_array()
