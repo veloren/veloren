245290358e4bae6ee18a81e0ecff815c57261a8a
jshipsey <jshipsey18@gmail.com>
2019-06-02 22:38:57 -0400

    fixed timing bug

--- common/src/sys/actions.rs
+++ common/src/sys/actions.rs
@@ -27,9 +27,8 @@ impl<'a> System<'a> for Sys {
     );
 
     fn run(&mut self, (entities, dt, mut attacks, mut rolls): Self::SystemData) {
-        for (entity, attack, roll) in (&entities, &mut attacks, &mut rolls).join() {
+        for (entity, attack) in (&entities, &mut attacks,).join() {
             attack.time += dt.0;
-            roll.time += dt.0;
         }
         let finished_attacks = (&entities, &mut attacks)
             .join()
@@ -42,6 +41,10 @@ impl<'a> System<'a> for Sys {
         for entity in finished_attacks {
             attacks.remove(entity);
         }
+        for (entity, roll) in (&entities, &mut rolls).join() {
+
+            roll.time += dt.0;
+		} 
         let finished_rolls = (&entities, &mut rolls)
             .join()
             .filter(|(e, a)| {
