4fdc0f936a40351e15958777c808204d9c0997ef
jshipsey <jshipsey18@gmail.com>
2019-06-03 21:53:51 -0400

    animation state changes

--- common/src/sys/actions.rs
+++ common/src/sys/actions.rs
@@ -10,7 +10,6 @@ impl<'a> System<'a> for Sys {
         Read<'a, DeltaTime>,
         WriteStorage<'a, Attacking>,
         WriteStorage<'a, Rolling>,
-
     );
 
     fn run(&mut self, (entities, dt, mut attacks, mut rolls): Self::SystemData) {
