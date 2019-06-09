f51904040bcf6babde2702e8293ccaee70b95ff3
timokoesters <timo@koesters.xyz>
2019-06-09 21:33:20 +0200

    Improve organization of controls

--- common/src/sys/animation.rs
+++ common/src/sys/animation.rs
@@ -1,15 +1,63 @@
-use crate::{comp::AnimationInfo, state::DeltaTime};
-use specs::{Join, Read, System, WriteStorage};
+use crate::{
+    comp::{phys, Animation, AnimationInfo, Attacking, Gliding, Jumping, OnGround},
+    state::DeltaTime,
+};
+use specs::{Entities, Join, Read, ReadStorage, System, WriteStorage};
 
 // Basic ECS AI agent system
 pub struct Sys;
 
 impl<'a> System<'a> for Sys {
-    type SystemData = (Read<'a, DeltaTime>, WriteStorage<'a, AnimationInfo>);
+    type SystemData = (
+        Entities<'a>,
+        Read<'a, DeltaTime>,
+        ReadStorage<'a, phys::Vel>,
+        ReadStorage<'a, OnGround>,
+        ReadStorage<'a, Jumping>,
+        ReadStorage<'a, Gliding>,
+        ReadStorage<'a, Attacking>,
+        WriteStorage<'a, AnimationInfo>,
+    );
 
-    fn run(&mut self, (dt, mut animation_infos): Self::SystemData) {
-        for mut animation_info in (&mut animation_infos).join() {
+    fn run(
+        &mut self,
+        (entities, dt, velocities, on_grounds, jumpings, glidings, attackings, mut animation_infos): Self::SystemData,
+    ) {
+        for (entity, vel, on_ground, jumping, gliding, attacking, mut animation_info) in (
+            &entities,
+            &velocities,
+            on_grounds.maybe(),
+            jumpings.maybe(),
+            glidings.maybe(),
+            attackings.maybe(),
+            &mut animation_infos,
+        )
+            .join()
+        {
             animation_info.time += dt.0 as f64;
+
+            let animation = if on_ground.is_some() {
+                if vel.0.magnitude() > 3.0 {
+                    Animation::Run
+                } else if attacking.is_some() {
+                    Animation::Attack
+                } else {
+                    Animation::Idle
+                }
+            } else if gliding.is_some() {
+                Animation::Gliding
+            } else {
+                Animation::Jump
+            };
+
+            let last = animation_info.clone();
+            let changed = last.animation != animation;
+
+            *animation_info = AnimationInfo {
+                animation,
+                time: if changed { 0.0 } else { last.time },
+                changed,
+            };
         }
     }
 }
--- common/src/sys/phys.rs
+++ common/src/sys/phys.rs
@@ -1,17 +1,14 @@
 use crate::{
     comp::{
         phys::{Pos, Vel},
-        Stats,
+        OnGround, Stats,
     },
     state::DeltaTime,
     terrain::TerrainMap,
     vol::{ReadVol, Vox},
 };
-use specs::{Join, Read, ReadExpect, ReadStorage, System, WriteStorage};
+use specs::{Entities, Join, Read, ReadExpect, ReadStorage, System, WriteStorage};
 use vek::*;
-
-// Basic ECS physics system
-pub struct Sys;
 
 const GRAVITY: f32 = 9.81 * 4.0;
 const FRIC_GROUND: f32 = 0.15;
@@ -38,34 +35,53 @@ fn integrate_forces(dt: f32, mut lv: Vec3<f32>, damp: f32) -> Vec3<f32> {
     lv
 }
 
+/// This system applies forces and calculates new positions and velocities
+pub struct Sys;
 impl<'a> System<'a> for Sys {
     type SystemData = (
+        Entities<'a>,
         ReadExpect<'a, TerrainMap>,
         Read<'a, DeltaTime>,
         ReadStorage<'a, Stats>,
         WriteStorage<'a, Pos>,
         WriteStorage<'a, Vel>,
+        WriteStorage<'a, OnGround>,
     );
 
-    fn run(&mut self, (terrain, dt, stats, mut positions, mut velocities): Self::SystemData) {
-        for (stats, pos, vel) in (&stats, &mut positions, &mut velocities).join() {
+    fn run(
+        &mut self,
+        (entities, terrain, dt, stats, mut positions, mut velocities, mut on_grounds): Self::SystemData,
+    ) {
+        for (entity, stats, pos, vel) in (&entities, &stats, &mut positions, &mut velocities).join()
+        {
             // Disable while dead TODO: Replace with client states
             if stats.is_dead {
                 continue;
             }
 
-            let on_ground = terrain
-                .get((pos.0 - Vec3::unit_z() * 0.1).map(|e| e.floor() as i32))
-                .map(|vox| !vox.is_empty())
-                .unwrap_or(false)
-                && vel.0.z <= 0.0;
-
             // Movement
             pos.0 += vel.0 * dt.0;
 
+            // Update OnGround component
+            if terrain
+                .get((pos.0 - Vec3::unit_z() * 0.1).map(|e| e.floor() as i32))
+                .map(|vox| !vox.is_empty())
+                .unwrap_or(false)
+                && vel.0.z <= 0.0
+            {
+                on_grounds.insert(entity, OnGround);
+            } else {
+                on_grounds.remove(entity);
+            }
+
             // Integrate forces
             // Friction is assumed to be a constant dependent on location
-            let friction = 50.0 * if on_ground { FRIC_GROUND } else { FRIC_AIR };
+            let friction = 50.0
+                * if on_grounds.get(entity).is_some() {
+                    FRIC_GROUND
+                } else {
+                    FRIC_AIR
+                };
             vel.0 = integrate_forces(dt.0, vel.0, friction);
 
             // Basic collision with terrain
