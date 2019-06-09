69cb2ed84f794bbbba1eb176e3e66096e69d7a75
timokoesters <timo@koesters.xyz>
2019-06-09 16:20:20 +0200

    Revamp control system

--- common/src/sys/inputs.rs
+++ common/src/sys/inputs.rs
@@ -1,7 +1,7 @@
 use crate::{
     comp::{
         phys::{ForceUpdate, Ori, Pos, Vel},
-        Animation, AnimationInfo, Attacking, Control, Gliding, HealthSource, Jumping, Stats,
+        Animation, AnimationInfo, Attacking, Controller, Gliding, HealthSource, Jumping, Stats,
     },
     state::{DeltaTime, Uid},
     terrain::TerrainMap,
@@ -13,17 +13,6 @@ use vek::*;
 
 // Basic ECS AI agent system
 pub struct Sys;
-
-const HUMANOID_ACCEL: f32 = 100.0;
-const HUMANOID_SPEED: f32 = 500.0;
-const HUMANOID_AIR_ACCEL: f32 = 10.0;
-const HUMANOID_AIR_SPEED: f32 = 100.0;
-const HUMANOID_JUMP_ACCEL: f32 = 16.0;
-const GLIDE_ACCEL: f32 = 15.0;
-const GLIDE_SPEED: f32 = 45.0;
-// Gravity is 9.81 * 4, so this makes gravity equal to .15
-const GLIDE_ANTIGRAV: f32 = 9.81 * 3.95;
-
 impl<'a> System<'a> for Sys {
     type SystemData = (
         Entities<'a>,
@@ -35,7 +24,7 @@ impl<'a> System<'a> for Sys {
         WriteStorage<'a, Ori>,
         WriteStorage<'a, AnimationInfo>,
         WriteStorage<'a, Stats>,
-        ReadStorage<'a, Control>,
+        ReadStorage<'a, Controller>,
         WriteStorage<'a, Jumping>,
         WriteStorage<'a, Gliding>,
         WriteStorage<'a, Attacking>,
@@ -54,74 +43,38 @@ impl<'a> System<'a> for Sys {
             mut orientations,
             mut animation_infos,
             mut stats,
-            controls,
+            controllers,
             mut jumps,
             glides,
             mut attacks,
             mut force_updates,
         ): Self::SystemData,
     ) {
-        for (entity, pos, control, stats, mut ori, mut vel) in (
+        for (entity, pos, controller, stats, mut ori, mut vel) in (
             &entities,
             &positions,
-            &controls,
+            &controllers,
             &stats,
             &mut orientations,
             &mut velocities,
         )
             .join()
         {
-            // Disable while dead TODO: Replace with client states
-            if stats.is_dead {
-                continue;
-            }
-
             let on_ground = terrain
                 .get((pos.0 - Vec3::unit_z() * 0.1).map(|e| e.floor() as i32))
                 .map(|vox| !vox.is_empty())
                 .unwrap_or(false)
                 && vel.0.z <= 0.0;
 
-            let gliding = glides.get(entity).is_some() && vel.0.z < 0.0;
-            let move_dir = if control.move_dir.magnitude() > 1.0 {
-                control.move_dir.normalized()
-            } else {
-                control.move_dir
-            };
-
-            if on_ground {
-                // Move player according to move_dir
-                if vel.0.magnitude() < HUMANOID_SPEED {
-                    vel.0 += Vec2::broadcast(dt.0) * move_dir * HUMANOID_ACCEL;
-                }
-
-                // Jump
-                if jumps.get(entity).is_some() && vel.0.z <= 0.0 {
-                    vel.0.z = HUMANOID_JUMP_ACCEL;
-                    jumps.remove(entity);
-                }
-            } else if gliding && vel.0.magnitude() < GLIDE_SPEED {
-                let anti_grav = GLIDE_ANTIGRAV + vel.0.z.powf(2.0) * 0.2;
-                vel.0.z += dt.0 * anti_grav * Vec2::<f32>::from(vel.0 * 0.15).magnitude().min(1.0);
-                vel.0 += Vec2::broadcast(dt.0) * move_dir * GLIDE_ACCEL;
-            } else if vel.0.magnitude() < HUMANOID_AIR_SPEED {
-                vel.0 += Vec2::broadcast(dt.0) * move_dir * HUMANOID_AIR_ACCEL;
-            }
-
-            // Set direction based on velocity
-            if vel.0.magnitude_squared() != 0.0 {
-                ori.0 = vel.0.normalized() * Vec3::new(1.0, 1.0, 0.0);
-            }
-
             let animation = if on_ground {
-                if control.move_dir.magnitude() > 0.01 {
+                if controller.move_dir.magnitude() > 0.01 {
                     Animation::Run
                 } else if attacks.get(entity).is_some() {
                     Animation::Attack
                 } else {
                     Animation::Idle
                 }
-            } else if glides.get(entity).is_some() {
+            } else if controller.glide {
                 Animation::Gliding
             } else {
                 Animation::Jump
--- common/src/sys/mod.rs
+++ common/src/sys/mod.rs
@@ -1,6 +1,7 @@
 pub mod actions;
 pub mod agent;
 pub mod animation;
+pub mod controller;
 pub mod inputs;
 pub mod phys;
 mod stats;
@@ -10,6 +11,7 @@ use specs::DispatcherBuilder;
 
 // System names
 const AGENT_SYS: &str = "agent_sys";
+const CONTROLLER_SYS: &str = "controller_sys";
 const INPUTS_SYS: &str = "inputs_sys";
 const ACTIONS_SYS: &str = "actions_sys";
 const PHYS_SYS: &str = "phys_sys";
@@ -20,6 +22,7 @@ pub fn add_local_systems(dispatch_builder: &mut DispatcherBuilder) {
     dispatch_builder.add(agent::Sys, AGENT_SYS, &[]);
     dispatch_builder.add(phys::Sys, PHYS_SYS, &[]);
     dispatch_builder.add(actions::Sys, ACTIONS_SYS, &[]);
+    dispatch_builder.add(controller::Sys, CONTROLLER_SYS, &[]);
     dispatch_builder.add(inputs::Sys, INPUTS_SYS, &[]);
     dispatch_builder.add(animation::Sys, ANIMATION_SYS, &[]);
     dispatch_builder.add(stats::Sys, STATS_SYS, &[INPUTS_SYS]);
