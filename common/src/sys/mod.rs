244b27cb29417ab4efc1c672c40132082847b26f
Cody <Mason.Cody.M@gmail.com>
2019-06-11 15:28:25 -0400

    Removes input sys, moves code to combat and phys

--- common/src/sys/mod.rs
+++ common/src/sys/mod.rs
@@ -1,7 +1,7 @@
 pub mod agent;
 pub mod animation;
+pub mod combat;
 pub mod controller;
-pub mod inputs;
 pub mod phys;
 mod stats;
 
@@ -12,7 +12,7 @@ use specs::DispatcherBuilder;
 const AGENT_SYS: &str = "agent_sys";
 const CONTROLLER_SYS: &str = "controller_sys";
 const PHYS_SYS: &str = "phys_sys";
-const INPUTS_SYS: &str = "inputs_sys";
+const COMBAT_SYS: &str = "combat_sys";
 const ANIMATION_SYS: &str = "animation_sys";
 const STATS_SYS: &str = "stats_sys";
 
@@ -20,7 +20,7 @@ pub fn add_local_systems(dispatch_builder: &mut DispatcherBuilder) {
     dispatch_builder.add(agent::Sys, AGENT_SYS, &[]);
     dispatch_builder.add(controller::Sys, CONTROLLER_SYS, &[]);
     dispatch_builder.add(phys::Sys, PHYS_SYS, &[]);
-    dispatch_builder.add(inputs::Sys, INPUTS_SYS, &[]);
+    dispatch_builder.add(combat::Sys, COMBAT_SYS, &[]);
     dispatch_builder.add(animation::Sys, ANIMATION_SYS, &[]);
-    dispatch_builder.add(stats::Sys, STATS_SYS, &[INPUTS_SYS]);
+    dispatch_builder.add(stats::Sys, STATS_SYS, &[COMBAT_SYS]);
 }
