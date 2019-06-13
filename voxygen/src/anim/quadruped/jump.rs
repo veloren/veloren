99b4406d79f4fd39be10acf3bfafed87c5f3e554
jshipsey <jshipsey18@gmail.com>
2019-06-13 00:14:06 -0400

    cleared out unused animation variables

--- voxygen/src/anim/quadruped/jump.rs
+++ voxygen/src/anim/quadruped/jump.rs
@@ -15,13 +15,7 @@ impl Animation for JumpAnimation {
     ) -> Self::Skeleton {
         let mut next = (*skeleton).clone();
 
-        let wave = (anim_time as f32 * 14.0).sin();
-        let wave_test = wave.cbrt();
-        let fuzz_wave = (anim_time as f32 * 12.0).sin();
-        let wave_cos = (anim_time as f32 * 14.0).cos();
         let wave_slow = (anim_time as f32 * 7.0 + PI).sin();
-        let wave_slow_cos = (anim_time as f32 * 8.0 + PI).cos();
-        let wave_dip = (wave_slow.abs() - 0.5).abs();
         let wave_stop = (anim_time as f32 * 4.5).min(PI / 2.0).sin();
 
         next.pig_head.offset = Vec3::new(0.0, 0.0, -1.5) / 11.0;
--- voxygen/src/anim/quadrupedmedium/run.rs
+++ voxygen/src/anim/quadrupedmedium/run.rs
@@ -16,16 +16,11 @@ impl Animation for RunAnimation {
         let mut next = (*skeleton).clone();
 
         let wave = (anim_time as f32 * 14.0).sin();
-        let wave_ultra_slow = (anim_time as f32 * 1.0 + PI).sin();
-        let wave_ultra_slow_cos = (anim_time as f32 * 1.0 + PI).cos();
-        let wave_cos = (anim_time as f32 * 14.0).cos();
         let wave_slow = (anim_time as f32 * 3.5 + PI).sin();
         let wave_slow_cos = (anim_time as f32 * 3.5 + PI).cos();
-        let wave_dip = (wave_slow.abs() - 0.5).abs();
         let wave_quick = (anim_time as f32 * 18.0).sin();
         let wave_med = (anim_time as f32 * 12.0).sin();
         let wave_med_cos = (anim_time as f32 * 12.0).cos();
-
         let wave_quick_cos = (anim_time as f32 * 18.0).cos();
 
         let wolf_look = Vec2::new(
