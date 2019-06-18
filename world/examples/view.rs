716dfd2e0d45163f6a2babbb452643e541d19301
Joshua Barretto <joshua.s.barretto@gmail.com>
2019-06-18 22:22:31 +0100

    Basic region generation

--- world/examples/view.rs
+++ world/examples/view.rs
@@ -15,41 +15,56 @@ fn main() {
 
     let mut focus = Vec2::zero();
     let mut gain = 1.0;
+    let mut scale = 4;
 
     while win.is_open() {
         let mut buf = vec![0; W * H];
 
         for i in 0..W {
             for j in 0..H {
-                let pos = focus + Vec2::new(i as i32, j as i32) * 4;
+                let pos = focus + Vec2::new(i as i32, j as i32) * scale;
 
-                let alt = sampler
+                let (alt, location) = sampler
                     .get(pos)
-                    .map(|sample| sample.alt.sub(64.0).add(gain).mul(0.7).max(0.0).min(255.0) as u8)
-                    .unwrap_or(0);
+                    .map(|sample| (
+                        sample.alt.sub(64.0).add(gain).mul(0.7).max(0.0).min(255.0) as u8,
+                        sample.location,
+                    ))
+                    .unwrap_or((0, None));
 
-                buf[j * W + i] = u32::from_le_bytes([alt; 4]);
+                let loc_color = location.map(|l| (
+                    l.name().bytes().nth(0).unwrap() * 17,
+                    l.name().bytes().nth(1).unwrap() * 17,
+                )).unwrap_or((0, 0));
+
+                buf[j * W + i] = u32::from_le_bytes([loc_color.0, loc_color.1, alt, alt]);
             }
         }
 
         let spd = 32;
         if win.is_key_down(minifb::Key::W) {
-            focus.y -= spd;
+            focus.y -= spd * scale;
         }
         if win.is_key_down(minifb::Key::A) {
-            focus.x -= spd;
+            focus.x -= spd * scale;
         }
         if win.is_key_down(minifb::Key::S) {
-            focus.y += spd;
+            focus.y += spd * scale;
         }
         if win.is_key_down(minifb::Key::D) {
-            focus.x += spd;
+            focus.x += spd * scale;
         }
         if win.is_key_down(minifb::Key::Q) {
             gain += 10.0;
         }
         if win.is_key_down(minifb::Key::E) {
             gain -= 10.0;
+        }
+        if win.is_key_down(minifb::Key::R) {
+            scale += 1;
+        }
+        if win.is_key_down(minifb::Key::F) {
+            scale -= 1;
         }
 
         win.update_with_buffer(&buf).unwrap();
