1bea8fe69de80085a57db553e1bee1b24677bfa8
Cody <Mason.Cody.M@gmail.com>
2019-06-22 10:57:48 -0400

    Renames `phys` to `physics`.

--- common/src/comp/mod.rs
+++ common/src/comp/mod.rs
@@ -3,7 +3,7 @@ mod agent;
 mod animation;
 mod controller;
 mod inputs;
-mod phys;
+mod physics;
 mod player;
 mod stats;
 
@@ -13,6 +13,6 @@ pub use agent::Agent;
 pub use animation::{Animation, AnimationInfo};
 pub use controller::Controller;
 pub use inputs::{Attacking, Gliding, Jumping, MoveDir, OnGround, Respawning, Rolling};
-pub use phys::{Acceleration, ForceUpdate, Ori, Pos, Position, Vel, Velocity};
+pub use physics::{Acceleration, ForceUpdate, Ori, Pos, Position, Vel, Velocity};
 pub use player::Player;
 pub use stats::{Dying, HealthSource, Stats};
--- common/src/sys/mod.rs
+++ common/src/sys/mod.rs
@@ -2,7 +2,7 @@ pub mod agent;
 pub mod animation;
 pub mod combat;
 pub mod controller;
-pub mod phys;
+pub mod physics;
 mod stats;
 
 // External
@@ -11,7 +11,7 @@ use specs::DispatcherBuilder;
 // System names
 const AGENT_SYS: &str = "agent_sys";
 const CONTROLLER_SYS: &str = "controller_sys";
-const PHYS_SYS: &str = "phys_sys";
+const PHYSICS_SYS: &str = "physics_sys";
 const COMBAT_SYS: &str = "combat_sys";
 const ANIMATION_SYS: &str = "animation_sys";
 const STATS_SYS: &str = "stats_sys";
@@ -19,7 +19,7 @@ const STATS_SYS: &str = "stats_sys";
 pub fn add_local_systems(dispatch_builder: &mut DispatcherBuilder) {
     dispatch_builder.add(agent::Sys, AGENT_SYS, &[]);
     dispatch_builder.add(controller::Sys, CONTROLLER_SYS, &[]);
-    dispatch_builder.add(phys::Sys, PHYS_SYS, &[]);
+    dispatch_builder.add(physics::Sys, PHYSICS_SYS, &[]);
     dispatch_builder.add(combat::Sys, COMBAT_SYS, &[]);
     dispatch_builder.add(animation::Sys, ANIMATION_SYS, &[]);
     dispatch_builder.add(stats::Sys, STATS_SYS, &[COMBAT_SYS]);
