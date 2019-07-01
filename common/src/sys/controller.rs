21cd028424908fde259a3462186d8fff0c152664
timokoesters <timo@koesters.xyz>
2019-07-01 22:42:43 +0200

    Fix warnings and clippy recommendations in common

--- common/src/sys/controller.rs
+++ common/src/sys/controller.rs
@@ -1,23 +1,16 @@
-use crate::{
-    comp::{
-        ActionState, Animation, AnimationInfo, Attacking, Controller, Gliding, HealthSource,
-        Jumping, MoveDir, OnGround, Respawning, Rolling, Stats, {ForceUpdate, Ori, Pos, Vel},
-    },
-    state::DeltaTime,
+use crate::comp::{
+    ActionState, Attacking, Controller, Gliding, Jumping, MoveDir, Respawning, Rolling, Stats, Vel,
 };
-use specs::{Entities, Join, Read, ReadStorage, System, WriteStorage};
+use specs::{Entities, Join, ReadStorage, System, WriteStorage};
 
 /// This system is responsible for validating controller inputs
 pub struct Sys;
 impl<'a> System<'a> for Sys {
     type SystemData = (
         Entities<'a>,
-        Read<'a, DeltaTime>,
         ReadStorage<'a, Controller>,
         ReadStorage<'a, Stats>,
-        ReadStorage<'a, Pos>,
         ReadStorage<'a, Vel>,
-        ReadStorage<'a, Ori>,
         WriteStorage<'a, ActionState>,
         WriteStorage<'a, MoveDir>,
         WriteStorage<'a, Jumping>,
@@ -31,12 +24,9 @@ impl<'a> System<'a> for Sys {
         &mut self,
         (
             entities,
-            dt,
             controllers,
             stats,
-            positions,
             velocities,
-            orientations,
             mut action_states,
             mut move_dirs,
             mut jumpings,
@@ -46,13 +36,11 @@ impl<'a> System<'a> for Sys {
             mut glidings,
         ): Self::SystemData,
     ) {
-        for (entity, controller, stats, pos, vel, ori, mut a) in (
+        for (entity, controller, stats, vel, mut a) in (
             &entities,
             &controllers,
             &stats,
-            &positions,
             &velocities,
-            &orientations,
             // Although this is changed, it is only kept for this system
             // as it will be replaced in the action state system
             &mut action_states,
@@ -62,14 +50,14 @@ impl<'a> System<'a> for Sys {
             if stats.is_dead {
                 // Respawn
                 if controller.respawn {
-                    respawns.insert(entity, Respawning);
+                    let _ = respawns.insert(entity, Respawning);
                 }
                 continue;
             }
 
             // Move dir
             if !a.rolling {
-                move_dirs.insert(
+                let _ = move_dirs.insert(
                     entity,
                     MoveDir(if controller.move_dir.magnitude_squared() > 1.0 {
                         controller.move_dir.normalized()
@@ -81,16 +69,16 @@ impl<'a> System<'a> for Sys {
 
             // Glide
             if controller.glide && !a.on_ground && !a.attacking && !a.rolling {
-                glidings.insert(entity, Gliding);
+                let _ = glidings.insert(entity, Gliding);
                 a.gliding = true;
             } else {
-                glidings.remove(entity);
+                let _ = glidings.remove(entity);
                 a.gliding = false;
             }
 
             // Attack
             if controller.attack && !a.attacking && !a.gliding && !a.rolling {
-                attackings.insert(entity, Attacking::start());
+                let _ = attackings.insert(entity, Attacking::start());
                 a.attacking = true;
             }
 
@@ -102,13 +90,13 @@ impl<'a> System<'a> for Sys {
                 && !a.attacking
                 && !a.gliding
             {
-                rollings.insert(entity, Rolling::start());
+                let _ = rollings.insert(entity, Rolling::start());
                 a.rolling = true;
             }
 
             // Jump
             if controller.jump && a.on_ground && vel.0.z <= 0.0 {
-                jumpings.insert(entity, Jumping);
+                let _ = jumpings.insert(entity, Jumping);
                 a.on_ground = false;
             }
         }
