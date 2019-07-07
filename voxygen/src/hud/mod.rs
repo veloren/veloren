7f45d274a8eec004bb91856a91e9c03fceba55c9
Pfauenauge90 <44173739+Pfauenauge90@users.noreply.github.com>
2019-07-07 16:06:27 +0200

    clean up

--- voxygen/src/hud/mod.rs
+++ voxygen/src/hud/mod.rs
@@ -286,8 +286,7 @@ impl Hud {
             ui,
             imgs,
             fonts,
-            ids,
-            ui_scale,
+            ids,            
             new_messages: VecDeque::new(),
             inventory_space: 8,
             show: Show {
