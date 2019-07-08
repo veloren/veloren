56b8201a37feb10cda8d092fdb9769c9af4824db
Joshua Barretto <joshua.s.barretto@gmail.com>
2019-07-09 00:35:59 +0100

    Smoothed temperate/savannah border

--- world/src/column/mod.rs
+++ world/src/column/mod.rs
@@ -131,7 +131,7 @@ impl<'a> Sampler for ColumnGen<'a> {
                     .mul(256.0),
             ),
             Rgb::lerp(tropical, sand, temp.sub(CONFIG.desert_temp).mul(32.0)),
-            temp.sub(CONFIG.tropical_temp).mul(64.0),
+            temp.sub(CONFIG.tropical_temp).mul(16.0),
         );
 
         // Work out if we're on a path or near a town
