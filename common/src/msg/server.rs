dc28070640975b2e7d5e8dfeffe8b64c35ade448
tommy <coder@tommywatson.com>
2019-07-17 18:10:42 -0400

    Adds colour to tells in chat box #194

--- common/src/msg/server.rs
+++ common/src/msg/server.rs
@@ -1,9 +1,5 @@
 use super::{ClientState, EcsCompPacket, EcsResPacket};
-use crate::{
-    comp,
-    terrain::{Block, TerrainChunk},
-};
-use fxhash::FxHashMap;
+use crate::{comp, terrain::TerrainChunk, ChatType};
 use vek::*;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -32,7 +28,10 @@ pub enum ServerMsg {
     ForceState(ClientState),
     Ping,
     Pong,
-    Chat(String),
+    ChatMsg {
+        chat_type: ChatType,
+        msg: String,
+    },
     SetPlayerEntity(u64),
     EcsSync(sphynx::SyncPackage<EcsCompPacket, EcsResPacket>),
     EntityPhysics {
@@ -56,4 +55,19 @@ pub enum ServerMsg {
 pub enum ServerError {
     TooManyPlayers,
     //TODO: InvalidAlias,
+}
+
+impl ServerMsg {
+    pub fn chat(message: String) -> crate::msg::server::ServerMsg {
+        crate::msg::server::ServerMsg::ChatMsg {
+            chat_type: ChatType::Chat,
+            msg: message,
+        }
+    }
+    pub fn tell(message: String) -> crate::msg::server::ServerMsg {
+        crate::msg::server::ServerMsg::ChatMsg {
+            chat_type: ChatType::Tell,
+            msg: message,
+        }
+    }
 }
