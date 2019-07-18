74a020cb921572cec331e42e08d7e5c7d5d80b1b
Joshua Barretto <joshua.s.barretto@gmail.com>
2019-07-18 16:42:31 +0100

    Switched to Debug trait object for backend errors

--- voxygen/src/error.rs
+++ voxygen/src/error.rs
@@ -1,6 +1,6 @@
 use crate::render::RenderError;
 use client;
-use std::any;
+use std::fmt::Debug;
 
 /// Represents any error that may be triggered by Voxygen.
 #[derive(Debug)]
@@ -8,7 +8,7 @@ pub enum Error {
     /// An error relating to the internal client.
     ClientError(client::Error),
     /// A miscellaneous error relating to a backend dependency.
-    BackendError(Box<dyn any::Any>),
+    BackendError(Box<dyn Debug>),
     /// An error relating the rendering subsystem.
     RenderError(RenderError),
     /// A miscellaneous error with an unknown or unspecified source.
