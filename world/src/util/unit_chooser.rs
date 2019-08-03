f14674ee9ae2e7a27f142d66d66a21d3aba4459a
Joshua Barretto <joshua.s.barretto@gmail.com>
2019-08-03 22:11:31 +0100

    Switched to UnitChooser

---
+++ world/src/util/unit_chooser.rs
@@ -0,0 +1,34 @@
+use super::{RandomPerm, Sampler};
+use vek::*;
+
+const UNIT_CHOICES: [(Vec2<i32>, Vec2<i32>); 8] = [
+    (Vec2 { x: 1, y: 0 }, Vec2 { x: 0, y: 1 }),
+    (Vec2 { x: 1, y: 0 }, Vec2 { x: 0, y: -1 }),
+    (Vec2 { x: -1, y: 0 }, Vec2 { x: 0, y: 1 }),
+    (Vec2 { x: -1, y: 0 }, Vec2 { x: 0, y: -1 }),
+    (Vec2 { x: 0, y: 1 }, Vec2 { x: 1, y: 0 }),
+    (Vec2 { x: 0, y: 1 }, Vec2 { x: -1, y: 0 }),
+    (Vec2 { x: 0, y: -1 }, Vec2 { x: 1, y: 0 }),
+    (Vec2 { x: 0, y: -1 }, Vec2 { x: -1, y: 0 }),
+];
+
+pub struct UnitChooser {
+    perm: RandomPerm,
+}
+
+impl UnitChooser {
+    pub const fn new(seed: u32) -> Self {
+        Self {
+            perm: RandomPerm::new(seed),
+        }
+    }
+}
+
+impl Sampler for UnitChooser {
+    type Index = u32;
+    type Sample = (Vec2<i32>, Vec2<i32>);
+
+    fn get(&self, perm: Self::Index) -> Self::Sample {
+        UNIT_CHOICES[self.perm.get(perm) as usize % UNIT_CHOICES.len()]
+    }
+}
