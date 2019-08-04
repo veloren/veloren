e5bcc8912e8311bb4e3683d6880b1f94b6ca90e1
Joshua Barretto <joshua.s.barretto@gmail.com>
2019-08-04 15:30:45 +0100

    Stopped stars appearing in the day

--- voxygen/shaders/include/sky.glsl
+++ voxygen/shaders/include/sky.glsl
@@ -97,12 +97,12 @@ vec3 get_sky_color(vec3 dir, float time_of_day) {
 	vec3 sky_top = mix(
 		mix(
 			SKY_DUSK_TOP,
-			SKY_NIGHT_TOP,
+			SKY_NIGHT_TOP + star,
 			max(sun_dir.z, 0)
 		),
 		SKY_DAY_TOP,
 		max(-sun_dir.z, 0)
-	) + star;
+	);
 
 	vec3 sky_mid = mix(
 		mix(
