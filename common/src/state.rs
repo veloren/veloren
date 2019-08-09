99275ca4e72443ad79f14e9a4e282b1ba7c6ce47
Piotr Korgól <kpiotr2005@gmail.com>
2019-08-09 16:22:59 +0200

    Add AdminPerms component, make some commands require it

--- common/src/state.rs
+++ common/src/state.rs
@@ -145,6 +145,7 @@ impl State {
         ecs.register::<comp::ForceUpdate>();
         ecs.register::<comp::InventoryUpdate>();
         ecs.register::<comp::Inventory>();
+        ecs.register::<comp::AdminPerms>();
         // Controller effects
         ecs.register::<comp::MoveDir>();
         ecs.register::<comp::OnGround>();
