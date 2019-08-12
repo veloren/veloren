6a477d6b695de24500df29d04bfd719cc6ad7dcd
Imbris <imbrisf@gmail.com>
2019-08-11 22:45:57 -0400

    Tweak lighting

--- assets/voxygen/shaders/figure/frag.glsl
+++ assets/voxygen/shaders/figure/frag.glsl
@@ -29,7 +29,7 @@ out vec4 tgt_color;
 
 void main() {
 	vec3 light = get_sun_diffuse(f_norm, time_of_day.x) + light_at(f_pos, f_norm);
-	vec3 surf_color = srgb_to_linear(model_col.rgb * f_col) * 2.0 * light;
+	vec3 surf_color = srgb_to_linear(model_col.rgb * f_col) * 4.0 * light;
 
 	float fog_level = fog(f_pos.xy, focus_pos.xy);
 	vec3 fog_color = get_sky_color(normalize(f_pos - cam_pos.xyz), time_of_day.x);
