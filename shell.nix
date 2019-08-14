aa765814c7fbce1d3825816f2af52bff6f764c9d
haslersn <sebastian.hasler@gmx.net>
2019-08-14 15:12:54 +0200

    default.nix, shell.nix: init
    This commit inits the `default.nix` and `shell.nix`. They support
    `nix build` and `nix-shell`, respectively.
    
    * `nix-shell` sets up the build environment for Veloren so
      that you can manually build it using `cargo build`. This
      is for developping as it speeds up subsequent compilations
      because intermediate compilation artifacts are reused.
    * `nix build` builds Veloren, stores the result in the Nix
      store and creates a `result` symlink to it. Make sure that
      the LFS files are checked out before using `nix build`.

---
+++ shell.nix
@@ -0,0 +1,28 @@
+with import <nixpkgs> {};
+
+import ./default.nix {
+  # The source is copied to the nix store. We don't want to do this (including assets) for every
+  # time the `nix-shell` is entered. Therefore we create a source which contains only the files
+  # necessary to evaluate `buildRustPackage` successfully:
+  veloren-src = runCommand "veloren-shell" {} (lib.concatMapStrings
+    (p: "mkdir -p $(dirname $out/${p}); cp ${./. + "/${p}"} $out/${p}\n")
+    [
+      "Cargo.lock"
+      "Cargo.toml"
+      "chat-cli/Cargo.toml"
+      "chat-cli/src/main.rs"
+      "client/Cargo.toml"
+      "client/src/lib.rs"
+      "common/Cargo.toml"
+      "common/src/lib.rs"
+      "server-cli/Cargo.toml"
+      "server-cli/src/main.rs"
+      "server/Cargo.toml"
+      "server/src/lib.rs"
+      "voxygen/Cargo.toml"
+      "voxygen/src/main.rs"
+      "world/Cargo.toml"
+      "world/src/lib.rs"
+    ]
+  );
+}
