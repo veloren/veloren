15598b0c3771a0d2b2f2f934fb73f5c09255c004
Joshua Barretto <joshua.s.barretto@gmail.com>
2019-08-20 10:59:15 +0100

    Patchier plants, wind swaying

--- assets/voxygen/shaders/sprite-vert.glsl
+++ assets/voxygen/shaders/sprite-vert.glsl
@@ -19,6 +19,9 @@ const float SCALE = 1.0 / 11.0;
 void main() {
 	f_pos = inst_pos + v_pos * SCALE;
 
+	// Wind waving
+	f_pos += vec3(sin(tick.x * 1.7 + inst_pos.y * 0.2), sin(tick.x * 1.3 + inst_pos.x * 0.2), 0) * sin(v_pos.z * 0.03) * 0.5;
+
 	f_norm = v_norm;
 
 	f_col = v_col * inst_col;
