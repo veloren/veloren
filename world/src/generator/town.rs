637e16c23fd51fc5afcff02b1668ecff71cb23bd
Joshua Barretto <joshua.s.barretto@gmail.com>
2019-08-23 22:33:14 +0100

    Added test generator

--- world/src/generator/town.rs
+++ world/src/generator/town.rs
@@ -1,18 +1,23 @@
 use super::Generator;
 use crate::util::Sampler;
-use common::terrain::Block;
+use common::terrain::{Block, BlockKind};
 use vek::*;
 
+#[derive(Clone)]
 pub struct TownState;
 
 pub struct TownGen;
 
 impl<'a> Sampler<'a> for TownGen {
     type Index = (&'a TownState, Vec3<i32>);
-    type Sample = Block;
+    type Sample = Option<Block>;
 
     fn get(&self, (town, pos): Self::Index) -> Self::Sample {
-        unimplemented!()
+        if pos.z < 150 {
+            Some(Block::new(BlockKind::Normal, Rgb::broadcast(255)))
+        } else {
+            None
+        }
     }
 }
 
