2dfe94bf0b3d08c940a6bd8bdf1829c1124a078a
timokoesters <timo@koesters.xyz>
2019-08-24 18:38:59 +0200

    Block when pressing right click while looking at the attacker

--- common/src/comp/controller.rs
+++ common/src/comp/controller.rs
@@ -7,6 +7,7 @@ pub struct Controller {
     pub move_dir: Vec2<f32>,
     pub jump: bool,
     pub attack: bool,
+    pub block: bool,
     pub roll: bool,
     pub glide: bool,
     pub respawn: bool,
