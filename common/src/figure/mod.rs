7bebffb2afd85b877c93ddfddaa04dd974e3ff7a
Imbris <imbrisf@gmail.com>
2019-08-24 19:18:47 -0400

    Add ron file for head offsets, segment unionizer

--- common/src/figure/mod.rs
+++ common/src/figure/mod.rs
@@ -47,10 +47,11 @@ impl From<&DotVoxData> for Segment {
 }
 
 impl Segment {
+    // TODO: this is currenlty unused, remove?
     /// Create a new `Segment` by combining two existing ones
     pub fn union(&self, other: &Self, other_offset: Vec3<i32>) -> Self {
         let size = self.get_size();
-        let other_size = self.get_size();
+        let other_size = other.get_size();
         let new_size = other_offset
             .map2(other_size, |oo, os| (oo, os))
             .map2(size, |(oo, os), s| {
@@ -86,6 +87,7 @@ impl Segment {
         self
     }
     /// Preserve the luminance of all the colors but set the chomaticity to match the provided color
+    // TODO add more advanced recoloring and/or indexed based coloring
     pub fn chromify(mut self, chroma: Rgb<u8>) -> Self {
         let chroma = chroma.map(|e| e as f32 * 255.0);
         for pos in self.iter_positions() {
@@ -105,5 +107,57 @@ impl Segment {
         }
 
         self
+    }
+}
+
+/// A `Segment` builder that combines segments
+pub struct SegmentUnionizer(Vec<(Segment, Vec3<i32>)>);
+
+impl SegmentUnionizer {
+    pub fn new() -> Self {
+        SegmentUnionizer(Vec::new())
+    }
+    pub fn add(mut self, segment: Segment, offset: Vec3<i32>) -> Self {
+        self.0.push((segment, offset));
+        self
+    }
+    pub fn maybe_add(self, maybe: Option<(Segment, Vec3<i32>)>) -> Self {
+        match maybe {
+            Some((segment, offset)) => self.add(segment, offset),
+            None => self,
+        }
+    }
+    pub fn unify(self) -> (Segment, Vec3<i32>) {
+        if self.0.is_empty() {
+            return (
+                Segment::filled(Vec3::new(0, 0, 0), Cell::empty(), ()),
+                Vec3::new(0, 0, 0),
+            );
+        }
+
+        // Determine size of the new segment
+        let mut min_point = self.0[0].1;
+        let mut max_point = self.0[0].1 + self.0[0].0.get_size().map(|e| e as i32);
+        for (segment, offset) in self.0.iter().skip(1) {
+            let size = segment.get_size().map(|e| e as i32);
+            min_point = min_point.map2(*offset, std::cmp::min);
+            max_point = max_point.map2(offset + size, std::cmp::max);
+        }
+        let new_size = (max_point - min_point).map(|e| e as u32);
+        // Allocate new segment
+        let mut combined = Segment::filled(new_size, Cell::empty(), ());
+        // Copy segments into combined
+        let origin = min_point.map(|e| e * -1);
+        for (segment, offset) in self.0 {
+            for pos in segment.iter_positions() {
+                if let Cell::Filled(col) = *segment.get(pos).unwrap() {
+                    combined
+                        .set(origin + offset + pos, Cell::Filled(col))
+                        .unwrap();
+                }
+            }
+        }
+
+        (combined, origin)
     }
 }
--- voxygen/src/scene/figure/cache.rs
+++ voxygen/src/scene/figure/cache.rs
@@ -3,7 +3,10 @@ use crate::{
     anim::SkeletonAttr,
     render::{FigurePipeline, Mesh, Model, Renderer},
 };
-use common::comp::{Body, Equipment};
+use common::{
+    assets::watch::ReloadIndicator,
+    comp::{Body, Equipment},
+};
 use hashbrown::HashMap;
 
 #[derive(PartialEq, Eq, Hash, Clone)]
@@ -14,12 +17,14 @@ enum FigureKey {
 
 pub struct FigureModelCache {
     models: HashMap<FigureKey, ((Model<FigurePipeline>, SkeletonAttr), u64)>,
+    manifest_indicator: ReloadIndicator,
 }
 
 impl FigureModelCache {
     pub fn new() -> Self {
         Self {
             models: HashMap::new(),
+            manifest_indicator: ReloadIndicator::new(),
         }
     }
 
@@ -45,32 +50,44 @@ impl FigureModelCache {
                     key.clone(),
                     (
                         {
+                            let humanoid_head_spec =
+                                HumHeadSpec::load_watched(&mut self.manifest_indicator);
                             let bone_meshes = match body {
                                 Body::Humanoid(body) => [
-                                    Some(load_head(body.race, body.body_type)),
-                                    Some(load_chest(body.chest)),
-                                    Some(load_belt(body.belt)),
-                                    Some(load_pants(body.pants)),
-                                    Some(load_left_hand(body.hand)),
-                                    Some(load_right_hand(body.hand)),
-                                    Some(load_left_foot(body.foot)),
-                                    Some(load_right_foot(body.foot)),
-                                    Some(load_main(equipment.and_then(|e| e.main.as_ref()))),
-                                    Some(load_left_shoulder(body.shoulder)),
-                                    Some(load_right_shoulder(body.shoulder)),
-                                    Some(load_draw()),
+                                    Some(humanoid_head_spec.mesh_head(
+                                        body.race,
+                                        body.body_type,
+                                        body.hair_color,
+                                        body.hair_style,
+                                        body.beard,
+                                        body.eye_color,
+                                        body.skin,
+                                        body.eyebrows,
+                                        body.accessory,
+                                    )),
+                                    Some(mesh_chest(body.chest)),
+                                    Some(mesh_belt(body.belt)),
+                                    Some(mesh_pants(body.pants)),
+                                    Some(mesh_left_hand(body.hand)),
+                                    Some(mesh_right_hand(body.hand)),
+                                    Some(mesh_left_foot(body.foot)),
+                                    Some(mesh_right_foot(body.foot)),
+                                    Some(mesh_main(equipment.and_then(|e| e.main.as_ref()))),
+                                    Some(mesh_left_shoulder(body.shoulder)),
+                                    Some(mesh_right_shoulder(body.shoulder)),
+                                    Some(mesh_draw()),
                                     None,
                                     None,
                                     None,
                                     None,
                                 ],
                                 Body::Quadruped(body) => [
-                                    Some(load_pig_head(body.head)),
-                                    Some(load_pig_chest(body.chest)),
-                                    Some(load_pig_leg_lf(body.leg_l)),
-                                    Some(load_pig_leg_rf(body.leg_r)),
-                                    Some(load_pig_leg_lb(body.leg_l)),
-                                    Some(load_pig_leg_rb(body.leg_r)),
+                                    Some(mesh_pig_head(body.head)),
+                                    Some(mesh_pig_chest(body.chest)),
+                                    Some(mesh_pig_leg_lf(body.leg_l)),
+                                    Some(mesh_pig_leg_rf(body.leg_r)),
+                                    Some(mesh_pig_leg_lb(body.leg_l)),
+                                    Some(mesh_pig_leg_rb(body.leg_r)),
                                     None,
                                     None,
                                     None,
@@ -83,17 +100,17 @@ impl FigureModelCache {
                                     None,
                                 ],
                                 Body::QuadrupedMedium(body) => [
-                                    Some(load_wolf_head_upper(body.head_upper)),
-                                    Some(load_wolf_jaw(body.jaw)),
-                                    Some(load_wolf_head_lower(body.head_lower)),
-                                    Some(load_wolf_tail(body.tail)),
-                                    Some(load_wolf_torso_back(body.torso_back)),
-                                    Some(load_wolf_torso_mid(body.torso_mid)),
-                                    Some(load_wolf_ears(body.ears)),
-                                    Some(load_wolf_foot_lf(body.foot_lf)),
-                                    Some(load_wolf_foot_rf(body.foot_rf)),
-                                    Some(load_wolf_foot_lb(body.foot_lb)),
-                                    Some(load_wolf_foot_rb(body.foot_rb)),
+                                    Some(mesh_wolf_head_upper(body.head_upper)),
+                                    Some(mesh_wolf_jaw(body.jaw)),
+                                    Some(mesh_wolf_head_lower(body.head_lower)),
+                                    Some(mesh_wolf_tail(body.tail)),
+                                    Some(mesh_wolf_torso_back(body.torso_back)),
+                                    Some(mesh_wolf_torso_mid(body.torso_mid)),
+                                    Some(mesh_wolf_ears(body.ears)),
+                                    Some(mesh_wolf_foot_lf(body.foot_lf)),
+                                    Some(mesh_wolf_foot_rf(body.foot_rf)),
+                                    Some(mesh_wolf_foot_lb(body.foot_lb)),
+                                    Some(mesh_wolf_foot_rb(body.foot_rb)),
                                     None,
                                     None,
                                     None,
@@ -101,7 +118,7 @@ impl FigureModelCache {
                                     None,
                                 ],
                                 Body::Object(object) => [
-                                    Some(load_object(object)),
+                                    Some(mesh_object(object)),
                                     None,
                                     None,
                                     None,
@@ -148,6 +165,11 @@ impl FigureModelCache {
     }
 
     pub fn clean(&mut self, tick: u64) {
+        // Check for reloaded manifests
+        // TODO: maybe do this in a different function, maintain?
+        if self.manifest_indicator.reloaded() {
+            self.models.clear();
+        }
         // TODO: Don't hard-code this.
         self.models
             .retain(|_, (_, last_used)| *last_used + 60 > tick);
