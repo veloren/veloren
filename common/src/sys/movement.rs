0912de2a268a68a76e887a9e1082d0dbb144ac23
timokoesters <timo@koesters.xyz>
2019-08-24 21:12:54 +0200

    Make aiming more precise

--- common/src/sys/movement.rs
+++ common/src/sys/movement.rs
@@ -94,13 +94,7 @@ impl<'a> System<'a> for Sys {
                 };
 
             // Set direction based on move direction when on the ground
-            let ori_dir = if controller
-                .look_dir
-                .map(|n| !n.is_normal() || n.abs() < std::f32::EPSILON)
-                .reduce_or()
-                || character.movement == Glide
-                || character.movement.is_roll()
-            {
+            let ori_dir = if character.movement == Glide || character.movement.is_roll() {
                 Vec2::from(vel.0)
             } else {
                 Vec2::from(controller.look_dir).normalized()
