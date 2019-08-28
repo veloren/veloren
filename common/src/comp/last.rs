8900a9e4f32b640875eb27f0ed481f6dac8790e7
timokoesters <timo@koesters.xyz>
2019-08-28 14:46:20 +0200

    Fix rolling for the player

--- common/src/comp/last.rs
+++ common/src/comp/last.rs
@@ -7,9 +7,3 @@ pub struct Last<C: Component + PartialEq>(pub C);
 impl<C: Component + Send + Sync + PartialEq> Component for Last<C> {
     type Storage = VecStorage<Self>;
 }
-
-impl<C: Component + PartialEq> PartialEq<C> for Last<C> {
-    fn eq(&self, other: &C) -> bool {
-        self.0 == *other
-    }
-}
