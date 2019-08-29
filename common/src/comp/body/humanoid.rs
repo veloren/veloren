a1c8a4a2f90434e6a7563433dd794e29e75c4c35
Imbris <imbrisf@gmail.com>
2019-08-28 22:48:06 -0400

    adjust how figure head coloring is done

--- common/src/comp/body/humanoid.rs
+++ common/src/comp/body/humanoid.rs
@@ -1,4 +1,5 @@
-use rand::{seq::SliceRandom, thread_rng};
+use rand::{seq::SliceRandom, thread_rng, Rng};
+use vek::Rgb;
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct Body {
@@ -10,20 +11,21 @@ pub struct Body {
     pub hand: Hand,
     pub foot: Foot,
     pub shoulder: Shoulder,
-    pub hair_color: HairColor,
     pub hair_style: HairStyle,
     pub beard: Beard,
-    pub skin: Skin,
     pub eyebrows: Eyebrows,
-    pub eye_color: EyeColor,
     pub accessory: Accessory,
+    pub hair_color: u8,
+    pub skin: u8,
+    pub eye_color: u8,
 }
 
 impl Body {
     pub fn random() -> Self {
         let mut rng = thread_rng();
+        let race = *(&ALL_RACES).choose(&mut rng).unwrap();
         Self {
-            race: *(&ALL_RACES).choose(&mut rng).unwrap(),
+            race,
             body_type: *(&ALL_BODY_TYPES).choose(&mut rng).unwrap(),
             chest: *(&ALL_CHESTS).choose(&mut rng).unwrap(),
             belt: *(&ALL_BELTS).choose(&mut rng).unwrap(),
@@ -31,13 +33,13 @@ impl Body {
             hand: *(&ALL_HANDS).choose(&mut rng).unwrap(),
             foot: *(&ALL_FEET).choose(&mut rng).unwrap(),
             shoulder: *(&ALL_SHOULDERS).choose(&mut rng).unwrap(),
-            hair_color: *(&ALL_HAIR_COLORS).choose(&mut rng).unwrap(),
             hair_style: *(&ALL_HAIR_STYLES).choose(&mut rng).unwrap(),
             beard: *(&ALL_BEARDS).choose(&mut rng).unwrap(),
-            skin: *(&ALL_SKINS).choose(&mut rng).unwrap(),
             eyebrows: *(&ALL_EYEBROWS).choose(&mut rng).unwrap(),
-            eye_color: *(&ALL_EYE_COLORS).choose(&mut rng).unwrap(),
             accessory: *(&ALL_ACCESSORIES).choose(&mut rng).unwrap(),
+            hair_color: rng.gen_range(0, race.num_hair_colors()) as u8,
+            skin: rng.gen_range(0, race.num_skin_colors()) as u8,
+            eye_color: rng.gen_range(0, race.num_eye_colors()) as u8,
         }
     }
 }
@@ -59,6 +61,92 @@ pub const ALL_RACES: [Race; 6] = [
     Race::Orc,
     Race::Undead,
 ];
+// Hair Colors
+pub const DANARI_HAIR_COLORS: [(u8, u8, u8); 4] = [
+    (198, 169, 113),
+    (200, 100, 100),
+    (100, 100, 200),
+    (100, 200, 100),
+];
+pub const DWARF_HAIR_COLORS: [(u8, u8, u8); 3] =
+    [(200, 100, 100), (100, 100, 200), (100, 200, 100)];
+pub const ELF_HAIR_COLORS: [(u8, u8, u8); 3] = [(200, 100, 100), (100, 100, 200), (100, 200, 100)];
+pub const HUMAN_HAIR_COLORS: [(u8, u8, u8); 3] =
+    [(200, 100, 100), (100, 100, 200), (100, 200, 100)];
+pub const ORC_HAIR_COLORS: [(u8, u8, u8); 3] = [(200, 100, 100), (100, 100, 200), (100, 200, 100)];
+pub const UNDEAD_HAIR_COLORS: [(u8, u8, u8); 3] =
+    [(200, 100, 100), (100, 100, 200), (100, 200, 100)];
+// Skin colors
+pub const DANARI_SKIN_COLORS: [(u8, u8, u8); 4] = [
+    (198, 169, 113),
+    (200, 100, 100),
+    (100, 100, 200),
+    (100, 200, 100),
+];
+pub const DWARF_SKIN_COLORS: [(u8, u8, u8); 3] =
+    [(200, 100, 100), (100, 100, 200), (100, 200, 100)];
+pub const ELF_SKIN_COLORS: [(u8, u8, u8); 3] = [(200, 100, 100), (100, 100, 200), (100, 200, 100)];
+pub const HUMAN_SKIN_COLORS: [(u8, u8, u8); 3] =
+    [(200, 100, 100), (100, 100, 200), (100, 200, 100)];
+pub const ORC_SKIN_COLORS: [(u8, u8, u8); 3] = [(200, 100, 100), (100, 100, 200), (100, 200, 100)];
+pub const UNDEAD_SKIN_COLORS: [(u8, u8, u8); 3] =
+    [(200, 100, 100), (100, 100, 200), (100, 200, 100)];
+impl Race {
+    fn hair_colors(self) -> &'static [(u8, u8, u8)] {
+        match self {
+            Race::Danari => &DANARI_HAIR_COLORS,
+            Race::Dwarf => &DWARF_HAIR_COLORS,
+            Race::Elf => &ELF_HAIR_COLORS,
+            Race::Human => &HUMAN_HAIR_COLORS,
+            Race::Orc => &ORC_HAIR_COLORS,
+            Race::Undead => &UNDEAD_HAIR_COLORS,
+        }
+    }
+    fn skin_colors(self) -> &'static [(u8, u8, u8)] {
+        match self {
+            Race::Danari => &DANARI_SKIN_COLORS,
+            Race::Dwarf => &DWARF_SKIN_COLORS,
+            Race::Elf => &ELF_SKIN_COLORS,
+            Race::Human => &HUMAN_SKIN_COLORS,
+            Race::Orc => &ORC_SKIN_COLORS,
+            Race::Undead => &UNDEAD_SKIN_COLORS,
+        }
+    }
+    fn eye_colors(self) -> &'static [EyeColor] {
+        match self {
+            _ => &ALL_EYE_COLORS,
+        }
+    }
+    pub fn hair_color(self, val: u8) -> Rgb<u8> {
+        self.hair_colors()
+            .get(val as usize)
+            .copied()
+            .unwrap_or((0, 0, 0))
+            .into()
+    }
+    pub fn num_hair_colors(self) -> usize {
+        self.hair_colors().len()
+    }
+    pub fn skin_color(self, val: u8) -> Rgb<u8> {
+        self.skin_colors()
+            .get(val as usize)
+            .copied()
+            .unwrap_or((0, 0, 0))
+            .into()
+    }
+    pub fn num_skin_colors(self) -> usize {
+        self.skin_colors().len()
+    }
+    pub fn eye_color(self, val: u8) -> EyeColor {
+        self.eye_colors()
+            .get(val as usize)
+            .copied()
+            .unwrap_or(EyeColor::Black)
+    }
+    pub fn num_eye_colors(self) -> usize {
+        self.eye_colors().len()
+    }
+}
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub enum BodyType {
@@ -136,31 +224,6 @@ pub enum HairStyle {
 pub const ALL_HAIR_STYLES: [HairStyle; 3] = [HairStyle::None, HairStyle::Temp1, HairStyle::Temp2];
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
-pub enum HairColor {
-    Red,
-    Green,
-    Blue,
-    Brown,
-    Black,
-}
-pub const ALL_HAIR_COLORS: [HairColor; 5] = [
-    HairColor::Red,
-    HairColor::Green,
-    HairColor::Blue,
-    HairColor::Brown,
-    HairColor::Black,
-];
-
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
-pub enum Skin {
-    Light,
-    Medium,
-    Dark,
-    Rainbow,
-}
-pub const ALL_SKINS: [Skin; 4] = [Skin::Light, Skin::Medium, Skin::Dark, Skin::Rainbow];
-
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub enum Eyebrows {
     Yup,
 }
@@ -183,6 +246,34 @@ pub const ALL_EYE_COLORS: [EyeColor; 6] = [
     EyeColor::Red,
     EyeColor::White,
 ];
+impl EyeColor {
+    pub fn light_rgb(self) -> Rgb<u8> {
+        match self {
+            EyeColor::Black => Rgb::new(0, 0, 0),
+            EyeColor::Blue => Rgb::new(0, 0, 200),
+            EyeColor::Green => Rgb::new(0, 200, 0),
+            EyeColor::Brown => Rgb::new(150, 150, 0),
+            EyeColor::Red => Rgb::new(255, 0, 0),
+            EyeColor::White => Rgb::new(255, 255, 255),
+        }
+    }
+    pub fn dark_rgb(self) -> Rgb<u8> {
+        match self {
+            EyeColor::Black => Rgb::new(0, 0, 0),
+            EyeColor::Blue => Rgb::new(0, 0, 100),
+            EyeColor::Green => Rgb::new(0, 100, 0),
+            EyeColor::Brown => Rgb::new(50, 50, 0),
+            EyeColor::Red => Rgb::new(200, 0, 0),
+            EyeColor::White => Rgb::new(255, 255, 255),
+        }
+    }
+    pub fn white_rgb(self) -> Rgb<u8> {
+        match self {
+            EyeColor::White => Rgb::new(0, 0, 0),
+            _ => Rgb::new(255, 255, 255),
+        }
+    }
+}
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub enum Accessory {
