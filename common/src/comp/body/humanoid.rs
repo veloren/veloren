9328ac4f728ca90df2eaf623f671e040c5909637
jshipsey <jshipsey18@gmail.com>
2019-08-31 14:26:50 -0400

    tweaked hair colors, head offsets, skin colors

--- common/src/comp/body/humanoid.rs
+++ common/src/comp/body/humanoid.rs
@@ -66,29 +66,29 @@ pub const ALL_RACES: [Race; 6] = [
 pub const DANARI_HAIR_COLORS: [(u8, u8, u8); 4] = [
     (198, 169, 113),
     (146, 32, 32),
-    (198, 169, 113),
-    (198, 169, 113),
+    (199, 131, 58),
+    (107, 32, 60),
 ];
-pub const DWARF_HAIR_COLORS: [(u8, u8, u8); 3] = [(126, 26, 26), (54, 46, 38), (54, 46, 38)];
-pub const ELF_HAIR_COLORS: [(u8, u8, u8); 3] = [(66, 83, 113), (13, 76, 41), (66, 83, 113)];
-pub const HUMAN_HAIR_COLORS: [(u8, u8, u8); 3] = [(73, 42, 36), (73, 42, 36), (73, 42, 36)];
-pub const ORC_HAIR_COLORS: [(u8, u8, u8); 3] = [(11, 11, 11), (54, 30, 26), (54, 30, 26)];
-pub const UNDEAD_HAIR_COLORS: [(u8, u8, u8); 3] = [(0, 131, 122), (24, 19, 17), (0, 131, 122)];
+pub const DWARF_HAIR_COLORS: [(u8, u8, u8); 3] = [(126, 26, 26), (54, 46, 38), (99, 75, 49)];
+pub const ELF_HAIR_COLORS: [(u8, u8, u8); 3] = [(66, 83, 113), (13, 76, 41), (189, 185, 126)];
+pub const HUMAN_HAIR_COLORS: [(u8, u8, u8); 3] = [(107, 76, 51), (161, 63, 18), (64, 32, 18)];
+pub const ORC_HAIR_COLORS: [(u8, u8, u8); 3] = [(66, 66, 59), (54, 30, 26), (125, 111, 51)];
+pub const UNDEAD_HAIR_COLORS: [(u8, u8, u8); 3] = [(0, 131, 122), (66, 66, 59), (111, 54, 117)];
 
 // Skin colors
 pub const DANARI_SKIN_COLORS: [(u8, u8, u8); 4] = [
+    (104, 168, 196),
+    (30, 149, 201),
     (57, 120, 148),
-    (57, 120, 148),
-    (57, 120, 148),
-    (57, 120, 148),
+    (40, 85, 105),
 ];
-pub const DWARF_SKIN_COLORS: [(u8, u8, u8); 3] = [(215, 175, 123), (193, 136, 87), (193, 136, 87)];
-pub const ELF_SKIN_COLORS: [(u8, u8, u8); 3] = [(169, 145, 177), (145, 133, 148), (255, 200, 15)];
+pub const DWARF_SKIN_COLORS: [(u8, u8, u8); 3] = [(215, 175, 123), (191, 125, 94), (212, 128, 89)];
+pub const ELF_SKIN_COLORS: [(u8, u8, u8); 3] = [(176, 161, 181), (132, 139, 161), (138, 119, 201)];
 pub const HUMAN_SKIN_COLORS: [(u8, u8, u8); 3] =
-    [(255, 200, 159), (220, 169, 125), (220, 169, 125)];
-pub const ORC_SKIN_COLORS: [(u8, u8, u8); 3] = [(68, 129, 44), (77, 150, 51), (45, 95, 32)];
+    [(255, 200, 159), (186, 140, 104), (87, 57, 34)];
+pub const ORC_SKIN_COLORS: [(u8, u8, u8); 3] = [(77, 150, 51), (82, 117, 36), (71, 94, 42)];
 pub const UNDEAD_SKIN_COLORS: [(u8, u8, u8); 3] =
-    [(255, 255, 255), (203, 255, 215), (213, 220, 180)];
+    [(255, 255, 255), (178, 178, 178), (145, 135, 121)];
 
 // Eye colors
 pub const DANARI_EYE_COLORS: [EyeColor; 7] = [
