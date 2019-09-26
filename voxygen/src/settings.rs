59e6febb4103d52d3aef08dc9a52dd50d9630dbf
Joshua Barretto <joshua.s.barretto@gmail.com>
2019-09-26 12:03:39 +0100

    Fixed build placement bug, changed climb to LControl

--- voxygen/src/settings.rs
+++ voxygen/src/settings.rs
@@ -64,7 +64,7 @@ impl Default for ControlSettings {
             jump: KeyMouse::Key(VirtualKeyCode::Space),
             sit: KeyMouse::Key(VirtualKeyCode::K),
             glide: KeyMouse::Key(VirtualKeyCode::LShift),
-            climb: KeyMouse::Key(VirtualKeyCode::Space),
+            climb: KeyMouse::Key(VirtualKeyCode::LControl),
             climb_down: KeyMouse::Key(VirtualKeyCode::LShift),
             wall_leap: KeyMouse::Mouse(MouseButton::Middle),
             mount: KeyMouse::Key(VirtualKeyCode::F),
