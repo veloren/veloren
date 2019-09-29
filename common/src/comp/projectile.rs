ba3db852f745d2e3bcb9b145fe1228beac308d9e
timokoesters <timo@koesters.xyz>
2019-09-29 10:37:07 +0200

    feat(bow): make arrows stick to walls

--- common/src/comp/projectile.rs
+++ common/src/comp/projectile.rs
@@ -11,6 +11,7 @@ pub enum Effect {
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Projectile {
     pub hit_ground: Vec<Effect>,
+    pub hit_wall: Vec<Effect>,
     pub hit_entity: Vec<Effect>,
 }
 
