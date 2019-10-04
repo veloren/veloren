e5e9cf3db60f46d1f7276144cdc95c030ffdef43
Shane Handley <shanehandley@fastmail.com>
2019-10-04 13:24:56 +0900

    RON property for matching  music tracks to the players current biome.

--- voxygen/src/scene/music.rs
+++ voxygen/src/scene/music.rs
@@ -1,6 +1,7 @@
 use crate::audio::{fader::Fader, AudioFrontend};
 use client::Client;
-use rand::seq::SliceRandom;
+use common::terrain::BiomeKind;
+use rand::seq::{SliceRandom};
 use rand::thread_rng;
 use ron::de::from_str;
 use serde::Deserialize;
@@ -12,11 +13,17 @@ struct SoundtrackCollection {
     tracks: Vec<SoundtrackItem>,
 }
 
-#[derive(Clone, Debug, Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct SoundtrackItem {
-    title: String,
     path: String,
     length: f64,
+    biome: Vec<BiomeKind>,
+}
+
+#[derive(Debug, Deserialize)]
+enum DayCyclePeriod {
+    Day,
+    Night,
 }
 
 pub struct MusicMgr {
@@ -38,14 +45,14 @@ impl MusicMgr {
         }
     }
 
-    pub fn maintain(&mut self, audio: &mut AudioFrontend, _client: &Client) {
+    pub fn maintain(&mut self, audio: &mut AudioFrontend, client: &Client) {
         const TRACK_CROSSOVER_TIME_SECONDS: f64 = 10.0;
 
         audio.set_listener_pos(&Vec3::zero(), &Vec3::zero());
 
         // Kick off soundtrack if not yet playing
         if !self.playing {
-            self.current_channel = self.play_random_track(audio);
+            self.current_channel = self.play_random_track(audio, client);
             self.playing = true;
         }
 
@@ -58,12 +65,29 @@ impl MusicMgr {
                 Fader::fade_out(TRACK_CROSSOVER_TIME_SECONDS as f32),
             );
 
-            self.current_channel = self.play_random_track(audio);
+            self.current_channel = self.play_random_track(audio, client);
         }
     }
 
-    fn play_random_track(&mut self, audio: &mut AudioFrontend) -> usize {
+    fn play_random_track(&mut self, audio: &mut AudioFrontend, client: &Client) -> usize {
+        let chunk = client.current_chunk();
+        let biome = match chunk {
+            Some(chunk) => chunk.meta().biome(),
+            None => BiomeKind::Void,
+        };
+
         let mut rng = thread_rng();
+
+        let tracks = self
+            .soundtrack
+            .tracks
+            .iter()
+            .filter(|track| track.biome.is_empty() || track.biome.contains(&biome))
+            .collect::<Vec<_>>();
+
+        log::warn!("current biome is {}", format!("{:#?}", biome));
+        log::warn!("Available Tracks {}", format!("{:#?}", tracks));
+
         let track = self.soundtrack.tracks.choose(&mut rng).unwrap();
 
         self.began_playing = Instant::now();
@@ -80,42 +104,58 @@ impl MusicMgr {
         (
           title: \"Field Grazing\",
           path: \"voxygen.audio.soundtrack.field_grazing\",
-          length: 154.0
+          length: 154.0,
+          biome: [
+            Grassland,
+            Forest
+          ],
         ),
         (
           title: \"Sacred Temple\",
           path: \"voxygen.audio.soundtrack.sacred_temple\",
-          length: 75.0
+          length: 75.0,
+          biome: [],
         ),
         (
           title: \"Ruination\",
           path: \"voxygen.audio.soundtrack.Ruination\",
-          length: 134.0
+          length: 134.0,
+          biome: [],
         ),
         (
           title: \"Snowtop Volume\",
           path: \"voxygen.audio.soundtrack.Snowtop_volume\",
-          length: 30.0
+          length: 30.0,
+          biome: [
+            Mountain,
+            Snowlands
+          ],
         ),
         (
           title: \"Ethereal Bonds\",
           path: \"voxygen.audio.soundtrack.Ethereal_Bonds\",
-          length: 59.0
+          length: 59.0,
+          biome: [],
         ),
         (
           title: \"Mineral Deposits\",
           path: \"voxygen.audio.soundtrack.Mineral_Deposits\",
-          length: 148.0
+          length: 148.0,
+          biome: [
+            Mountain
+          ],
         ),
         (
           title: \"Library Theme\",
           path: \"voxygen.audio.soundtrack.library_theme_with_harpsichord\",
-          length: 64.0
+          length: 64.0,
+          biome: [],
         ),
         (
           title: \"Fiesta del Pueblo\",
           path: \"voxygen.audio.soundtrack.fiesta_del_pueblo\",
-          length: 182.0
+          length: 182.0,
+          biome: [],
         ),
       ],
     )";
