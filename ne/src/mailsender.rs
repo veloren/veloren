89c9d4be7e307557a360d24b3be1b441a7458499
Acrimon <joel.wejdenstal@gmail.com>
2019-10-09 01:22:32 +0200

    Added ID's to mailsenders.

--- ne/src/mailsender.rs
+++ ne/src/mailsender.rs
@@ -5,10 +5,15 @@ use serde::de::DeserializeOwned;
 use crate::NetworkResult;
 
 pub struct MailSender<T> {
+    id: u32,
     sender: Sender<NetworkMessage<T>>,
 }
 
 impl<T: Send + Serialize + DeserializeOwned> MailSender<T> {
+    pub fn id(&self) -> u32 {
+        self.id
+    }
+
     pub fn send(&self, data: T) -> NetworkResult<()> {
         let (message, result_receiver) = NetworkMessage::new(data);
         self.sender.send(message)?;
