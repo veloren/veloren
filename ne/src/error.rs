c81f3ac93b564d2295ae66bb35992a16bae61e53
Acrimon <joel.wejdenstal@gmail.com>
2019-10-09 18:15:03 +0200

    Added doc comments for the error module.

--- ne/src/error.rs
+++ ne/src/error.rs
@@ -1,11 +1,14 @@
 use crossbeam_channel::Receiver;
 
+/// An error that encompasses all possible error states. This is always used when something can fail.
+/// This enum has both wrapper variants and some variants for internal errors of this crate.
 pub enum NetworkError {
     Io(std::io::Error),
     Serde(serde_cbor::Error),
     EngineShutdown,
 }
 
+/// A shorthand for a Result whose error type is NetworkError.
 pub type NetworkResult<T> = Result<T, NetworkError>;
 
 impl From<std::io::Error> for NetworkError {
@@ -32,6 +35,7 @@ impl From<crossbeam_channel::RecvError> for NetworkError {
     }
 }
 
+/// A container for a NetworkResult that isn't available immediately but will be at some point in the future.
 pub struct FutureNetworkResult<T> {
     done: bool,
     error_receiver: Receiver<NetworkResult<T>>,
@@ -57,6 +61,9 @@ impl<T> FutureNetworkResult<T> {
         Self::new(rx)
     }
 
+    /// Poll the future to check if the result is ready. If it isn't ready yet this will return None.
+    /// If the result is ready it will return Some(result);
+    /// After the result has been received this must not be polled again. Doing so will result in a panic.
     pub fn poll(&mut self) -> Option<NetworkResult<T>> {
         if self.done {
             panic!("FutureNetworkError polled after receiving error");
