c41b902243f550b169a1903125084950f3d72d2f
Acrimon <joel.wejdenstal@gmail.com>
2019-10-09 18:31:12 +0200

    Added docs for the mailsender module.

--- ne/src/mailsender.rs
+++ ne/src/mailsender.rs
@@ -4,16 +4,21 @@ use serde::Serialize;
 use serde::de::DeserializeOwned;
 use crate::{FutureNetworkResult, Reliability};
 
+/// A mail sender. This will send mail to the end it is connection to. This may be anything running a NetworkEngine.
 pub struct MailSender<T> {
     id: u32,
     sender: Sender<InternalNetworkMessage<T>>,
 }
 
 impl<T: Send + Serialize + DeserializeOwned> MailSender<T> {
+    /// Grab the ID of the receiver.
     pub fn id(&self) -> u32 {
         self.id
     }
 
+    /// Send a mail. Here you will also specify the reliability guarantee of the mail. See the Reliability enum for details.
+    /// This method doesn't block until the message is sent and therefore returns a eager future that will contain
+    /// the result at some point in time. See the documentation for FutureNetworkResult for details.
     pub fn send(&self, data: T, reliability: Reliability) -> FutureNetworkResult<()> {
         let (message, result_receiver) = InternalNetworkMessage::new(data, reliability, self.id);
         match self.sender.send(message) {
