6ec87d5120e0f9fda3a771582cdfa7c7564758be
Acrimon <joel.wejdenstal@gmail.com>
2019-10-09 08:14:42 +0200

    Updated behaviour for MailBox::receive.

--- ne/src/error.rs
+++ ne/src/error.rs
@@ -1,41 +1,40 @@
 pub enum NetworkError {
-    IoError(std::io::Error),
-    SerdeError(serde_cbor::Error),
-    EngineShutdownError,
-    MailBoxEmpty,
+    Io(std::io::Error),
+    Serde(serde_cbor::Error),
+    EngineShutdown,
 }
 
 pub type NetworkResult<T> = Result<T, NetworkError>;
 
 impl From<std::io::Error> for NetworkError {
     fn from(e: std::io::Error) -> Self {
-        Self::IoError(e)
+        Self::Io(e)
     }
 }
 
 impl From<serde_cbor::Error> for NetworkError {
     fn from(e: serde_cbor::Error) -> Self {
-        Self::SerdeError(e)
+        Self::Serde(e)
     }
 }
 
 impl<T> From<crossbeam_channel::SendError<T>> for NetworkError {
     fn from(_e: crossbeam_channel::SendError<T>) -> Self {
-        Self::EngineShutdownError
+        Self::EngineShutdown
     }
 }
 
 impl From<crossbeam_channel::RecvError> for NetworkError {
     fn from(_e: crossbeam_channel::RecvError) -> Self {
-        Self::EngineShutdownError
+        Self::EngineShutdown
     }
 }
 
 impl From<crossbeam_channel::TryRecvError> for NetworkError {
     fn from(e: crossbeam_channel::TryRecvError) -> Self {
         match e {
-            crossbeam_channel::TryRecvError::Empty => Self::MailBoxEmpty,
-            crossbeam_channel::TryRecvError::Disconnected => Self::EngineShutdownError,
+            crossbeam_channel::TryRecvError::Empty => Self::EngineShutdown,
+            crossbeam_channel::TryRecvError::Disconnected => Self::EngineShutdown,
         }
     }
 }
--- ne/src/mailbox.rs
+++ ne/src/mailbox.rs
@@ -1,8 +1,9 @@
-use crate::NetworkResult;
+use crate::{NetworkResult, NetworkError};
 use crate::message::InternalNetworkMessage;
 use crossbeam_channel::Receiver;
 use serde::Serialize;
 use serde::de::DeserializeOwned;
+use crossbeam_channel::TryRecvError;
 
 pub struct Mail<T> {
     id: u32,
@@ -32,9 +33,19 @@ pub struct MailBox<T> {
 }
 
 impl<T: Send + Serialize + DeserializeOwned> MailBox<T> {
-    pub fn receive(&self) -> NetworkResult<Mail<T>> {
-        let message = self.message_queue.try_recv()?;
-        let (id, data) = message.deconstruct();
-        Ok(Mail::new(id, data))
+    pub fn receive(&self) -> NetworkResult<Option<Mail<T>>> {
+        match self.message_queue.try_recv() {
+            Ok(message) => {
+                let (id, data) = message.deconstruct();
+                Ok(Some(Mail::new(id, data)))
+            }
+
+            Err(error) => {
+                match error {
+                    TryRecvError::Empty => Ok(None),
+                    TryRecvError::Disconnected => Err(NetworkError::EngineShutdown),
+                }
+            }
+        }
     }
 }
