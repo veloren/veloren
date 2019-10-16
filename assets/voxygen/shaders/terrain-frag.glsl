3279a278b7f3a68a0fc02cd6a7a9d678a189d07e
Joshua Barretto <joshua.s.barretto@gmail.com>
2019-10-16 15:05:45 +0100

    Move computations to terrain fragment shaders

--- assets/voxygen/shaders/terrain-frag.glsl
+++ assets/voxygen/shaders/terrain-frag.glsl
@@ -3,7 +3,7 @@
 #include <globals.glsl>
 
 in vec3 f_pos;
-flat in vec3 f_norm;
+flat in uint f_pos_norm;
 in vec3 f_col;
 in float f_light;
 
@@ -19,6 +19,16 @@ out vec4 tgt_color;
 #include <light.glsl>
 
 void main() {
+	// First 3 normals are negative, next 3 are positive
+	vec3 normals[6] = vec3[]( vec3(-1,0,0), vec3(0,-1,0), vec3(0,0,-1), vec3(1,0,0), vec3(0,1,0), vec3(0,0,1) );
+
+	// TODO: last 3 bits in v_pos_norm should be a number between 0 and 5, rather than 0-2 and a direction.
+	uint norm_axis = (f_pos_norm >> 30) & 0x3u;
+	// Increase array access by 3 to access positive values
+	uint norm_dir = ((f_pos_norm >> 29) & 0x1u) * 3u;
+	// Use an array to avoid conditional branching
+	vec3 f_norm = normals[norm_axis + norm_dir];
+
 	vec3 light, diffuse_light, ambient_light;
 	get_sun_diffuse(f_norm, time_of_day.x, light, diffuse_light, ambient_light, 1.0);
 	float point_shadow = shadow_at(f_pos, f_norm);
@@ -27,7 +37,7 @@ void main() {
 	vec3 point_light = light_at(f_pos, f_norm);
 	light += point_light;
 	diffuse_light += point_light;
-	vec3 surf_color = illuminate(f_col, light, diffuse_light, ambient_light);
+	vec3 surf_color = illuminate(srgb_to_linear(f_col), light, diffuse_light, ambient_light);
 
 	float fog_level = fog(f_pos.xyz, focus_pos.xyz, medium.x);
 	vec3 fog_color = get_sky_color(normalize(f_pos - cam_pos.xyz), time_of_day.x, true);
