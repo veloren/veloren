69b008e0e653dbb4734642c5498d461bc2a1cb4a
Shane Handley <shanehandley@fastmail.com>
2019-10-20 14:42:54 +0900

    Fix issue with the timeout error showing when there is an intentional logout from the game.

--- voxygen/src/menu/char_selection/mod.rs
+++ voxygen/src/menu/char_selection/mod.rs
@@ -114,12 +114,9 @@ impl PlayState for CharSelectionState {
                 .tick(comp::ControllerInputs::default(), clock.get_last_delta())
             {
                 error!("Failed to tick the scene: {:?}", err);
-                global_state.info_message = Some(
-                    "Connection lost!\nDid the server restart?\nIs the client up to date?"
-                        .to_owned(),
-                );
                 return PlayStateResult::Pop;
             }
+
             self.client.borrow_mut().cleanup();
 
             // Finish the frame.
