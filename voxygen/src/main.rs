5beea76624e408029192e8a8717c30dbadc8fbad
Acrimon <joel.wejdenstal@gmail.com>
2019-10-29 06:55:45 +0000

    Remove heaptrack as it is now deprecated.

--- voxygen/src/main.rs
+++ voxygen/src/main.rs
@@ -38,11 +38,6 @@ use log::{self, debug, error, info};
 use simplelog::{CombinedLogger, Config, TermLogger, TerminalMode, WriteLogger};
 use std::{fs::File, mem, panic, str::FromStr};
 
-#[cfg(feature = "heaptrack")]
-use heaptrack::track_mem;
-#[cfg(feature = "heaptrack")]
-track_mem!();
-
 /// A type used to store state that is shared between all play states.
 pub struct GlobalState {
     settings: Settings,
