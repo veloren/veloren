b4e89a94f74acd6bafb7c1e034da04b70a4f4875
flyingfryingpan <mixj444@gmail.com>
2019-11-06 03:19:50 -0800

    forsharp

--- world/src/column/mod.rs
+++ world/src/column/mod.rs
@@ -825,23 +825,24 @@ impl<'a> Sampler<'a> for ColumnGen<'a> {
             .mul(0.5)
             .add(marble_small.sub(0.5).mul(0.25));
 
-        let temp = temp.add((marble - 0.5) * 0.25);
-        let humidity = humidity.add((marble - 0.5) * 0.25);
+        //let temp = temp.add((marble - 0.5) * 0.1);
+        //let humidity = humidity.add((marble - 0.5) * 0.10);
 
         // Colours
-        let cold_grass = Rgb::new(0.0, 0.5, 0.25);
-        let warm_grass = Rgb::new(0.4, 0.8, 0.0);
-        let dark_grass = Rgb::new(0.15, 0.4, 0.1);
-        let wet_grass = Rgb::new(0.1, 0.8, 0.2);
-        let cold_stone = Rgb::new(0.57, 0.67, 0.8);
-        let warm_stone = Rgb::new(0.77, 0.77, 0.64);
+        let cold_grass = Rgb::new(0.1, 0.5, 0.1);
+        let warm_grass = Rgb::new(0.1, 0.6, 0.0);
+        let dark_grass = Rgb::new(0.1, 0.3, 0.2);
+        let wet_grass = Rgb::new(0.1, 0.5, 0.5);
+        let cold_stone = Rgb::new(0.5, 0.5, 0.5);
+        //let warm_stone = Rgb::new(0.6, 0.6, 0.5);
+	let warm_stone = Rgb::new(0.7, 0.8, 0.7);
         let beach_sand = Rgb::new(0.9, 0.82, 0.6);
-        let desert_sand = Rgb::new(0.95, 0.75, 0.5);
-        let snow = Rgb::new(0.8, 0.85, 1.0);
-
-        let dirt = Lerp::lerp(
-            Rgb::new(0.075, 0.07, 0.3),
-            Rgb::new(0.75, 0.55, 0.1),
+        let desert_sand = Rgb::new(0.7, 0.7, 0.4);
+        let snow = Rgb::new(0.0, 0.0, 0.1);
+	let stone_col = Rgb::new(205, 186, 50);
+	let dirt = Lerp::lerp(
+            Rgb::new(0.6, 0.5, 0.0),
+            Rgb::new(0.6, 0.5, 0.1),
             marble,
         );
         let tundra = Lerp::lerp(snow, Rgb::new(0.01, 0.3, 0.0), 0.4 + marble * 0.6);
@@ -880,7 +881,7 @@ impl<'a> Sampler<'a> for ColumnGen<'a> {
                 sand,
                 temp.sub(CONFIG.snow_temp)
                     .div(CONFIG.desert_temp.sub(CONFIG.snow_temp))
-                    .mul(0.5),
+                    .mul(4.5),
             ),
             cliff,
             alt.sub(CONFIG.mountain_scale * 0.25)
@@ -901,7 +902,7 @@ impl<'a> Sampler<'a> for ColumnGen<'a> {
                                 .div(CONFIG.snow_temp.neg())
                                 /*.sub((marble - 0.5) * 0.05)
                                 .mul(256.0)*/
-                                .mul(1.0),
+                                .mul(8.0),
                         ),
                         // 0 to tropical_temp
                         grass,
@@ -911,18 +912,18 @@ impl<'a> Sampler<'a> for ColumnGen<'a> {
                     moss,
                     temp.sub(CONFIG.tropical_temp)
                         .div(CONFIG.desert_temp.sub(CONFIG.tropical_temp))
-                        .mul(1.0),
+                        .mul(8.0),
                 ),
                 // above desert_temp
                 sand,
                 temp.sub(CONFIG.desert_temp)
                     .div(1.0 - CONFIG.desert_temp)
-                    .mul(4.0),
+                    .mul(8.0),
             ),
             humidity
                 .sub(CONFIG.desert_hum)
                 .div(CONFIG.forest_hum.sub(CONFIG.desert_hum))
-                .mul(1.0),
+                .mul(8.0),
         );
         // From forest to jungle humidity, we go from snow to dark grass to grass to tropics to sand
         // depending on temperature.
@@ -940,18 +941,18 @@ impl<'a> Sampler<'a> for ColumnGen<'a> {
                     tropical,
                     temp.sub(CONFIG.tropical_temp)
                         .div(CONFIG.desert_temp.sub(CONFIG.tropical_temp))
-                        .mul(1.0),
+                        .mul(8.0),
                 ),
                 // above desert_temp
                 sand,
                 temp.sub(CONFIG.desert_temp)
                     .div(1.0 - CONFIG.desert_temp)
-                    .mul(4.0),
+                    .mul(8.0),
             ),
             humidity
                 .sub(CONFIG.forest_hum)
                 .div(CONFIG.jungle_hum.sub(CONFIG.forest_hum))
-                .mul(1.0),
+                .mul(8.0),
         );
         // From jungle humidity upwards, we go from snow to grass to rainforest to tropics to sand.
         let ground = Rgb::lerp(
@@ -968,13 +969,13 @@ impl<'a> Sampler<'a> for ColumnGen<'a> {
                     tropical,
                     temp.sub(CONFIG.tropical_temp)
                         .div(CONFIG.desert_temp.sub(CONFIG.tropical_temp))
-                        .mul(4.0),
+                        .mul(8.0),
                 ),
                 // above desert_temp
                 sand,
                 temp.sub(CONFIG.desert_temp)
                     .div(1.0 - CONFIG.desert_temp)
-                    .mul(4.0),
+                    .mul(8.0),
             ),
             humidity.sub(CONFIG.jungle_hum).mul(1.0),
         );
@@ -1045,7 +1046,7 @@ impl<'a> Sampler<'a> for ColumnGen<'a> {
                 // Beach
                 ((ocean_level - 1.0) / 2.0).max(0.0),
             ),
-            sub_surface_color: dirt,
+            sub_surface_color: warm_grass,
             tree_density,
             forest_kind: sim_chunk.forest_kind,
             close_structures: self.gen_close_structures(wpos),
@@ -1062,6 +1063,7 @@ impl<'a> Sampler<'a> for ColumnGen<'a> {
             humidity,
             spawn_rate,
             location: sim_chunk.location.as_ref(),
+	    stone_col,
 
             chunk: sim_chunk,
             spawn_rules: sim_chunk
@@ -1102,6 +1104,8 @@ pub struct ColumnSample<'a> {
     pub humidity: f32,
     pub spawn_rate: f32,
     pub location: Option<&'a LocationInfo>,
+    //making cliffs
+    pub stone_col: Rgb<u8>,
 
     pub chunk: &'a SimChunk,
     pub spawn_rules: SpawnRules,
