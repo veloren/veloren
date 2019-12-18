3607e45f6b3d79d44efd7e5db8c0bb87bf2390c8
Songtronix <contact@songtronix.com>
2019-12-18 20:35:17 +0100

    feat(log): configurable creation of log file
    additionally allows to ignore any errors with the log file
    
    furthermore prints out full log file path to the user incase of a panic

--- voxygen/src/logging.rs
+++ voxygen/src/logging.rs
@@ -1,6 +1,12 @@
 use fern::colors::{Color, ColoredLevelConfig};
 
-pub fn init(term_log_level: log::LevelFilter, file_log_level: log::LevelFilter) {
+use crate::settings::Settings;
+
+pub fn init(
+    settings: &Settings,
+    term_log_level: log::LevelFilter,
+    file_log_level: log::LevelFilter,
+) {
     let colors = ColoredLevelConfig::new()
         .error(Color::Red)
         .warn(Color::Yellow)
@@ -8,7 +14,7 @@ pub fn init(term_log_level: log::LevelFilter, file_log_level: log::LevelFilter) 
         .debug(Color::Green)
         .trace(Color::BrightBlack);
 
-    let base = fern::Dispatch::new()
+    let mut base = fern::Dispatch::new()
         .level_for("dot_vox::parser", log::LevelFilter::Warn)
         .level_for("gfx_device_gl::factory", log::LevelFilter::Warn)
         .level_for("veloren_voxygen::discord", log::LevelFilter::Warn)
@@ -17,25 +23,35 @@ pub fn init(term_log_level: log::LevelFilter, file_log_level: log::LevelFilter) 
 
     let time = chrono::offset::Utc::now();
 
-    let file_cfg = fern::Dispatch::new()
-        .level(file_log_level)
-        .format(|out, message, record| {
-            out.finish(format_args!(
-                "{}[{}:{}][{}] {}",
-                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
-                record.target(),
-                record
-                    .line()
-                    .map(|x| x.to_string())
-                    .unwrap_or("X".to_string()),
-                record.level(),
-                message
-            ))
-        })
-        .chain(
+    let mut file_cfg =
+        fern::Dispatch::new()
+            .level(file_log_level)
+            .format(|out, message, record| {
+                out.finish(format_args!(
+                    "{}[{}:{}][{}] {}",
+                    chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                    record.target(),
+                    record
+                        .line()
+                        .map(|x| x.to_string())
+                        .unwrap_or("X".to_string()),
+                    record.level(),
+                    message
+                ))
+            });
+
+    // Based on settings ignore errors incase log file can't be created
+    if !settings.log.ignore_errors {
+        file_cfg = file_cfg.chain(
             fern::log_file(&format!("voxygen-{}.log", time.format("%Y-%m-%d-%H")))
                 .expect("Failed to create log file!"),
         );
+    } else {
+        if let Ok(log_file) = fern::log_file(&format!("voxygen-{}.log", time.format("%Y-%m-%d-%H")))
+        {
+            file_cfg = file_cfg.chain(log_file);
+        }
+    }
 
     let stdout_cfg = fern::Dispatch::new()
         .level(term_log_level)
@@ -48,8 +64,10 @@ pub fn init(term_log_level: log::LevelFilter, file_log_level: log::LevelFilter) 
         })
         .chain(std::io::stdout());
 
-    base.chain(file_cfg)
-        .chain(stdout_cfg)
+    if settings.log.log_to_file {
+        base = base.chain(file_cfg);
+    }
+    base.chain(stdout_cfg)
         .apply()
         .expect("Failed to setup logging!");
 }
