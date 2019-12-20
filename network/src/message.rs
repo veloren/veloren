833e5955dce9cbdb43e7672e9475332555df08e9
Marcel Märtens <marcel.cochem@googlemail.com>
2019-12-20 14:56:01 +0100

    initial implementation of a network api

---
+++ network/src/message.rs
@@ -0,0 +1,45 @@
+use bincode;
+use serde::{Deserialize, Serialize};
+use std::{collections::VecDeque, sync::Arc};
+pub trait Message<'a> = Serialize + Deserialize<'a>;
+
+struct MessageBuffer {
+    // use VecDeque for msg storage, because it allows to quickly remove data from front.
+    //however VecDeque needs custom bincode code, but it's possible
+    data: Vec<u8>,
+}
+
+struct OutGoingMessage {
+    buffer: Arc<MessageBuffer>,
+    cursor: u64,
+}
+
+fn serialize<'a, M: Message<'a>>(message: &M) -> MessageBuffer {
+    let mut writer = {
+        let actual_size = bincode::serialized_size(message).unwrap();
+        Vec::<u8>::with_capacity(actual_size as usize)
+    };
+    if let Err(e) = bincode::serialize_into(&mut writer, message) {
+        println!("Oh nooo {}", e);
+    };
+    MessageBuffer { data: writer }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::message::*;
+
+    #[test]
+    fn serialize_test() {
+        let msg = "abc";
+        let mb = serialize(&msg);
+        assert_eq!(mb.data.len(), 11);
+        assert_eq!(mb.data[0], 3);
+        assert_eq!(mb.data[1], 0);
+        assert_eq!(mb.data[7], 0);
+        assert_eq!(mb.data[8], 'a' as u8);
+        assert_eq!(mb.data[8], 97);
+        assert_eq!(mb.data[9], 'b' as u8);
+        assert_eq!(mb.data[10], 'c' as u8);
+    }
+}
