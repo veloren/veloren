b66803e5ed2746895448202bd4d704b10100550a
Joshua Barretto <joshua.s.barretto@gmail.com>
2020-01-20 14:21:06 +0000

    Enumerated Body type

--- common/src/comp/body/fish_medium.rs
+++ common/src/comp/body/fish_medium.rs
@@ -1,6 +1,7 @@
 use rand::{seq::SliceRandom, thread_rng};
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[repr(C)]
 pub struct Body {
     pub head: Head,
     pub torso: Torso,
@@ -24,36 +25,42 @@ impl Body {
 }
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[repr(u32)]
 pub enum Head {
     Default,
 }
 const ALL_HEADS: [Head; 1] = [Head::Default];
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[repr(u32)]
 pub enum Torso {
     Default,
 }
 const ALL_TORSOS: [Torso; 1] = [Torso::Default];
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[repr(u32)]
 pub enum Rear {
     Default,
 }
 const ALL_REARS: [Rear; 1] = [Rear::Default];
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[repr(u32)]
 pub enum Tail {
     Default,
 }
 const ALL_TAILS: [Tail; 1] = [Tail::Default];
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[repr(u32)]
 pub enum FinL {
     Default,
 }
 const ALL_FIN_LS: [FinL; 1] = [FinL::Default];
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[repr(u32)]
 pub enum FinR {
     Default,
 }
