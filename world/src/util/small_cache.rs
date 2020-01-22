0b7f428310c8a1e20ed646f72b6dff71af3ef8e5
Joshua Yanovski <pythonesque@gmail.com>
2020-01-22 14:35:13 +0100

    Erosion cleanup, part 1.
    Covers all files touched by MR that are not in world/src/sim.

--- world/src/util/small_cache.rs
+++ world/src/util/small_cache.rs
@@ -9,7 +9,8 @@ fn calc_idx(v: Vec2<i32>) -> usize {
     (x ^ y) as usize
 }
 
-const CACHE_LEN: usize = /*512*//*128*/32;
+// NOTE: Use 128 if TerrainChunkSize::RECT_SIZE.x = 128.
+const CACHE_LEN: usize = 32;
 
 pub struct SmallCache<V: Default> {
     index: [Option<Vec2<i32>>; CACHE_LEN + 9],
@@ -20,15 +21,11 @@ impl<V: Default> Default for SmallCache<V> {
     fn default() -> Self {
         Self {
             index: [None; CACHE_LEN + 9],
-            data: arr![V::default(); /*521*//*137*/41], // TODO: Use CACHE_LEN + 9
+            data: arr![V::default(); 41], // TODO: Use CACHE_LEN
             random: 1,
         }
     }
 }
-
-/* pub struct SmalllCacheKeys<'id, V> {
-} */
-
 impl<V: Default> SmallCache<V> {
     pub fn get<F: FnOnce(Vec2<i32>) -> V>(&mut self, key: Vec2<i32>, f: F) -> &V {
         let idx = calc_idx(key) % CACHE_LEN;
