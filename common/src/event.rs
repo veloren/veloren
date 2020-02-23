d87061fe145e6ba28efe8239b481a28fae55942b
S Handley <shanehandley@fastmail.com>
2020-02-23 01:26:51 +0000

    Add a volume option to SfxEvents, and use this to dispatch movement sfx for quadripeds at a volume proportionate to their size.

--- common/src/event.rs
+++ common/src/event.rs
@@ -9,12 +9,21 @@ use vek::*;
 pub struct SfxEventItem {
     pub sfx: SfxEvent,
     pub pos: Option<Vec3<f32>>,
+    pub vol: Option<f32>,
 }
 
 impl SfxEventItem {
-    pub fn new(sfx: SfxEvent, pos: Option<Vec3<f32>>) -> Self { Self { sfx, pos } }
+    pub fn new(sfx: SfxEvent, pos: Option<Vec3<f32>>, vol: Option<f32>) -> Self {
+        Self { sfx, pos, vol }
+    }
 
-    pub fn at_player_position(sfx: SfxEvent) -> Self { Self { sfx, pos: None } }
+    pub fn at_player_position(sfx: SfxEvent) -> Self {
+        Self {
+            sfx,
+            pos: None,
+            vol: None,
+        }
+    }
 }
 
 #[derive(Copy, Clone, Debug, PartialEq, Deserialize, Hash, Eq)]
