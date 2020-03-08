cc5845d7560c87ea9c022b14136cf5f3bf40e408
Capucho <jcapucho7@gmail.com>
2020-03-08 08:06:22 +0000

    Proper fix to the logout timeout problem using Disconnect ACK

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -78,6 +78,8 @@ pub struct Client {
     loaded_distance: f32,
 
     pending_chunks: HashMap<Vec2<i32>, Instant>,
+
+    disconnected: bool,
 }
 
 impl Client {
@@ -164,6 +166,8 @@ impl Client {
             loaded_distance: 0.0,
 
             pending_chunks: HashMap::new(),
+
+            disconnected: false,
         })
     }
 
@@ -721,8 +725,9 @@ impl Client {
                         );
                     },
                     ServerMsg::Disconnect => {
+                        self.disconnected = true;
                         frontend_events.push(Event::Disconnect);
-                        self.client_state = ClientState::Disconnected;
+                        self.postbox.send_message(ClientMsg::Terminate);
                     },
                 }
             }
@@ -734,6 +739,9 @@ impl Client {
         }
         Ok(frontend_events)
     }
+
+    // Get's whether or not the client just disconnected
+    pub fn disconnected(&self) -> bool { self.disconnected }
 
     /// Get the player's entity.
     pub fn entity(&self) -> EcsEntity { self.entity }
