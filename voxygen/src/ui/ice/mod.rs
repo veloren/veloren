e64f8aec6087b89b7c5ecd4a463cb51fa7d88011
Imbris <imbrisf@gmail.com>
2020-03-16 17:56:50 -0400

    Get resizing working

--- voxygen/src/ui/ice/mod.rs
+++ voxygen/src/ui/ice/mod.rs
@@ -10,25 +10,39 @@ pub use renderer::IcedRenderer;
 pub use widget::image::Image;
 pub use winit_conversion::window_event;
 
-use super::graphic::{self, Graphic};
+use super::{
+    graphic::{self, Graphic},
+    scale::{Scale, ScaleMode},
+};
 use crate::{render::Renderer, window::Window, Error};
 use clipboard::Clipboard;
 use iced::{Cache, Element, MouseCursor, Size, UserInterface};
+use vek::*;
 
 pub struct IcedUi {
     renderer: IcedRenderer,
     cache: Option<Cache>,
     events: Vec<Event>,
     clipboard: Clipboard,
+    // Scaling of the ui
+    scale: Scale,
+    window_resized: Option<Vec2<u32>>,
 }
 impl IcedUi {
     pub fn new(window: &mut Window) -> Result<Self, Error> {
+        let scale = Scale::new(window, ScaleMode::Absolute(1.0));
+        let renderer = window.renderer_mut();
+
+        let scaled_dims = scale.scaled_window_size().map(|e| e as f32);
+
         Ok(Self {
-            renderer: IcedRenderer::new(window)?,
+            renderer: IcedRenderer::new(renderer, scaled_dims)?,
             cache: Some(Cache::new()),
             events: Vec::new(),
             // TODO: handle None
             clipboard: Clipboard::new(window.window()).unwrap(),
+            scale,
+            window_resized: None,
         })
     }
 
@@ -37,8 +51,40 @@ impl IcedUi {
         self.renderer.add_graphic(graphic)
     }
 
-    // TODO: handle scaling here
-    pub fn handle_event(&mut self, event: Event) { self.events.push(event); }
+    pub fn handle_event(&mut self, event: Event) {
+        use iced::{input::mouse, window};
+        match event {
+            // Intercept resizing events
+            Event::Window(window::Event::Resized { width, height }) => {
+                self.window_resized = Some(Vec2::new(width, height));
+            },
+            // Scale cursor movement events
+            // Note: in some cases the scaling could be off if a resized event occured in the same
+            // frame, in practice this shouldn't be an issue
+            Event::Mouse(mouse::Event::CursorMoved { x, y }) => {
+                // TODO: return f32 here
+                let scale = self.scale.scale_factor_logical() as f32;
+                self.events.push(Event::Mouse(mouse::Event::CursorMoved {
+                    x: x * scale,
+                    y: y * scale,
+                }));
+            },
+            // Scale pixel scrolling events
+            Event::Mouse(mouse::Event::WheelScrolled {
+                delta: mouse::ScrollDelta::Pixels { x, y },
+            }) => {
+                // TODO: return f32 here
+                let scale = self.scale.scale_factor_logical() as f32;
+                self.events.push(Event::Mouse(mouse::Event::WheelScrolled {
+                    delta: mouse::ScrollDelta::Pixels {
+                        x: x * scale,
+                        y: y * scale,
+                    },
+                }));
+            },
+            event => self.events.push(event),
+        }
+    }
 
     // TODO: produce root internally???
     pub fn maintain<'a, M, E: Into<Element<'a, M, IcedRenderer>>>(
@@ -46,8 +92,33 @@ impl IcedUi {
         root: E,
         renderer: &mut Renderer,
     ) -> (Vec<M>, MouseCursor) {
+        // Handle window resizing
+        if let Some(new_dims) = self.window_resized.take() {
+            let old_scaled_dims = self.scale.scaled_window_size();
+            // TODO maybe use u32 in Scale to be consistent with iced
+            self.scale
+                .window_resized(new_dims.map(|e| e as f64), renderer);
+            let scaled_dims = self.scale.scaled_window_size();
+
+            self.events
+                .push(Event::Window(iced::window::Event::Resized {
+                    width: scaled_dims.x as u32,
+                    height: scaled_dims.y as u32,
+                }));
+
+            // Avoid panic in graphic cache when minimizing.
+            // Avoid resetting cache if window size didn't change
+            // Somewhat inefficient for elements that won't change size after a window
+            // resize
+            let res = renderer.get_resolution();
+            if res.x > 0 && res.y > 0 && scaled_dims != old_scaled_dims {
+                self.renderer
+                    .resize(scaled_dims.map(|e| e as f32), renderer);
+            }
+        }
+
         // TODO: convert to f32 at source
-        let window_size = self.renderer.scaled_window_size().map(|e| e as f32);
+        let window_size = self.scale.scaled_window_size().map(|e| e as f32);
 
         let mut user_interface = UserInterface::build(
             root,
