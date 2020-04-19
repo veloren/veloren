6e43b536fbae49793fe55435b76cf5330c9a1e8f
jshipsey <jshipsey18@gmail.com>
2020-04-19 02:09:58 -0400

    corrected villager empty weapon issue

--- voxygen/src/anim/biped_large/run.rs
+++ voxygen/src/anim/biped_large/run.rs
@@ -17,7 +17,7 @@ impl Animation for RunAnimation {
     ) -> Self::Skeleton {
         let mut next = (*skeleton).clone();
 
-        let lab = 14.0;
+        let lab = 1.0;
 
         let legl = (anim_time as f32 * lab as f32).sin();
         let legr = (anim_time as f32 * lab as f32 + PI).sin();
@@ -30,9 +30,9 @@ impl Animation for RunAnimation {
         let footvertr = (anim_time as f32 * lab as f32 + PI * 0.4).sin().max(0.0);
 
         next.head.offset =
-            Vec3::new(0.0, skeleton_attr.head.0, skeleton_attr.head.1 + belt * 1.0) / 8.0;
+            Vec3::new(0.0, skeleton_attr.head.0, skeleton_attr.head.1 + belt * 1.0) * 1.02;
         next.head.ori = Quaternion::rotation_z(belt * 0.1) * Quaternion::rotation_x(0.3);
-        next.head.scale = Vec3::one() / 8.0;
+        next.head.scale = Vec3::one() * 1.02;
 
         next.upper_torso.offset = Vec3::new(
             0.0,
@@ -72,7 +72,7 @@ impl Animation for RunAnimation {
             skeleton_attr.hand.2,
         );
         next.hand_l.ori = Quaternion::rotation_z(0.0) * Quaternion::rotation_x(0.3 + legr * 0.5);
-        next.hand_l.scale = Vec3::one();
+        next.hand_l.scale = Vec3::one() * 1.02;
 
         next.hand_r.offset = Vec3::new(
             skeleton_attr.hand.0,
@@ -80,42 +80,42 @@ impl Animation for RunAnimation {
             skeleton_attr.hand.2,
         );
         next.hand_r.ori = Quaternion::rotation_z(0.0) * Quaternion::rotation_x(0.3 - legr * 0.5);
-        next.hand_r.scale = Vec3::one();
+        next.hand_r.scale = Vec3::one() * 1.02;
 
         next.leg_l.offset = Vec3::new(
             -skeleton_attr.leg.0,
             skeleton_attr.leg.1,
-            skeleton_attr.leg.2 + belt * 0.4,
-        ) / 8.0;
+            skeleton_attr.leg.2 + legl * 0.4,
+        ) * 1.02;
         next.leg_l.ori = Quaternion::rotation_z(0.0) * Quaternion::rotation_x(-legl * 0.3);
-        next.leg_l.scale = Vec3::one() / 8.0;
+        next.leg_l.scale = Vec3::one() * 1.02;
 
         next.leg_r.offset = Vec3::new(
             skeleton_attr.leg.0,
             skeleton_attr.leg.1,
-            skeleton_attr.leg.2 + belt * 0.4,
-        ) / 8.0;
+            skeleton_attr.leg.2 + legr * 0.4,
+        ) * 1.02;
         next.leg_r.ori = Quaternion::rotation_z(0.0) * Quaternion::rotation_x(-legr * 0.3);
-        next.leg_r.scale = Vec3::one() / 8.0;
+        next.leg_r.scale = Vec3::one() * 1.02;
 
         next.foot_l.offset = Vec3::new(
             -skeleton_attr.foot.0,
-            skeleton_attr.foot.1 - foothoril * 4.5 - 0.5,
-            skeleton_attr.foot.2 + footvertl * 6.0,
+            skeleton_attr.foot.1 + foothoril * -4.0,
+            skeleton_attr.foot.2 + footvertl * 3.0,
         ) / 8.0;
-        next.foot_l.ori = Quaternion::rotation_z(0.0) * Quaternion::rotation_x(0.0 - legl * 0.5);
+        next.foot_l.ori = Quaternion::rotation_z(0.0) * Quaternion::rotation_x(0.0);
         next.foot_l.scale = Vec3::one() / 8.0 * 0.98;
 
         next.foot_r.offset = Vec3::new(
             skeleton_attr.foot.0,
-            skeleton_attr.foot.1 - foothorir * 4.5 - 0.5,
-            skeleton_attr.foot.2 + footvertr * 6.0,
+            skeleton_attr.foot.1 + foothorir * -4.0,
+            skeleton_attr.foot.2 + footvertr * 3.0,
         ) / 8.0;
-        next.foot_r.ori = Quaternion::rotation_z(0.0) * Quaternion::rotation_x(0.0 - legr * 0.5);
+        next.foot_r.ori = Quaternion::rotation_z(0.0) * Quaternion::rotation_x(0.0);
         next.foot_r.scale = Vec3::one() / 8.0 * 0.98;
 
         next.torso.offset = Vec3::new(0.0, 0.0, 0.0);
-        next.torso.ori = Quaternion::rotation_z(0.0) * Quaternion::rotation_x(-0.3);
+        next.torso.ori = Quaternion::rotation_z(0.0) * Quaternion::rotation_x(-0.2);
         next.torso.scale = Vec3::one();
         next
     }
