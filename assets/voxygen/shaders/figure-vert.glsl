ba3688f29424c4e6dd929036f013f88211b6351b
Joshua Barretto <joshua.s.barretto@gmail.com>
2020-04-25 15:07:58 +0100

    Squashed figure vertices for better performance

--- assets/voxygen/shaders/figure-vert.glsl
+++ assets/voxygen/shaders/figure-vert.glsl
@@ -2,11 +2,10 @@
 
 #include <globals.glsl>
 
-in vec3 v_pos;
+in uint v_pos_norm;
 in vec3 v_norm;
-in vec3 v_col;
-in float v_ao;
-in uint v_bone_idx;
+in uint v_col;
+in uint v_ao_bone;
 
 layout (std140)
 uniform u_locals {
@@ -33,20 +32,27 @@ flat out vec3 f_norm;
 
 void main() {
 	// Pre-calculate bone matrix
-	mat4 combined_mat = model_mat * bones[v_bone_idx].bone_mat;
+	uint bone_idx = (v_ao_bone >> 2) & 0x3Fu;
+	mat4 combined_mat = model_mat * bones[bone_idx].bone_mat;
+
+	vec3 pos = vec3((uvec3(v_pos_norm) >> uvec3(0, 8, 16)) & uvec3(0xFFu)) - 128.0;
 
 	f_pos = (
 		combined_mat *
-		vec4(v_pos, 1)).xyz;
+		vec4(pos, 1)).xyz;
 
-	f_col = v_col;
+	f_col = vec3((uvec3(v_col) >> uvec3(0, 8, 16)) & uvec3(0xFFu)) / 255.0;
 
-	f_ao = v_ao;
+	f_ao = float(v_ao_bone & 0x3u) / 4.0;
+
+	// First 3 normals are negative, next 3 are positive
+	vec3 normals[6] = vec3[](vec3(-1,0,0), vec3(1,0,0), vec3(0,-1,0), vec3(0,1,0), vec3(0,0,-1), vec3(0,0,1));
+	vec3 norm = normals[(v_pos_norm >> 24) & 0x7u];
 
 	// Calculate normal here rather than for each pixel in the fragment shader
 	f_norm = normalize((
 		combined_mat *
-		vec4(v_norm, 0.0)
+		vec4(norm, 0.0)
 	).xyz);
 
 	gl_Position = all_mat * vec4(f_pos, 1);
