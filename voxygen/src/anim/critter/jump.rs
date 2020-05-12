ceb771bb2c0e4a8890eb8c3a03e60adf8154c41f
jshipsey <jshipsey18@gmail.com>
2020-05-11 21:43:57 -0400

    keep mobs from exploding whenever they're hit

--- voxygen/src/anim/critter/jump.rs
+++ voxygen/src/anim/critter/jump.rs
@@ -13,28 +13,28 @@ impl Animation for JumpAnimation {
         _global_time: Self::Dependency,
         _anim_time: f64,
         _rate: &mut f32,
-        _skeleton_attr: &CritterAttr,
+        skeleton_attr: &CritterAttr,
     ) -> Self::Skeleton {
         let mut next = (*skeleton).clone();
 
-        next.head.offset = Vec3::new(0.0, 0.0, 0.0) / 18.0;
-        next.head.ori = Quaternion::rotation_z(0.0);
+        next.head.offset = Vec3::new(0.0, skeleton_attr.head.0, skeleton_attr.head.1) / 18.0;
+        next.head.ori = Quaternion::rotation_z(0.8) * Quaternion::rotation_x(0.5);
         next.head.scale = Vec3::one() / 18.0;
 
-        next.chest.offset = Vec3::new(0.0, 0.0, 0.0) / 18.0;
-        next.chest.ori = Quaternion::rotation_x(0.0);
+        next.chest.offset = Vec3::new(0.0, skeleton_attr.chest.0, skeleton_attr.chest.1) / 18.0;
+        next.chest.ori = Quaternion::rotation_y(0.0);
         next.chest.scale = Vec3::one() / 18.0;
 
-        next.feet_f.offset = Vec3::new(0.0, 0.0, 0.0) / 18.0;
+        next.feet_f.offset = Vec3::new(0.0, skeleton_attr.feet_f.0, skeleton_attr.feet_f.1) / 18.0;
         next.feet_f.ori = Quaternion::rotation_z(0.0);
         next.feet_f.scale = Vec3::one() / 18.0;
 
-        next.feet_b.offset = Vec3::new(0.0, 0.0, 0.0) / 18.0;
+        next.feet_b.offset = Vec3::new(0.0, skeleton_attr.feet_b.0, skeleton_attr.feet_b.1) / 18.0;
         next.feet_b.ori = Quaternion::rotation_x(0.0);
         next.feet_b.scale = Vec3::one() / 18.0;
 
-        next.tail.offset = Vec3::new(0.0, 0.0, 0.0) / 18.0;
-        next.tail.ori = Quaternion::rotation_x(0.0);
+        next.tail.offset = Vec3::new(0.0, skeleton_attr.tail.0, skeleton_attr.tail.1) / 18.0;
+        next.tail.ori = Quaternion::rotation_y(0.0);
         next.tail.scale = Vec3::one() / 18.0;
 
         next
