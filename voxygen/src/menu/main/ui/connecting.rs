f576c38c050c841f5c0da46b7c2e05567ae55d4f
Imbris <imbrisf@gmail.com>
2020-06-03 01:23:51 -0400

    Add styling for Container, implement auth trust prompt, misc additions

--- voxygen/src/menu/main/ui/connecting.rs
+++ voxygen/src/menu/main/ui/connecting.rs
@@ -1,20 +1,19 @@
-use super::{IcedImgs as Imgs, Message};
+use super::{ConnectionState, IcedImgs as Imgs, Message};
 use crate::{
     i18n::Localization,
     ui::{
         fonts::IcedFonts as Fonts,
-        ice::{
-            component::neat_button,
-            widget::{image, BackgroundContainer, Image},
-            ButtonStyle, Element,
-        },
+        ice::{component::neat_button, style, widget::image, Element},
     },
 };
-use iced::{button, Color, Column, Container, HorizontalAlignment, Length, Row, Space, Text};
+use iced::{
+    button, Align, Color, Column, Container, HorizontalAlignment, Length, Row, Space, Text,
+};
 
 /// Connecting screen for the main menu
 pub struct Screen {
     cancel_button: button::State,
+    add_button: button::State,
 }
 
 // TODO: move to super and unify with identical login consts
@@ -25,6 +24,7 @@ impl Screen {
     pub fn new() -> Self {
         Self {
             cancel_button: Default::default(),
+            add_button: Default::default(),
         }
     }
 
@@ -34,13 +34,13 @@ impl Screen {
         imgs: &Imgs,
         bg_img: image::Handle,
         start: &std::time::Instant,
-        status_text: &str,
+        connection_state: &ConnectionState,
         version: &str,
         time: f32,
         i18n: &Localization,
     ) -> Element<Message> {
         let fade_msg = (time * 2.0).sin() * 0.5 + 0.51;
-        let button_style = ButtonStyle::new(imgs.button)
+        let button_style = style::button::Style::new(imgs.button)
             .hover_image(imgs.button_hover)
             .press_image(imgs.button_press)
             .text_color(TEXT_COLOR)
@@ -49,41 +49,99 @@ impl Screen {
         let version = Text::new(version)
             .size(fonts.cyri.scale(15)) // move version text size to const
             .width(Length::Fill)
-            .height(Length::Fill)
+            .height(if matches!(connection_state, ConnectionState::InProgress {..}){Length::Fill}else{Length::Shrink})
             .horizontal_alignment(HorizontalAlignment::Right);
 
-        let status = Text::new(status_text)
-            .size(fonts.alkhemi.scale(80))
-            .font(fonts.alkhemi.id)
-            .color(Color::from_rgba(1.0, 1.0, 1.0, fade_msg))
-            .width(Length::Fill);
+        let (middle, bottom) = match connection_state {
+            ConnectionState::InProgress { status } => {
+                let status = Text::new(status)
+                    .size(fonts.alkhemi.scale(80))
+                    .font(fonts.alkhemi.id)
+                    .color(Color::from_rgba(1.0, 1.0, 1.0, fade_msg))
+                    .width(Length::Fill);
 
-        let status = Row::with_children(vec![
-            Space::new(Length::Units(80), Length::Shrink).into(),
-            status.into(),
-        ]);
+                let status = Row::with_children(vec![
+                    Space::new(Length::Units(80), Length::Shrink).into(),
+                    status.into(),
+                ]);
 
-        let cancel = neat_button(
-            &mut self.cancel_button,
-            i18n.get("common.cancel"),
-            0.7,
-            button_style,
-            Some(Message::CancelConnect),
-        );
+                let cancel = neat_button(
+                    &mut self.cancel_button,
+                    i18n.get("common.cancel"),
+                    0.7,
+                    button_style,
+                    Some(Message::CancelConnect),
+                );
 
-        let cancel = Container::new(cancel)
-            .width(Length::Fill)
-            .height(Length::Units(fonts.cyri.scale(50)))
-            .center_x()
-            .padding(3);
+                let cancel = Container::new(cancel)
+                    .width(Length::Fill)
+                    .height(Length::Units(fonts.cyri.scale(50)))
+                    .center_x()
+                    .padding(3);
 
-        let content = Column::with_children(vec![version.into(), status.into(), cancel.into()])
+                (status.into(), cancel.into())
+            },
+            ConnectionState::AuthTrustPrompt { msg, .. } => {
+                let text = Text::new(msg).size(fonts.cyri.scale(25));
+
+                let cancel = neat_button(
+                    &mut self.cancel_button,
+                    i18n.get("common.cancel"),
+                    0.7,
+                    button_style,
+                    Some(Message::TrustPromptCancel),
+                );
+                let add = neat_button(
+                    &mut self.add_button,
+                    i18n.get("common.add"),
+                    0.7,
+                    button_style,
+                    Some(Message::TrustPromptAdd),
+                );
+
+                let content = Column::with_children(vec![
+                    text.into(),
+                    Container::new(
+                        Row::with_children(vec![cancel.into(), add.into()])
+                            .spacing(20)
+                            .height(Length::Units(25)),
+                    )
+                    .align_x(Align::End)
+                    .width(Length::Fill)
+                    .into(),
+                ])
+                .spacing(4)
+                .max_width(500)
+                .width(Length::Fill)
+                .height(Length::Fill);
+
+                let prompt_window = Container::new(content)
+                    // TODO: add borders
+                    .style(style::container::Style::Color((10, 10, 0, 255).into()))
+                    .padding(10);
+
+                let container = Container::new(prompt_window)
+                    .width(Length::Fill)
+                    .height(Length::Fill)
+                    .center_x()
+                    .center_y();
+
+                (
+                    container.into(),
+                    Space::new(Length::Fill, Length::Units(fonts.cyri.scale(15))).into(),
+                )
+            },
+        };
+
+        let content = Column::with_children(vec![version.into(), middle, bottom])
             .width(Length::Fill)
             .height(Length::Fill)
             .padding(3);
 
         // Note: could replace this with styling on iced's container since we aren't
         // using fixed aspect ratio
-        BackgroundContainer::new(Image::new(bg_img), content).into()
+        Container::new(content)
+            .style(style::container::Style::image(bg_img))
+            .into()
     }
 }
--- voxygen/src/ui/ice/renderer/widget/container.rs
+++ voxygen/src/ui/ice/renderer/widget/container.rs
@@ -1,23 +1,47 @@
-use super::super::IcedRenderer;
+use super::super::{super::Rotation, style, IcedRenderer, Primitive};
+use common::util::srgba_to_linear;
 use iced::{container, Element, Layout, Point, Rectangle};
 
 impl container::Renderer for IcedRenderer {
-    type Style = ();
+    type Style = style::container::Style;
 
     fn draw<M>(
         &mut self,
         defaults: &Self::Defaults,
-        _bounds: Rectangle,
+        bounds: Rectangle,
         cursor_position: Point,
-        _style_sheet: &Self::Style,
+        style_sheet: &Self::Style,
         content: &Element<'_, M, Self>,
         content_layout: Layout<'_>,
     ) -> Self::Output {
         let (content, mouse_interaction) =
             content.draw(self, defaults, content_layout, cursor_position);
 
-        // We may have more stuff here if styles are used
+        let prim = match style_sheet {
+            Self::Style::Image(handle, color) => {
+                let background = Primitive::Image {
+                    handle: (*handle, Rotation::None),
+                    bounds,
+                    color: *color,
+                };
 
-        (content, mouse_interaction)
+                Primitive::Group {
+                    primitives: vec![background, content],
+                }
+            },
+            Self::Style::Color(color) => {
+                let background = Primitive::Rectangle {
+                    bounds,
+                    linear_color: srgba_to_linear(color.map(|e| e as f32 / 255.0)),
+                };
+
+                Primitive::Group {
+                    primitives: vec![background, content],
+                }
+            },
+            Self::Style::None => content,
+        };
+
+        (prim, mouse_interaction)
     }
 }
