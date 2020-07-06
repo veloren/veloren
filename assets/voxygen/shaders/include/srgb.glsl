1ffbbda59be8cfb1e65faf32e5b5f674bff80220
Joshua Barretto <joshua.s.barretto@gmail.com>
2020-07-06 22:41:21 +0100

    Fixed sprite select flickering

--- assets/voxygen/shaders/include/srgb.glsl
+++ assets/voxygen/shaders/include/srgb.glsl
@@ -6,3 +6,11 @@ vec3 srgb_to_linear(vec3 srgb) {
 
     return mix(higher, lower, cutoff);
 }
+
+vec3 linear_to_srgb(vec3 linear) {
+    bvec3 cutoff = lessThan(linear, vec3(0.0031308));
+    vec3 higher = vec3(1.055) * pow(linear, vec3(1.0 / 2.4)) - vec3(0.055);
+    vec3 lower = linear * vec3(12.92);
+
+    return mix(higher, lower, cutoff);
+}
