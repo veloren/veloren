1e229e187e20eb19af6963ba927b5c25fabab646
scott-c <xodster@gmail.com>
2020-07-19 15:16:06 +0800

    Add fireball and bomb particle effects

--- assets/voxygen/shaders/particle-vert.glsl
+++ assets/voxygen/shaders/particle-vert.glsl
@@ -6,10 +6,7 @@
 in vec3 v_pos;
 in uint v_col;
 in uint v_norm_ao;
-in vec4 inst_mat0;
-in vec4 inst_mat1;
-in vec4 inst_mat2;
-in vec4 inst_mat3;
+in vec3 inst_pos;
 in float inst_time;
 in float inst_entropy;
 in int inst_mode;
@@ -22,7 +19,8 @@ out float f_light;
 
 const float SCALE = 1.0 / 11.0;
 
-float PHI = 1.61803398874989484820459;  // Φ = Golden Ratio   
+// Φ = Golden Ratio
+float PHI = 1.61803398874989484820459;
 
 float gold_noise(in vec2 xy, in float seed){
        return fract(tan(distance(xy * PHI, xy) * seed) * xy.x);
@@ -31,46 +29,54 @@ float gold_noise(in vec2 xy, in float seed){
 // Modes
 const int SMOKE = 0;
 const int FIRE = 1;
-const int FLAMETHROWER = 2;
+const int GUN_POWDER_SPARK = 2;
+
+// meters per second
+const float earth_gravity = 9.807;
+
+mat4 translate(vec3 vec){
+    return mat4(
+        vec4(1.0,   0.0,   0.0,   0.0),
+        vec4(0.0,   1.0,   0.0,   0.0),
+        vec4(0.0,   0.0,   1.0,   0.0),
+        vec4(vec.x, vec.y, vec.z, 1.0)
+    );
+}
 
 void main() {
-	mat4 inst_mat;
-	inst_mat[0] = inst_mat0;
-	inst_mat[1] = inst_mat1;
-	inst_mat[2] = inst_mat2;
-	inst_mat[3] = inst_mat3;
+	mat4 inst_mat = translate(inst_pos);
 
 	float rand1 = gold_noise(vec2(0.0, 0.0), inst_entropy);
-	float rand2 = gold_noise(vec2(1.0, 1.0), inst_entropy);
-	float rand3 = gold_noise(vec2(2.0, 2.0), inst_entropy);
-	float rand4 = gold_noise(vec2(3.0, 3.0), inst_entropy);
-	float rand5 = gold_noise(vec2(4.0, 4.0), inst_entropy);
-	float rand6 = gold_noise(vec2(5.0, 5.0), inst_entropy);
+	float rand2 = gold_noise(vec2(10.0, 10.0), inst_entropy);
+	float rand3 = gold_noise(vec2(20.0, 20.0), inst_entropy);
+	float rand4 = gold_noise(vec2(30.0, 30.0), inst_entropy);
+	float rand5 = gold_noise(vec2(40.0, 40.0), inst_entropy);
+	float rand6 = gold_noise(vec2(50.0, 50.0), inst_entropy);
 
 	vec3 inst_vel = vec3(0.0, 0.0, 0.0);
-	vec3 inst_pos = vec3(0.0, 0.0, 0.0);
+	vec3 inst_pos2 = vec3(0.0, 0.0, 0.0);
 	vec3 inst_col = vec3(1.0, 1.0, 1.0);
 
 	if (inst_mode == SMOKE) {
 		inst_col = vec3(1.0, 1.0, 1.0);
 		inst_vel = vec3(rand1 * 0.2 - 0.1, rand2 * 0.2 - 0.1, 1.0 + rand3);
-		inst_pos = vec3(rand4 * 5.0 - 2.5, rand5 * 5.0 - 2.5, 0.0);
+		inst_pos2 = vec3(rand4 * 5.0 - 2.5, rand5 * 5.0 - 2.5, 0.0);
 	} else if (inst_mode == FIRE) {
 		inst_col = vec3(1.0, 1.0 * inst_entropy, 0.0);
 		inst_vel = vec3(rand1 * 0.2 - 0.1, rand2 * 0.2 - 0.1, 4.0 + rand3);
-		inst_pos = vec3(rand4 * 5.0 - 2.5, rand5 * 5.0 - 2.5, 0.0);
-	} else if (inst_mode == FLAMETHROWER) {
-		// TODO: velocity based on attack range, angle and parent orientation.
-		inst_col = vec3(1.0, 1.0 * inst_entropy, 0.0);
-		inst_vel = vec3(rand1 * 0.1, rand2 * 0.1, 3.0 + rand3);
-		inst_pos = vec3(rand4 * 5.0 - 2.5, rand5 * 5.0 - 2.5, 0.0);
+		inst_pos2 = vec3(rand4 * 5.0 - 2.5, rand5 * 5.0 - 2.5, 0.0);
+	} else if (inst_mode == GUN_POWDER_SPARK) {
+		inst_col = vec3(1.0, 1.0, 0.0);
+		inst_vel = vec3(rand2 * 2.0 - 1.0, rand1 * 2.0 - 1.0, 5.0 + rand3);
+		inst_vel -= vec3(0.0, 0.0, earth_gravity * (tick.x - inst_time));
+		inst_pos2 = vec3(0.0, 0.0, 0.0);
 	} else {
 		inst_col = vec3(rand1, rand2, rand3);
 		inst_vel = vec3(rand4, rand5, rand6);
-		inst_pos = vec3(rand1, rand2, rand3);
+		inst_pos2 = vec3(rand1, rand2, rand3);
 	}
 
-	f_pos = (inst_mat * vec4((v_pos + inst_pos) * SCALE, 1)).xyz;
+	f_pos = (inst_mat * vec4((v_pos + inst_pos2) * SCALE, 1)).xyz;
 
 	f_pos += inst_vel * (tick.x - inst_time);
 
