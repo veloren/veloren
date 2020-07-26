014a5eba8a45080701610ea0faaf7b2d4a976164
Joshua Barretto <joshua.s.barretto@gmail.com>
2020-07-26 12:41:46 +0100

    Added stalagtites

--- world/src/index.rs
+++ world/src/index.rs
@@ -1,10 +1,11 @@
 use crate::site::Site;
 use common::store::{Id, Store};
+use noise::{NoiseFn, Seedable, SuperSimplex};
 
-#[derive(Default)]
 pub struct Index {
     pub seed: u32,
     pub time: f32,
+    pub noise: Noise,
     pub sites: Store<Site>,
 }
 
@@ -12,7 +13,21 @@ impl Index {
     pub fn new(seed: u32) -> Self {
         Self {
             seed,
-            ..Self::default()
+            time: 0.0,
+            noise: Noise::new(seed),
+            sites: Store::default(),
+        }
+    }
+}
+
+pub struct Noise {
+    pub cave_nz: SuperSimplex,
+}
+
+impl Noise {
+    fn new(seed: u32) -> Self {
+        Self {
+            cave_nz: SuperSimplex::new().set_seed(seed),
         }
     }
 }
