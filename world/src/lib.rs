331a61e426b536a0d16a9dee521998a99536010d
Joshua Barretto <joshua.s.barretto@gmail.com>
2020-08-12 15:04:17 +0100

    Improved spawn rates of castles and cave monsters

--- world/src/lib.rs
+++ world/src/lib.rs
@@ -230,7 +230,14 @@ impl World {
         }
 
         // Apply layer supplement
-        layer::apply_caves_supplement(&mut rng, chunk_wpos2d, sample_get, &chunk, &self.index, &mut supplement);
+        layer::apply_caves_supplement(
+            &mut rng,
+            chunk_wpos2d,
+            sample_get,
+            &chunk,
+            &self.index,
+            &mut supplement,
+        );
 
         // Apply site supplementary information
         sim_chunk.sites.iter().for_each(|site| {
