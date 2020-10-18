61a0aa2055f55782de05b00c0161fcbf618f8040
Imbris <imbrisf@gmail.com>
2020-10-18 17:30:17 -0400

    Fix tooltip border images, fade tooltips properly, improve tooltip positioning

---
+++ voxygen/src/ui/ice/renderer/widget/tooltip.rs
@@ -0,0 +1,23 @@
+use super::super::{super::widget::tooltip, IcedRenderer, Primitive};
+use iced::{Element, Layout, Point};
+
+impl tooltip::Renderer for IcedRenderer {
+    fn draw<M>(
+        &mut self,
+        alpha: f32,
+        defaults: &Self::Defaults,
+        cursor_position: Point,
+        content: &Element<'_, M, Self>,
+        content_layout: Layout<'_>,
+    ) -> Self::Output {
+        let (primitive, cursor_interaction) =
+            content.draw(self, defaults, content_layout, cursor_position);
+        (
+            Primitive::Opacity {
+                alpha,
+                content: Box::new(primitive),
+            },
+            cursor_interaction,
+        )
+    }
+}
