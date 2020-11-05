8da2871897a905f5edc6552b4edaf2b427af9dc6
Monty Marz <m.marzouq@gmx.de>
2020-11-05 21:02:54 +0100

    potion buff

--- voxygen/src/hud/img_ids.rs
+++ voxygen/src/hud/img_ids.rs
@@ -353,6 +353,7 @@ image_ids! {
         // Buffs
         buff_plus_0: "voxygen.element.icons.de_buffs.buff_plus_0",
         buff_saturation_0: "voxygen.element.icons.de_buffs.buff_saturation_0",
+        buff_potion_0: "voxygen.element.icons.de_buffs.buff_potion_0",
 
         // Debuffs
         debuff_skull_0: "voxygen.element.icons.de_buffs.debuff_skull_0",
