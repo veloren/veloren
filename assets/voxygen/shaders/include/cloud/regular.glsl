e13694164315103efdf8ca18dcb0dddc005028ca
Joshua Barretto <joshua.s.barretto@gmail.com>
2020-11-17 20:43:33 +0000

    Resolved or patches various water shader issues

--- assets/voxygen/shaders/include/cloud/regular.glsl
+++ assets/voxygen/shaders/include/cloud/regular.glsl
@@ -74,8 +74,8 @@ vec4 cloud_at(vec3 pos, float dist, out vec3 emission) {
             float cloud_tendency_x = cloud_tendency_at(pos.xy + vec2(100, 0));
             float cloud_tendency_y = cloud_tendency_at(pos.xy + vec2(0, 100));
             vec3 cloud_norm = vec3(
-                (cloud_tendency - cloud_tendency_x) * 6,
-                (cloud_tendency - cloud_tendency_y) * 6,
+                (cloud_tendency - cloud_tendency_x) * 4,
+                (cloud_tendency - cloud_tendency_y) * 4,
                 (pos.z - cloud_attr.x) / 250 + turb_noise + 0.25
             );
             sun_access = mix(max(dot(-sun_dir.xyz, cloud_norm) + 0.0, 0.025), sun_access, 0.25);
@@ -98,7 +98,7 @@ vec4 cloud_at(vec3 pos, float dist, out vec3 emission) {
         float z = clamp(pos.z, 0, 10000);
         float emission_alt = 4000.0;
         #if (CLOUD_MODE >= CLOUD_MODE_LOW)
-            emission_alt += (noise_3d(vec3(wind_pos.xy * 0.00003 + cloud_tendency * 0.2, time_of_day.x * 0.0001)) - 0.5) * 8000;
+            emission_alt += (noise_3d(vec3(wind_pos.xy * 0.00003 + cloud_tendency * 0.2, time_of_day.x * 0.0001)) - 0.5) * 6000;
         #endif
         float tail = (texture(t_noise, wind_pos.xy * 0.00005).x - 0.5) * 10 + (z - emission_alt) * 0.001;
         vec3 emission_col = vec3(0.6 + tail * 0.6, 1.0, 0.3 + tail * 0.2);
@@ -135,12 +135,12 @@ const float DIST_CAP = 50000;
 
 const float STEP_SCALE = DIST_CAP / (10.0 * float(QUALITY));
 
-float step_to_dist(float step) {
-    return pow(step, 2) * STEP_SCALE;
+float step_to_dist(float step, float quality) {
+    return pow(step, 2) * STEP_SCALE / quality;
 }
 
-float dist_to_step(float dist) {
-    return pow(dist / STEP_SCALE, 0.5);
+float dist_to_step(float dist, float quality) {
+    return pow(dist / STEP_SCALE * quality, 0.5);
 }
 
 vec3 get_cloud_color(vec3 surf_color, vec3 dir, vec3 origin, const float time_of_day, float max_dist, const float quality) {
@@ -199,7 +199,7 @@ vec3 get_cloud_color(vec3 surf_color, vec3 dir, vec3 origin, const float time_of
             sky_color * moon_access * scatter_factor * get_moon_brightness();
 
         ldist = cdist;
-        cdist = step_to_dist(trunc(dist_to_step(cdist - 0.25)));
+        cdist = step_to_dist(trunc(dist_to_step(cdist - 0.25, quality)), quality);
     }
 
     return surf_color;
