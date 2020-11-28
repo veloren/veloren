46221a1620e5c3ddcd042e50418b15a8063629bd
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2020-11-28 17:45:58 +0300

    nix: fix build failing when on tag, combine nixpkgs.nix and common.nix

--- nix/common.nix
+++ nix/common.nix
@@ -1,5 +1,24 @@
-{ pkgs }:
-with pkgs;
+{ system, sources ? import ./sources.nix { inherit system; }
+, nixpkgs ? sources.nixpkgs }:
+let
+  mozPkgs = import "${sources.nixpkgsMoz}/package-set.nix" {
+    pkgs = import nixpkgs { inherit system; };
+  };
+  rustChannel = mozPkgs.rustChannelOf {
+    rustToolchain = ../rust-toolchain;
+    hash = "sha256-P4FTKRe0nM1FRDV0Q+QY2WcC8M9IR7aPMMLWDfv+rEk=";
+  };
+  pkgs = import nixpkgs {
+    inherit system;
+    overlays = [
+      (self: super: {
+        rustc = rustChannel.rust;
+        inherit (rustChannel)
+        ;
+      })
+    ];
+  };
+in with pkgs;
 let
   xorgLibraries = with xorg; [ libX11 libXcursor libXrandr libXi ];
   otherLibraries = [
@@ -19,4 +38,4 @@ let
     veloren-network = [ pkg-config openssl ];
     veloren-voxygen = [ atk cairo glib gtk3 pango ];
   };
-in { inherit neededLibPaths crateDeps; }
+in { inherit pkgs neededLibPaths crateDeps; }
