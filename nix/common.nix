8b0f060def30e1a3f586b454996dabe8d79b4d2e
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2020-11-29 02:02:46 +0300

    nix: auto setup cachix cache, do git-lfs check in Nix, link assets dir directly (no assets package) and other minor improvements

--- nix/common.nix
+++ nix/common.nix
@@ -1,12 +1,11 @@
-{ system, sources ? import ./sources.nix { inherit system; }
-, nixpkgs ? sources.nixpkgs }:
+{ system, nixpkgsMoz, nixpkgs }:
 let
-  mozPkgs = import "${sources.nixpkgsMoz}/package-set.nix" {
+  mozPkgs = import "${nixpkgsMoz}/package-set.nix" {
     pkgs = import nixpkgs { inherit system; };
   };
   rustChannel = mozPkgs.rustChannelOf {
     rustToolchain = ../rust-toolchain;
-    hash = "sha256-P4FTKRe0nM1FRDV0Q+QY2WcC8M9IR7aPMMLWDfv+rEk=";
+    sha256 = "sha256-P4FTKRe0nM1FRDV0Q+QY2WcC8M9IR7aPMMLWDfv+rEk=";
   };
   pkgs = import nixpkgs {
     inherit system;
@@ -29,7 +28,7 @@ let
        so I disabled it by default
     */
   ];
-  neededLibPaths = lib.concatStringsSep ":"
+  neededLibPathsVoxygen = lib.concatStringsSep ":"
     (map (p: "${p}/lib") (xorgLibraries ++ otherLibraries));
 
   crateDeps = {
@@ -38,4 +37,4 @@ let
     veloren-network = [ pkg-config openssl ];
     veloren-voxygen = [ atk cairo glib gtk3 pango ];
   };
-in { inherit pkgs neededLibPaths crateDeps; }
+in { inherit pkgs neededLibPathsVoxygen crateDeps; }
