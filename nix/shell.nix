fde9bd2617abd33e79606679cb2c09260b41453d
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2020-11-30 03:48:00 +0300

    nix: dont run git-lfs shell hook if its already initialized, add experimental nix-bundle bundling command, add nixGL to shell.nix for non-nixos users, switch to nixpkgs-fmt for formatting, update niv deps and many other minor improvements

--- nix/shell.nix
+++ nix/shell.nix
@@ -1,25 +1,51 @@
-{ nixpkgs ? <nixpkgs>, sources ? import ./sources.nix { }
-, system ? builtins.currentSystem }:
+{ nixpkgs ? <nixpkgs>
+, sources ? import ./sources.nix { }
+, system ? builtins.currentSystem
+, nvidia ? false
+}:
+let common = import ./common.nix { inherit nixpkgs system sources; };
+in
+with common.pkgs;
 let
-  common = import ./common.nix {
-    inherit nixpkgs system;
-    inherit (sources) nixpkgsMoz;
-  };
+  nixGLPackages = ((with nixGL; [ nixGLIntel ]) ++ (lib.optional nvidia
+    (with nixGL; [ nixGLNvidia nixGLNvidiaBumblebee ])));
 
-  crate2nix =
-    common.pkgs.callPackage sources.crate2nix { inherit (common) pkgs; };
-in with common.pkgs;
+  getAllCratesDeps = name:
+    (lib.concatLists
+      (map (attrset: attrset."${name}") (lib.attrValues common.crateDeps)));
+
+  bundleCrate = writeScriptBin "bundleCrate" ''
+    #!${stdenv.shell}
+    ${nix-bundle}/bin/nix-bundle "(pkgs.callPackage ./nix/default.nix { }).$1" /bin/$1
+  '';
+in
+with common;
 mkShell {
   name = "veloren-shell";
-  nativeBuildInputs =
-    [ git git-lfs niv nixfmt crate2nix cargo rustc rustfmt clippy cachix ];
-  buildInputs = lib.concatLists (lib.attrValues common.crateDeps);
+  nativeBuildInputs = [
+    bundleCrate
+    git
+    git-lfs
+    niv
+    nixpkgs-fmt
+    crate2nix
+    cargo
+    rustc
+    rustfmt
+    clippy
+    cachix
+  ] ++ nixGLPackages ++ (getAllCratesDeps "nativeBuildInputs");
+  buildInputs = getAllCratesDeps "buildInputs";
   shellHook = ''
+    # Setup our cachix "substituter"
     export NIX_CONFIG="
       substituters = https://cache.nixos.org https://veloren-nix.cachix.org
       trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= veloren-nix.cachix.org-1:zokfKJqVsNV6kI/oJdLF6TYBdNPYGSb+diMVQPn/5Rc=
     "
-    export LD_LIBRARY_PATH=${common.neededLibPathsVoxygen}
-    git lfs install --local && git lfs fetch && git lfs checkout
+    # We need this so that Voxygen runs
+    export LD_LIBRARY_PATH=${lib.makeLibraryPath voxygenNeededLibs}
+
+    # No need to install git-lfs and run fetch / checkout commands if we have it setup
+    [ "$(${file}/bin/file --mime-type ${gitLfsCheckFile})" = "${gitLfsCheckFile}: image/png" ] || (git lfs install --local && git lfs fetch && git lfs checkout)
   '';
 }
