3c6db5e10b991a7b355a347423a5b31e747a0f7f
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2020-12-11 18:00:16 +0300

    nix: refactor crateDeps in common to look better

--- nix/common.nix
+++ nix/common.nix
@@ -20,24 +20,16 @@ in
 with pkgs;
 let
   # deps that crates need (for compiling)
-  crateDeps = {
-    libudev-sys = {
-      buildInputs = [ libudev ];
-      nativeBuildInputs = [ pkg-config ];
+  crateDeps =
+    let
+      makeDeps = b: n: { buildInputs = b; nativeBuildInputs = n; };
+    in
+    {
+      libudev-sys = makeDeps [ libudev ] [ pkg-config ];
+      alsa-sys = makeDeps [ alsaLib ] [ pkg-config ];
+      veloren-network = makeDeps [ openssl ] [ pkg-config ];
+      veloren-voxygen = makeDeps [ xorg.libxcb ] [ ];
     };
-    alsa-sys = {
-      buildInputs = [ alsaLib ];
-      nativeBuildInputs = [ pkg-config ];
-    };
-    veloren-network = {
-      buildInputs = [ openssl ];
-      nativeBuildInputs = [ pkg-config ];
-    };
-    veloren-voxygen = {
-      buildInputs = [ xorg.libxcb ];
-      nativeBuildInputs = [ ];
-    };
-  };
 
   # deps that voxygen needs to function
   # FIXME: Wayland doesn't work (adding libxkbcommon, wayland and wayland-protocols results in a panic)
