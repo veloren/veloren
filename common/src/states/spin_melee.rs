d2b87c4327791787e75ebf555688743a47564ed3
Sam <samuelkeiffer@gmail.com>
2021-01-21 17:45:03 -0500

    Addressed comments.

--- common/src/states/spin_melee.rs
+++ common/src/states/spin_melee.rs
@@ -7,7 +7,6 @@ use crate::{
     },
     Damage, DamageSource, GroupTarget, Knockback,
 };
-use inline_tweak::*;
 use serde::{Deserialize, Serialize};
 use std::time::Duration;
 use vek::Vec3;
@@ -31,8 +30,8 @@ pub struct StaticData {
     pub energy_cost: u32,
     /// Whether spin state is infinite
     pub is_infinite: bool,
-    /// Used to maintain classic axe spin physics
-    pub is_helicopter: Helicopter,
+    /// Used to dictate how movement functions in this state
+    pub movement_behavior: MovementBehavior,
     /// Whether the state can be interrupted by other abilities
     pub is_interruptible: bool,
     /// Used for forced forward movement
@@ -62,15 +61,14 @@ impl CharacterBehavior for Data {
     fn behavior(&self, data: &JoinData) -> StateUpdate {
         let mut update = StateUpdate::from(data);
 
-        match self.static_data.is_helicopter {
-            Helicopter::No => {},
-            Helicopter::Yes => {
+        match self.static_data.movement_behavior {
+            MovementBehavior::ForwardGround => {},
+            MovementBehavior::AxeHover => {
                 let new_vel_z = update.vel.0.z + GRAVITY * data.dt.0 * 0.5;
                 update.vel.0 = Vec3::new(0.0, 0.0, new_vel_z) + data.inputs.move_dir * 5.0;
             },
-            Helicopter::Golem => {
-                update.vel.0 =
-                    Vec3::new(0.0, 0.0, tweak!(20.0)) + *data.inputs.look_dir * tweak!(25.0);
+            MovementBehavior::GolemHover => {
+                update.vel.0 = Vec3::new(0.0, 0.0, 20.0) + *data.inputs.look_dir * 25.0;
             },
         }
 
@@ -124,7 +122,10 @@ impl CharacterBehavior for Data {
                         knockback: Knockback::Away(self.static_data.knockback),
                     });
                 } else if self.timer < self.static_data.swing_duration {
-                    if matches!(self.static_data.is_helicopter, Helicopter::No) {
+                    if matches!(
+                        self.static_data.movement_behavior,
+                        MovementBehavior::ForwardGround
+                    ) {
                         handle_forced_movement(
                             data,
                             &mut update,
@@ -204,8 +205,8 @@ impl CharacterBehavior for Data {
 }
 
 #[derive(Copy, Clone, Debug, PartialEq, Serialize, Deserialize)]
-pub enum Helicopter {
-    No,
-    Yes,
-    Golem,
+pub enum MovementBehavior {
+    ForwardGround,
+    AxeHover,
+    GolemHover,
 }
