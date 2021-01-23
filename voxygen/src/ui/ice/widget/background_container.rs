e818403dcbb7b6dcb891a16b5a6e8192376f0c05
Imbris <imbrisf@gmail.com>
2021-01-23 01:06:15 -0500

    Update winit and iced

--- voxygen/src/ui/ice/widget/background_container.rs
+++ voxygen/src/ui/ice/widget/background_container.rs
@@ -258,7 +258,7 @@ where
         messages: &mut Vec<M>,
         renderer: &R,
         clipboard: Option<&dyn Clipboard>,
-    ) {
+    ) -> iced::event::Status {
         self.content.on_event(
             event,
             layout.children().next().unwrap(),
@@ -266,7 +266,7 @@ where
             messages,
             renderer,
             clipboard,
-        );
+        )
     }
 
     fn draw(
