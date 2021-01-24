50525f36c91396a5af4fa31fa0f4f0caab1e17dd
Scott Williams <swilliamsgames@gmail.com>
2021-01-24 19:45:44 +0000

    Changed some recipes to use other gems. Removed DELETE statements from migration.

--- server/src/migrations/2021-01-24-141845_shinygem-to-diamond/down.sql
+++ server/src/migrations/2021-01-24-141845_shinygem-to-diamond/down.sql
@@ -1,8 +1,2 @@
 UPDATE item
 SET item_definition_id = 'common.items.weapons.crafting.shiny_gem' WHERE item_definition_id = 'common.items.crafting_ing.diamond';
-
-DELETE FROM item WHERE item_definition_id = 'common.items.crafting_ing.ruby';
-DELETE FROM item WHERE item_definition_id = 'common.items.crafting_ing.emerald';
-DELETE FROM item WHERE item_definition_id = 'common.items.crafting_ing.sapphire';
-DELETE FROM item WHERE item_definition_id = 'common.items.crafting_ing.topaz';
-DELETE FROM item WHERE item_definition_id = 'common.items.crafting_ing.amethyst';
