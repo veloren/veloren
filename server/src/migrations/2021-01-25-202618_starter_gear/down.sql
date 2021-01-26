b6892261733df82cc28b5b237d6b981fda430b92
jiminycrick <jemelkonian@gmail.com>
2021-01-25 20:39:13 -0800

    Add migration for starter gear

---
+++ server/src/migrations/2021-01-25-202618_starter_gear/down.sql
@@ -0,0 +1,10 @@
+UPDATE item
+SET item_definition_id = 'common.items.armor.starter.glider' WHERE item_definition_id = 'common.items.glider.glider_cloverleaf';
+UPDATE item
+SET item_definition_id = 'common.items.armor.starter.lantern' WHERE item_definition_id = 'common.items.lantern.black_0';
+UPDATE item
+SET item_definition_id = 'common.items.armor.starter.rugged_chest' WHERE item_definition_id = 'common.items.armor.chest.rugged_chest';
+UPDATE item
+SET item_definition_id = 'common.items.armor.starter.rugged_pants' WHERE item_definition_id = 'common.items.armor.pants.rugged_pants';
+UPDATE item
+SET item_definition_id = 'common.items.armor.starter.sandals_0' WHERE item_definition_id = 'common.items.armor.foot.sandals_0';
