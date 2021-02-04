99a3e02c73468f3b5c96ebe197ee9806c4d86be2
Imbris <imbrisf@gmail.com>
2021-02-03 22:32:50 -0500

    fix test server

--- server/src/test_world.rs
+++ server/src/test_world.rs
@@ -52,7 +52,8 @@ impl World {
         let (x, y) = chunk_pos.map(|e| e.to_le_bytes()).into_tuple();
         let mut rng = SmallRng::from_seed([
             x[0], x[1], x[2], x[3], y[0], y[1], y[2], y[3], x[0], x[1], x[2], x[3], y[0], y[1],
-            y[2], y[3],
+            y[2], y[3], x[0], x[1], x[2], x[3], y[0], y[1], y[2], y[3], x[0], x[1], x[2], x[3],
+            y[0], y[1], y[2], y[3],
         ]);
         let height = rng.gen::<i32>() % 8;
 
