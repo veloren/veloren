86d1b007351dba7d2aa32a60c6567dc6ad75bf2d
Caelan <6938042-clango@users.noreply.gitlab.com>
2021-02-14 23:47:28 -0800

    Clean and update dependencies
     * Remove tweak feature
     * Remove const-tweaker
     * Update tiny_http
     * Update bitvec to 0.21.0
     * Downgrade euc to avoid conflict with vek 0.12.0
     * Require exactly vek 0.12.0
     * Update all other dependencies automatically based on these changes
     * Update gilrs to latest at the request of Ada Lovegirls
     * Update meshing benchmarks to new criterion API

--- voxygen/benches/meshing_benchmark.rs
+++ voxygen/benches/meshing_benchmark.rs
@@ -2,7 +2,7 @@ use common::{
     terrain::{Block, SpriteKind, TerrainGrid},
     vol::SampleVol,
 };
-use criterion::{black_box, criterion_group, criterion_main, Benchmark, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use std::sync::Arc;
 use vek::*;
 use veloren_voxygen::{mesh::Meshable, scene::terrain::BlocksOfInterest};
@@ -63,81 +63,82 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         (volume, aabb)
     };
 
+    let mut meshing_benches = c.benchmark_group("meshing");
+    // Lower sample size to save time
+    meshing_benches.sample_size(15);
     // Test speed of cloning voxel sample into a flat array
     let (volume, range) = sample(Vec2::new(1, 1));
-    c.bench(
-        "meshing",
-        Benchmark::new("copying 1,1 into flat array", move |b| {
-            b.iter(|| {
-                let mut flat = vec![Block::air(SpriteKind::Empty); range.size().product() as usize];
-                let mut i = 0;
-                let mut volume = volume.cached();
-                for x in 0..range.size().w {
-                    for y in 0..range.size().h {
-                        for z in 0..range.size().d {
-                            flat[i] = *volume.get(range.min + Vec3::new(x, y, z)).unwrap();
-                            i += 1;
-                        }
+    meshing_benches.bench_function("copying 1,1 into flat array", move |b| {
+        b.iter(|| {
+            let mut flat = vec![Block::air(SpriteKind::Empty); range.size().product() as usize];
+            let mut i = 0;
+            let mut volume = volume.cached();
+            for x in 0..range.size().w {
+                for y in 0..range.size().h {
+                    for z in 0..range.size().d {
+                        flat[i] = *volume.get(range.min + Vec3::new(x, y, z)).unwrap();
+                        i += 1;
                     }
                 }
-                /*let (w, h, d) = range.size().into_tuple();
-                for (chunk_key, chunk) in volume.iter() {
-                    let chunk_pos = volume.key_pos(chunk_key);
-                    let min = chunk_pos.map2(
-                        Vec2::new(range.min.x, range.min.y),
-                        |cmin: i32, rmin: i32| (rmin - cmin).max(0),
-                    );
-                    // Chunk not in area of interest
-                    if min
-                        .map2(TerrainGrid::chunk_size(), |m, size| m >= size as i32)
-                        .reduce_and()
-                    {
-                        // TODO: comment after ensuing no panics
-                        panic!("Shouldn't happen in this case");
-                        continue;
-                    }
-                    let min = min.map(|m| m.min(31));
-                    // TODO: Don't hardcode 31
-                    let max = chunk_pos.map2(Vec2::new(range.max.x, range.max.y), |cmin, rmax| {
-                        (rmax - cmin).min(31)
-                    });
-                    if max.map(|m| m < 0).reduce_and() {
-                        panic!("Shouldn't happen in this case: {:?}", max);
-                        continue;
-                    }
-                    let max = max.map(|m| m.max(0));
-                    // Add z dims
-                    let min = Vec3::new(min.x, min.y, range.min.z);
-                    let max = Vec3::new(max.x, max.y, range.max.z);
-                    // Offset of chunk in sample being cloned
-                    let offset = Vec3::new(
-                        chunk_pos.x - range.min.x,
-                        chunk_pos.y - range.min.y,
-                        -range.min.z,
-                    );
-                    for (pos, &block) in chunk.vol_iter(min, max) {
-                        let pos = pos + offset;
-                        flat[(w * h * pos.z + w * pos.y + pos.x) as usize] = block;
-                    }
-                }*/
-                black_box(flat);
-            });
-        })
-        // Lower sample size to save time
-        .sample_size(15),
-    );
+            }
+
+            /*let (w, h, d) = range.size().into_tuple();
+            for (chunk_key, chunk) in volume.iter() {
+                let chunk_pos = volume.key_pos(chunk_key);
+                let min = chunk_pos.map2(
+                    Vec2::new(range.min.x, range.min.y),
+                    |cmin: i32, rmin: i32| (rmin - cmin).max(0),
+                );
+                // Chunk not in area of interest
+                if min
+                    .map2(TerrainGrid::chunk_size(), |m, size| m >= size as i32)
+                    .reduce_and()
+                {
+                    // TODO: comment after ensuing no panics
+                    panic!("Shouldn't happen in this case");
+                    continue;
+                }
+                let min = min.map(|m| m.min(31));
+                // TODO: Don't hardcode 31
+                let max = chunk_pos.map2(Vec2::new(range.max.x, range.max.y), |cmin, rmax| {
+                    (rmax - cmin).min(31)
+                });
+                if max.map(|m| m < 0).reduce_and() {
+                    panic!("Shouldn't happen in this case: {:?}", max);
+                    continue;
+                }
+                let max = max.map(|m| m.max(0));
+                // Add z dims
+                let min = Vec3::new(min.x, min.y, range.min.z);
+                let max = Vec3::new(max.x, max.y, range.max.z);
+                // Offset of chunk in sample being cloned
+                let offset = Vec3::new(
+                    chunk_pos.x - range.min.x,
+                    chunk_pos.y - range.min.y,
+                    -range.min.z,
+                );
+                for (pos, &block) in chunk.vol_iter(min, max) {
+                    let pos = pos + offset;
+                    flat[(w * h * pos.z + w * pos.y + pos.x) as usize] = block;
+                }
+            } */
+
+            black_box(flat);
+        });
+    });
 
     for x in 1..GEN_SIZE - 1 {
         for y in 1..GEN_SIZE - 1 {
             let (volume, range) = sample(Vec2::new(x, y));
-            c.bench(
-                "meshing",
-                Benchmark::new(&format!("Terrain mesh {}, {}", x, y), move |b| {
-                    b.iter(|| volume.generate_mesh(black_box((range, Vec2::new(8192, 8192), &BlocksOfInterest::default()))))
+            meshing_benches.bench_function(&format!("Terrain mesh {}, {}", x, y), move |b| {
+                b.iter(|| {
+                    volume.generate_mesh(black_box((
+                        range,
+                        Vec2::new(8192, 8192),
+                        &BlocksOfInterest::default(),
+                    )))
                 })
-                // Lower sample size to save time
-                .sample_size(15),
-            );
+            });
         }
     }
 }
