e4cc802d6c51c0a42b4139cc51e26924662427a7
Sam <samuelkeiffer@gmail.com>
2021-03-01 23:27:20 -0500

    Migration for missed backpacks

---
+++ server/src/migrations/2021-03-02-041950_fix-other-backpacks/up.sql
@@ -0,0 +1,22 @@
+UPDATE item
+SET item_definition_id = 'common.items.armor.misc.bag.heavy_seabag' WHERE item_definition_id = 'common.items.armor.bag.heavy_seabag';
+UPDATE item
+SET item_definition_id = 'common.items.armor.misc.bag.knitted_red_pouch' WHERE item_definition_id = 'common.items.armor.bag.knitted_red_pouch';
+UPDATE item
+SET item_definition_id = 'common.items.armor.misc.bag.liana_kit' WHERE item_definition_id = 'common.items.armor.bag.liana_kit';
+UPDATE item
+SET item_definition_id = 'common.items.armor.misc.bag.mindflayer_spellbag' WHERE item_definition_id = 'common.items.armor.bag.mindflayer_spellbag';
+UPDATE item
+SET item_definition_id = 'common.items.armor.misc.bag.reliable_backpack' WHERE item_definition_id = 'common.items.armor.bag.reliable_backpack';
+UPDATE item
+SET item_definition_id = 'common.items.armor.misc.bag.soulkeeper_cursed' WHERE item_definition_id = 'common.items.armor.bag.soulkeeper_cursed';
+UPDATE item
+SET item_definition_id = 'common.items.armor.misc.bag.soulkeeper_pure' WHERE item_definition_id = 'common.items.armor.bag.soulkeeper_pure';
+UPDATE item
+SET item_definition_id = 'common.items.armor.misc.bag.tiny_leather_pouch' WHERE item_definition_id = 'common.items.armor.bag.tiny_leather_pouch';
+UPDATE item
+SET item_definition_id = 'common.items.armor.misc.bag.tiny_red_pouch' WHERE item_definition_id = 'common.items.armor.bag.tiny_red_pouch';
+UPDATE item
+SET item_definition_id = 'common.items.armor.misc.bag.troll_hide_pack' WHERE item_definition_id = 'common.items.armor.bag.troll_hide_pack';
+UPDATE item
+SET item_definition_id = 'common.items.armor.misc.bag.woven_red_bag' WHERE item_definition_id = 'common.items.armor.bag.woven_red_bag';
