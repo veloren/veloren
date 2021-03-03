e86d6318ecb89e06133fdf339681e5c99ae58521
Joshua Barretto <joshua.s.barretto@gmail.com>
2021-03-03 23:40:47 +0000

    Reverted to repr_c vek

--- voxygen/anim/src/vek.rs
+++ voxygen/anim/src/vek.rs
@@ -1,8 +1,8 @@
-pub use ::vek::{
+/*pub use ::vek::{
     bezier::repr_simd::*, geom::repr_simd::*, mat::repr_simd::column_major::Mat4, ops::*,
     quaternion::repr_simd::*, transform::repr_simd::*, transition::*, vec::repr_simd::*,
-};
-/* pub use ::vek::{
+};*/
+pub use ::vek::{
     bezier::repr_c::*, geom::repr_c::*, mat::repr_c::column_major::Mat4, ops::*,
     quaternion::repr_c::*, transform::repr_c::*, transition::*, vec::repr_c::*,
-}; */
+};
