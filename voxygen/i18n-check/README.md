6bc6f6bbba94f3f91b97967f2785ee9ac5934608
juliancoffee <lightdarkdaughter@gmail.com>
2021-05-03 16:48:46 +0300

    Extract localization test to binary
    - Implement `--all` option
    - Implement `--lang <code>` option
    - Implement `--verify` command

---
+++ voxygen/i18n-check/README.md
@@ -0,0 +1,5 @@
+# Usage
+Change your directory to veloren repo and run these commans <br/>
+`$ veloren-i18n-check --lang <lang_code>` <br/>
+`$ veloren-i18n-check --all`
+
---
+++ voxygen/i18n-check/src/bin/i18n-check.rs
@@ -0,0 +1,22 @@
+use std::{env::args, path::Path, vec::Vec};
+use veloren_i18n_check::analysis;
+
+fn main() {
+    let cli: Vec<String> = args().collect();
+
+    // Generate paths
+    let curr_dir = std::env::current_dir().unwrap();
+    let root = curr_dir.parent().unwrap().parent().unwrap();
+    let asset_path = Path::new("assets/voxygen/i18n/");
+    for (i, arg) in cli.iter().enumerate() {
+        match arg.as_str() {
+            "--all" => analysis::test_all_localizations(root, asset_path),
+            "--verify" => analysis::verify_all_localizations(root, asset_path),
+            "--lang" => {
+                let code = cli[i + 1].clone();
+                analysis::test_specific_localization(code, root, asset_path);
+            }
+            _ => continue,
+        }
+    }
+}
