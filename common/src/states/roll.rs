32ae8420fedfc89a9a33af07ca45212df7a242bb
Adam Strauss <adamwstrauss@gmail.com>
2021-05-07 15:40:11 -0700

    Fix #1105: Move server event so it does not emit constantly during build up

--- common/src/states/roll.rs
+++ common/src/states/roll.rs
@@ -56,11 +56,6 @@ impl CharacterBehavior for Data {
         match self.stage_section {
             StageSection::Buildup => {
                 handle_move(data, &mut update, 1.0);
-                // Remove burning effect if active
-                update.server_events.push_front(ServerEvent::Buff {
-                    entity: data.entity,
-                    buff_change: BuffChange::RemoveByKind(BuffKind::Burning),
-                });
                 if self.timer < self.static_data.buildup_duration {
                     // Build up
                     update.character = CharacterState::Roll(Data {
@@ -71,6 +66,11 @@ impl CharacterBehavior for Data {
                         ..*self
                     });
                 } else {
+                    // Remove burning effect if active
+                    update.server_events.push_front(ServerEvent::Buff {
+                        entity: data.entity,
+                        buff_change: BuffChange::RemoveByKind(BuffKind::Burning),
+                    });
                     // Transitions to movement section of stage
                     update.character = CharacterState::Roll(Data {
                         timer: Duration::default(),
