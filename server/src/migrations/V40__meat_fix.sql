5ef6bff0f219ae59eb46a333c19cab8f472a624f
jshipsey <jshipsey18@gmail.com>
2021-06-07 08:12:17 -0400

    mig fix

---
+++ server/src/migrations/V40__meat_fix.sql
@@ -0,0 +1,5 @@
+-- xx
+UPDATE item
+SET item_definition_id = 'common.items.crafting_ing.cloth.linen_red' WHERE item_definition_id = 'common.items.crafting_ing.cloth_scraps_red';
+UPDATE item
+SET item_definition_id = 'common.items.crafting_ing.cloth.linen' WHERE item_definition_id = 'common.items.crafting_ing.cloth_scraps';
