057c757384f075330a5d31600fafa34b8a54eb37
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-06-23 08:52:04 +0300

    build(nix): update audio resource patch

--- nix/nullOggPatch.nix
+++ nix/nullOggPatch.nix
@@ -3,16 +3,17 @@ pkgs.writeText
   "nullOgg.patch"
   ''
     diff --git a/src/audio/soundcache.rs b/src/audio/soundcache.rs
-    index 8cf703577..678295cac 100644
+    index f08a4bdab..c2036336e 100644
     --- a/src/audio/soundcache.rs
     +++ b/src/audio/soundcache.rs
-    @@ -44,7 +44,7 @@ impl OggSound {
- 
+    @@ -38,7 +38,7 @@ impl OggSound {
+     
          pub fn empty() -> OggSound {
-             OggSound(Arc::new(
-    -            include_bytes!("../../../assets/voxygen/audio/null.ogg").to_vec(),
-    +            include_bytes!("${nullOgg}").to_vec(),
-             ))
-         }
-     }
+             SoundLoader::load(
+    -            Cow::Borrowed(include_bytes!("../../../assets/voxygen/audio/null.ogg")),
+    +            Cow::Borrowed(include_bytes!("${nullOgg}")),
+                 "empty",
+             )
+             .unwrap()
   ''
+
