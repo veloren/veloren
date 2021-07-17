6de161a68f5beafe2a1e2c1159dcdd749195c0c3
Marcel Märtens <marcel.cochem@googlemail.com>
2021-07-17 21:53:32 +0200

    Revert "no longer incremental builds in CI"
    This reverts commit b3983c545188853b7e26c844d5c7231330009a5f.

--- .gitlab/scripts/linux.sh
+++ .gitlab/scripts/linux.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -r target/release/incremental/veloren_* || echo "all good" # TMP FIX FOR 2021-03-22-nightly
 export VELOREN_USERDATA_STRATEGY=executable
 time cargo build --release --no-default-features --features default-publish
 # eveluate --bin instead, last time i checked (2021-07-14) it was 2 minutes slower on release (but faster on debug)
