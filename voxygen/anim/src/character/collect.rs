0acd5c6d924cb25734038b65ca954e25a4801a01
jshipsey <jshipsey18@gmail.com>
2021-08-29 03:52:56 -0400

    finished anim

--- voxygen/anim/src/character/collect.rs
+++ voxygen/anim/src/character/collect.rs
@@ -38,15 +38,26 @@ impl Animation for CollectAnimation {
             Some(StageSection::Recover) => (1.0, 1.0, 1.0, anim_time.powi(4)),
             _ => (0.0, 0.0, 0.0, 0.0),
         };
+        let z_diff = (sprite_pos.z - position.z).round();
+        let z_diff = if z_diff > 0.0 { z_diff / 9.0 } else { 0.0 };
+        let squat = (1.0 - z_diff).powf(4.0);
+
         let pullback = 1.0 - move3;
 
-        let move1 = movement1 * pullback;
+        let move1 = movement1 * pullback * squat;
+        let move1_nosquat = movement1 * pullback;
+        let upshift = if squat < 0.35 {
+            move1_nosquat * 0.3
+        } else {
+            0.0
+        };
+        next.head.orientation = Quaternion::rotation_x(move1_nosquat * 0.2 + upshift * 1.3);
 
-        println!("{} pos z", position.z);
-        next.head.position = Vec3::new(0.0, s_a.head.0, s_a.head.1);
-        next.head.orientation = Quaternion::rotation_x(move1 * 0.2);
-
-        next.chest.position = Vec3::new(0.0, s_a.chest.0, s_a.chest.1 + move2 * 0.15);
+        next.chest.position = Vec3::new(
+            0.0,
+            s_a.chest.0 + upshift * 3.0,
+            s_a.chest.1 + move2 * 0.15 + upshift * 3.0,
+        );
         next.chest.orientation = Quaternion::rotation_x(move1 * -1.0 + move2alt * 0.015);
 
         next.belt.position = Vec3::new(0.0, s_a.belt.0 + move1 * 1.0, s_a.belt.1 + move1 * -0.0);
@@ -59,32 +70,35 @@ impl Animation for CollectAnimation {
         next.shorts.orientation = Quaternion::rotation_x(move1 * 0.3);
 
         next.hand_l.position = Vec3::new(
-            -s_a.hand.0 + move1 * 4.0 - move2alt * 1.0,
-            s_a.hand.1 + move1 * 8.0 + move2 * 1.0,
-            s_a.hand.2 + move1 * 5.0,
+            -s_a.hand.0 + move1_nosquat * 4.0 - move2alt * 1.0,
+            s_a.hand.1 + move1_nosquat * 8.0 + move2 * 1.0 + upshift * -5.0,
+            s_a.hand.2 + move1_nosquat * 5.0 + upshift * 15.0,
         );
 
-        next.hand_l.orientation = Quaternion::rotation_x(move1 * 1.9)
-            * Quaternion::rotation_y(move1 * -0.3 + move2alt * -0.2);
+        next.hand_l.orientation = Quaternion::rotation_x(move1_nosquat * 1.9 + upshift * 2.0)
+            * Quaternion::rotation_y(move1_nosquat * -0.3 + move2alt * -0.2);
 
         next.hand_r.position = Vec3::new(
-            s_a.hand.0 + move1 * -4.0 - move2 * 1.0,
-            s_a.hand.1 + move1 * 8.0 + move2alt * -1.0,
-            s_a.hand.2 + move1 * 5.0,
+            s_a.hand.0 + move1_nosquat * -4.0 - move2 * 1.0,
+            s_a.hand.1 + move1_nosquat * 8.0 + move2alt * -1.0 + upshift * -5.0,
+            s_a.hand.2 + move1_nosquat * 5.0 + upshift * 15.0,
         );
-        next.hand_r.orientation =
-            Quaternion::rotation_x(move1 * 1.9) * Quaternion::rotation_y(move1 * 0.3 + move2 * 0.3);
+        next.hand_r.orientation = Quaternion::rotation_x(move1_nosquat * 1.9 + upshift * 2.0)
+            * Quaternion::rotation_y(move1_nosquat * 0.3 + move2 * 0.3);
 
-        next.foot_l.position = Vec3::new(-s_a.foot.0, s_a.foot.1 + move1 * 2.0, s_a.foot.2);
-        next.foot_l.orientation = Quaternion::rotation_x(move1 * -0.2);
+        next.foot_l.position = Vec3::new(
+            -s_a.foot.0,
+            s_a.foot.1 + move1 * 2.0 + upshift * -3.5,
+            s_a.foot.2 + upshift * 2.0,
+        );
+        next.foot_l.orientation = Quaternion::rotation_x(move1 * -0.2 + upshift * -2.2);
 
-        next.foot_r.position = Vec3::new(s_a.foot.0, s_a.foot.1 + move1 * -4.0, s_a.foot.2);
-        next.foot_r.orientation = Quaternion::rotation_x(move1 * -0.8);
-
-        next.shoulder_l.position = Vec3::new(-s_a.shoulder.0, s_a.shoulder.1, s_a.shoulder.2);
-
-        next.shoulder_r.position = Vec3::new(s_a.shoulder.0, s_a.shoulder.1, s_a.shoulder.2);
-
+        next.foot_r.position = Vec3::new(
+            s_a.foot.0,
+            s_a.foot.1 + move1 * -4.0 + upshift * -0.5,
+            s_a.foot.2 + upshift * 2.0,
+        );
+        next.foot_r.orientation = Quaternion::rotation_x(move1 * -0.8 + upshift * -1.2);
         next
     }
 }
