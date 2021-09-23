ee9da5309bdc0e3f148f9a4f3bc5033c96e11ec8
EvanMeek <the_lty_mail@foxmail.com>
2021-09-23 12:23:05 +0800

    添加迁移Sql

---
+++ server/src/migrations/V44__rename_wecw_items.sql
@@ -0,0 +1,28 @@
+-- 大剑
+UPDATE item SET item_definition_id= "common.items.WECW.风暴使者" WHERE item_definition_id="common.items.WECW.caladbolgG";
+UPDATE item SET item_definition_id= "common.items.WECW.乌菲尔" WHERE item_definition_id="common.items.WECW.caladbolgEXG";
+UPDATE item SET item_definition_id= "common.items.WECW.灵魂风暴" WHERE item_definition_id="common.items.WECW.caladbolgex";
+UPDATE item SET item_definition_id= "common.items.WECW.白金巨剑" WHERE item_definition_id="common.items.WECW.holysword";
+UPDATE item SET item_definition_id= "common.items.WECW.萨顿的末日战刃" WHERE item_definition_id="common.items.WECW.darksteel";
+UPDATE item SET item_definition_id= "common.items.WECW.风暴大剑" WHERE item_definition_id="common.items.WECW.stormgreatsword";
+UPDATE item SET item_definition_id= "common.items.WECW.暗金巨像刃" WHERE item_definition_id="common.items.WECW.darksteel2";
+-- 权杖
+UPDATE item SET item_definition_id= "common.items.WECW.灵魂权杖" WHERE item_definition_id="common.items.WECW.mind";
+UPDATE item SET item_definition_id= "common.items.WECW.理智的摧残者" WHERE item_definition_id="common.items.WECW.mindflayer";
+
+-- 锤子
+UPDATE item SET item_definition_id= "common.items.WECW.冰风暴" WHERE item_definition_id="common.items.WECW.icehammer";
+UPDATE item SET item_definition_id= "common.items.WECW.帕拉丁战锤" WHERE item_definition_id="common.items.WECW.holyhammer";
+UPDATE item SET item_definition_id= "common.items.WECW.熔火之心" WHERE item_definition_id="common.items.WECW.darkhammerex";
+UPDATE item SET item_definition_id= "common.items.WECW.暗金冲击锤" WHERE item_definition_id="common.items.WECW.darkhammer";
+
+-- 弓箭
+UPDATE item SET item_definition_id= "common.items.WECW.旭日击破者" WHERE item_definition_id="common.items.WECW.bow";
+UPDATE item SET item_definition_id= "common.items.WECW.旭日击破者-觉醒" WHERE item_definition_id="common.items.WECW.exbow";
+UPDATE item SET item_definition_id= "common.items.WECW.旭日击破者-奥能" WHERE item_definition_id="common.items.WECW.exbowg";
+
+-- 斧头
+UPDATE item SET item_definition_id= "common.items.WECW.深蓝使者" WHERE item_definition_id="common.items.WECW.exsteelaxe";
+UPDATE item SET item_definition_id= "common.items.WECW.遗忘战斧" WHERE item_definition_id="common.items.WECW.forgottenaxe";
+UPDATE item SET item_definition_id= "common.items.WECW.埃尔安多" WHERE item_definition_id="common.items.WECW.eriandor";
+UPDATE item SET item_definition_id= "common.items.WECW.暗金切割者" WHERE item_definition_id="common.items.WECW.darkaxe";
