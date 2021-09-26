f99211d48278a95452a066b320ee9a3a6a52b499
Seth <sethmb@protonmail.com>
2021-09-26 13:49:49 +0000

    Reduce duplication in glider asset identifiers

---
+++ server/src/migrations/V44__reduce_asset_duplication.sql
@@ -0,0 +1,14 @@
+-- Update asset names
+
+UPDATE item SET item_definition_id = 'common.items.glider.basic_white' WHERE item_definition_id = 'common.items.glider.glider_basic_white';
+UPDATE item SET item_definition_id = 'common.items.glider.basic_red' WHERE item_definition_id = 'common.items.glider.glider_basic_red';
+UPDATE item SET item_definition_id = 'common.items.glider.blue' WHERE item_definition_id = 'common.items.glider.glider_blue';
+UPDATE item SET item_definition_id = 'common.items.glider.cloverleaf' WHERE item_definition_id = 'common.items.glider.glider_cloverleaf';
+UPDATE item SET item_definition_id = 'common.items.glider.leaves' WHERE item_definition_id = 'common.items.glider.glider_leaves';
+UPDATE item SET item_definition_id = 'common.items.glider.monarch' WHERE item_definition_id = 'common.items.glider.glider_monarch';
+UPDATE item SET item_definition_id = 'common.items.glider.morpho' WHERE item_definition_id = 'common.items.glider.glider_morpho';
+UPDATE item SET item_definition_id = 'common.items.glider.moth' WHERE item_definition_id = 'common.items.glider.glider_moth';
+UPDATE item SET item_definition_id = 'common.items.glider.skullgrin' WHERE item_definition_id = 'common.items.glider.glider_purp';
+UPDATE item SET item_definition_id = 'common.items.glider.sandraptor' WHERE item_definition_id = 'common.items.glider.glider_sandraptor';
+UPDATE item SET item_definition_id = 'common.items.glider.snowraptor' WHERE item_definition_id = 'common.items.glider.glider_snowraptor';
+UPDATE item SET item_definition_id = 'common.items.glider.woodraptor' WHERE item_definition_id = 'common.items.glider.glider_woodraptor';
