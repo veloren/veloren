8837dc81d512d3ee478b51923805bd908ff516bd
Imbris <imbrisf@gmail.com>
2021-10-10 02:31:53 -0400

    Rearrange iteration pattern so that x is in the inner loop in the default for_each_in (no or little perf change)

--- common/src/vol.rs
+++ common/src/vol.rs
@@ -119,9 +119,9 @@ pub trait ReadVol: BaseVol {
     where
         Self::Vox: Copy,
     {
-        for x in aabb.min.x..aabb.max.x + 1 {
+        for z in aabb.min.z..aabb.max.z + 1 {
             for y in aabb.min.y..aabb.max.y + 1 {
-                for z in aabb.min.z..aabb.max.z + 1 {
+                for x in aabb.min.x..aabb.max.x + 1 {
                     if let Ok(block) = self.get(Vec3::new(x, y, z)) {
                         f(Vec3::new(x, y, z), *block);
                     }
