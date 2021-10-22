e916e32547a36a10a4e73ac17f5f7c952adfd95f
Doctor Kompot <accessibledood@gmail.com>
2021-10-22 05:24:20 +0000

    DoctorKompot - New Necklaces

---
+++ server/src/migrations/V45__new_necklaces.sql
@@ -0,0 +1,6 @@
+UPDATE item
+SET item_definition_id = 'common.items.armor.misc.neck.pendant_of_protection' WHERE item_definition_id = 'common.items.armor.misc.neck.plain_0';
+UPDATE item
+SET item_definition_id = 'common.items.armor.cultist.ring' WHERE item_definition_id = 'common.items.armor.misc.ring.skull';
+UPDATE item
+SET item_definition_id = 'common.items.armor.misc.neck.gem_of_resilience' WHERE item_definition_id = 'common.items.armor.misc.neck.plain_1'
