8e20b9ffe7afb78a60e9a99711a7aea156f1168b
Imbris <imbrisf@gmail.com>
2021-11-16 00:37:41 -0500

    Apply Add-CI-build-script-for-cross-compiling-Aarch64.patch from @linux4kix

--- .gitlab/scripts/linux-aarch64.sh
+++ .gitlab/scripts/linux-aarch64.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export VELOREN_USERDATA_STRATEGY=executable
-time cargo build --release --no-default-features --features default-publish
+time PKG_CONFIG=/usr/bin/aarch64-linux-gnu-pkg-config cargo build --release --no-default-features --features default-publish --release --target aarch64-unknown-linux-gnu
 # evaluate --bin instead, last time i checked (2021-07-14) it was 2 minutes slower on release (but faster on debug)
