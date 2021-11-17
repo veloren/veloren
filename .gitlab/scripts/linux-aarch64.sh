34a391835c83eaedd2e1b372278af50dff8e1a8c
Imbris <imbrisf@gmail.com>
2021-11-16 22:07:47 -0500

    Fix order of env var and time command in aarch CI script, remove duplicate release flag, reorder target flag to be in the same order as the target flag for windows

--- .gitlab/scripts/linux-aarch64.sh
+++ .gitlab/scripts/linux-aarch64.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export VELOREN_USERDATA_STRATEGY=executable
-time PKG_CONFIG=/usr/bin/aarch64-linux-gnu-pkg-config cargo build --release --no-default-features --features default-publish --release --target aarch64-unknown-linux-gnu
+PKG_CONFIG="/usr/bin/aarch64-linux-gnu-pkg-config" time cargo build --target=aarch64-unknown-linux-gnu --release --no-default-features --features default-publish
 # evaluate --bin instead, last time i checked (2021-07-14) it was 2 minutes slower on release (but faster on debug)
