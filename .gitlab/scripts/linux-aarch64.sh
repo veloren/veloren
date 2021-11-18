2e79ed445cd0c836d7825dd1709fca7cf73b185c
Imbris <imbrisf@gmail.com>
2021-11-18 09:10:51 -0500

    Try with: export PKG_CONFIG

--- .gitlab/scripts/linux-aarch64.sh
+++ .gitlab/scripts/linux-aarch64.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export VELOREN_USERDATA_STRATEGY=executable
-PKG_CONFIG="/usr/bin/aarch64-linux-gnu-pkg-config" time cargo build --target=aarch64-unknown-linux-gnu --release --no-default-features --features default-publish
+export PKG_CONFIG="/usr/bin/aarch64-linux-gnu-pkg-config"
+time cargo build --target=aarch64-unknown-linux-gnu --release --no-default-features --features default-publish
 # evaluate --bin instead, last time i checked (2021-07-14) it was 2 minutes slower on release (but faster on debug)
