196bfbe9e965c4211d12215c12958a8b427359e8
juliancoffee <lightdarkdaughter@gmail.com>
2022-01-23 17:23:34 +0200

    remove outdated comment

--- common/src/comp/inventory/loadout_builder.rs
+++ common/src/comp/inventory/loadout_builder.rs
@@ -492,7 +492,6 @@ impl LoadoutBuilder {
             _ => None,
         };
 
-        // closures can't be used here, because it moves value
         if let Some(chest) = chest {
             self.chest(Some(Item::new_from_asset_expect(chest)))
         } else {
