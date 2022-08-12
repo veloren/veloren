0a5514ed32d6ff23a505a71fb5e55aae13d06711
Sam <samuelkeiffer@gmail.com>
2022-08-11 21:58:20 -0400

    Animation for crippling strike

--- voxygen/anim/src/character/combomelee.rs
+++ voxygen/anim/src/character/combomelee.rs
@@ -346,6 +346,53 @@ impl Animation for ComboAnimation {
                     next.control.orientation.rotate_z(move2 * -1.4);
                     next.control.position += Vec3::new(0.0, move2 * 6.0, move2 * -3.0);
                 },
+                Some("common.abilities.sword.crippling_strike") => {
+                    let (move1, move2, move3) = if strike == current_strike {
+                        match stage_section {
+                            Some(StageSection::Buildup) => (anim_time.powf(0.25), 0.0, 0.0),
+                            Some(StageSection::Action) => (1.0, anim_time.powi(2), 0.0),
+                            Some(StageSection::Recover) => (1.0, 1.0, anim_time.powi(4)),
+                            _ => (0.0, 0.0, 0.0),
+                        }
+                    } else {
+                        (1.0, 1.0, 0.0)
+                    };
+
+                    let move2alt = move2.min(0.5) * 2.0;
+                    let move2 = (move2.max(0.5) - 0.5) * 2.0;
+
+                    next.hand_l.position = Vec3::new(s_a.shl.0, s_a.shl.1, s_a.shl.2);
+                    next.hand_l.orientation =
+                        Quaternion::rotation_x(s_a.shl.3) * Quaternion::rotation_y(s_a.shl.4);
+                    next.hand_r.position =
+                        Vec3::new(-s_a.sc.0 + 6.0 + move1 * -12.0, -4.0 + move1 * 3.0, -2.0);
+                    next.hand_r.orientation = Quaternion::rotation_x(0.9 + move1 * 0.5);
+                    next.control.position = Vec3::new(s_a.sc.0, s_a.sc.1, s_a.sc.2);
+                    next.control.orientation = Quaternion::rotation_x(s_a.sc.3)
+                        * Quaternion::rotation_z((move2alt + move2) * -PI / 4.0);
+
+                    next.chest.orientation = Quaternion::rotation_z(move1 * 1.3)
+                        * Quaternion::rotation_x(move2alt * -0.3);
+                    next.head.orientation = Quaternion::rotation_z(move1 * -0.8 + move2 * 1.0)
+                        * Quaternion::rotation_x(move2alt * 0.1);
+                    next.belt.orientation = Quaternion::rotation_z(move1 * -0.4)
+                        * Quaternion::rotation_x(move2alt * 0.3);
+                    next.shorts.orientation = Quaternion::rotation_z(move1 * -1.0 + move2 * 1.0)
+                        * Quaternion::rotation_x(move2alt * 0.5);
+                    next.foot_l.orientation = Quaternion::rotation_z(move1 * 0.8);
+                    next.foot_l.position += Vec3::new(0.0, move1 * -4.0, 0.0);
+                    next.control.orientation.rotate_x(move1 * 0.4);
+
+                    next.foot_r.position += Vec3::new(0.0, move2alt * 4.0, 0.0);
+                    next.shorts.position +=
+                        Vec3::new(move2alt * 1.0, move2alt * 2.0, move2alt * 0.0);
+                    next.control
+                        .orientation
+                        .rotate_x(move2alt * -0.8 + move2 * -0.6);
+                    next.chest.orientation.rotate_z(move2 * -1.7);
+                    next.control.orientation.rotate_z(move2 * -1.1);
+                    next.control.position += Vec3::new(move2 * 14.0, move2 * 3.0, move2 * 6.0);
+                },
                 _ => {},
             }
         }
