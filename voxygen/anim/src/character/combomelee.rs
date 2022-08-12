53ae1548a86137cfc1a49ad62b06148b33fc6381
Sam <samuelkeiffer@gmail.com>
2022-08-11 21:03:19 -0400

    Crippling gouge animation

--- voxygen/anim/src/character/combomelee.rs
+++ voxygen/anim/src/character/combomelee.rs
@@ -206,17 +206,15 @@ impl Animation for ComboAnimation {
                     }
                 },
                 Some("common.abilities.sword.offensive_advance") => {
-                    let (move1, move2, move3, move2alt) = if strike == current_strike {
+                    let (move1, move2, move3) = if strike == current_strike {
                         match stage_section {
-                            Some(StageSection::Buildup) => (anim_time.powf(0.25), 0.0, 0.0, 0.0),
-                            Some(StageSection::Action) => {
-                                (1.0, anim_time.powi(2), 0.0, anim_time.powf(0.25))
-                            },
-                            Some(StageSection::Recover) => (1.0, 1.0, anim_time.powi(4), 1.0),
-                            _ => (0.0, 0.0, 0.0, 0.0),
+                            Some(StageSection::Buildup) => (anim_time.powf(0.25), 0.0, 0.0),
+                            Some(StageSection::Action) => (1.0, anim_time.powi(2), 0.0),
+                            Some(StageSection::Recover) => (1.0, 1.0, anim_time.powi(4)),
+                            _ => (0.0, 0.0, 0.0),
                         }
                     } else {
-                        (1.0, 1.0, 0.0, 1.0)
+                        (1.0, 1.0, 0.0)
                     };
 
                     next.hand_l.position = Vec3::new(s_a.shl.0, s_a.shl.1, s_a.shl.2);
@@ -311,6 +309,42 @@ impl Animation for ComboAnimation {
                         },
                         _ => {},
                     }
+                },
+                Some("common.abilities.sword.crippling_gouge") => {
+                    let (move1, move2, move3) = if strike == current_strike {
+                        match stage_section {
+                            Some(StageSection::Buildup) => (anim_time.powf(0.25), 0.0, 0.0),
+                            Some(StageSection::Action) => (1.0, anim_time.powi(2), 0.0),
+                            Some(StageSection::Recover) => (1.0, 1.0, anim_time.powi(4)),
+                            _ => (0.0, 0.0, 0.0),
+                        }
+                    } else {
+                        (1.0, 1.0, 0.0)
+                    };
+
+                    next.hand_l.position = Vec3::new(s_a.shl.0, s_a.shl.1, s_a.shl.2);
+                    next.hand_l.orientation =
+                        Quaternion::rotation_x(s_a.shl.3) * Quaternion::rotation_y(s_a.shl.4);
+                    next.hand_r.position =
+                        Vec3::new(-s_a.sc.0 + 6.0 + move1 * -12.0, -4.0 + move1 * 3.0, -2.0);
+                    next.hand_r.orientation = Quaternion::rotation_x(0.9 + move1 * 0.5);
+                    next.control.position = Vec3::new(s_a.sc.0, s_a.sc.1, s_a.sc.2);
+                    next.control.orientation = Quaternion::rotation_x(s_a.sc.3);
+
+                    next.chest.orientation = Quaternion::rotation_z(move1 * -1.5);
+                    next.head.orientation = Quaternion::rotation_z(move1 * 1.1);
+                    next.belt.orientation = Quaternion::rotation_z(move1 * 0.4);
+                    next.shorts.orientation = Quaternion::rotation_z(move1 * 1.0);
+                    next.control.orientation.rotate_y(move1 * -1.9);
+                    next.control.orientation.rotate_z(move1 * 0.7);
+                    next.control.position += Vec3::new(move1 * 10.0, 0.0, move1 * 9.0);
+
+                    next.chest.orientation.rotate_z(move2 * 1.4);
+                    next.head.orientation.rotate_z(move2 * -0.9);
+                    next.belt.orientation.rotate_z(move2 * -0.4);
+                    next.shorts.orientation.rotate_z(move2 * -0.9);
+                    next.control.orientation.rotate_z(move2 * -1.4);
+                    next.control.position += Vec3::new(0.0, move2 * 6.0, move2 * -3.0);
                 },
                 _ => {},
             }
