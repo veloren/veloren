c30f29e3d24b942ca7dbfafb23a440438d2f131f
Sam <samuelkeiffer@gmail.com>
2022-08-11 20:32:50 -0400

    Crippling finisher animation

--- voxygen/anim/src/character/finishermelee.rs
+++ voxygen/anim/src/character/finishermelee.rs
@@ -42,6 +42,7 @@ impl Animation for FinisherMeleeAnimation {
                     Quaternion::rotation_x(s_a.shl.3) * Quaternion::rotation_y(s_a.shl.4);
                 next.hand_r.position =
                     Vec3::new(-s_a.sc.0 + 6.0 + move1 * -12.0, -4.0 + move1 * 3.0, -2.0);
+                next.hand_r.orientation = Quaternion::rotation_x(0.9 + move1 * 0.5);
                 next.control.position = Vec3::new(
                     s_a.sc.0 + move2 * 12.0,
                     s_a.sc.1,
@@ -70,6 +71,7 @@ impl Animation for FinisherMeleeAnimation {
                     Quaternion::rotation_x(s_a.shl.3) * Quaternion::rotation_y(s_a.shl.4);
                 next.hand_r.position =
                     Vec3::new(-s_a.sc.0 + 6.0 + move1 * -12.0, -4.0 + move1 * 3.0, -2.0);
+                next.hand_r.orientation = Quaternion::rotation_x(0.9 + move1 * 0.5);
                 next.control.position = Vec3::new(s_a.sc.0, s_a.sc.1, s_a.sc.2);
                 next.control.orientation = Quaternion::rotation_x(s_a.sc.3);
 
@@ -97,6 +99,47 @@ impl Animation for FinisherMeleeAnimation {
                 next.shorts.orientation.rotate_z(move3 * -0.2);
                 next.control.position += Vec3::new(0.0, 0.0, move3 * 4.0);
                 next.control.orientation.rotate_x(move3 * 0.6);
+            },
+            Some("common.abilities.sword.crippling_finisher") => {
+                let (move1, move2, move3) = match stage_section {
+                    Some(StageSection::Buildup) => (anim_time.powf(0.5), 0.0, 0.0),
+                    Some(StageSection::Action) => (1.0, anim_time.powi(2), 0.0),
+                    Some(StageSection::Recover) => (1.0, 1.0, anim_time.powi(4)),
+                    _ => (0.0, 0.0, 0.0),
+                };
+
+                let move1alt1 = move1.min(0.5) * 2.0;
+                let move1alt2 = (move1.max(0.5) - 0.5) * 2.0;
+
+                next.hand_l.position = Vec3::new(s_a.shl.0, s_a.shl.1, s_a.shl.2);
+                next.hand_l.orientation = Quaternion::rotation_x(s_a.shl.3 + move1alt2 * PI)
+                    * Quaternion::rotation_y(s_a.shl.4);
+                next.hand_r.position = Vec3::new(
+                    -s_a.sc.0 + 6.0 + move1alt1 * -12.0,
+                    -4.0 + move1alt1 * 3.0,
+                    -2.0,
+                );
+                next.hand_r.orientation =
+                    Quaternion::rotation_x(0.9 + move1 * 0.5 + move1alt1 * PI);
+                next.control.position = Vec3::new(s_a.sc.0, s_a.sc.1, s_a.sc.2);
+                next.control.orientation = Quaternion::rotation_x(s_a.sc.3);
+
+                next.control.position += Vec3::new(
+                    move1 * 6.0,
+                    (1.0 - (move1 - 0.5).abs() * 2.0) * 3.0,
+                    move1 * 22.0,
+                );
+                next.control.orientation.rotate_x(move1 * -1.5);
+
+                next.chest.orientation = Quaternion::rotation_x(move2 * -0.4);
+                next.head.orientation = Quaternion::rotation_x(move2 * 0.2);
+                next.belt.orientation = Quaternion::rotation_x(move2 * 0.4);
+                next.shorts.orientation = Quaternion::rotation_x(move2 * 0.8);
+                next.control.orientation.rotate_x(move2 * -0.4);
+                next.control.position += Vec3::new(0.0, 0.0, move2 * -10.0);
+                next.belt.position += Vec3::new(0.0, move2 * 2.0, move2 * 0.0);
+                next.shorts.position += Vec3::new(0.0, move2 * 4.0, move2 * 1.0);
+                next.chest.position += Vec3::new(0.0, move2 * -2.5, 0.0);
             },
             _ => {},
         }
