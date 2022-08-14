8a23238fd1eb92a449d4db1c0e2057e5a688f499
Sam <samuelkeiffer@gmail.com>
2022-08-14 18:57:23 -0400

    Defensive retreat animation

--- voxygen/anim/src/character/combomelee.rs
+++ voxygen/anim/src/character/combomelee.rs
@@ -582,6 +582,47 @@ impl Animation for ComboAnimation {
                         _ => {},
                     }
                 },
+                Some("common.abilities.sword.defensive_retreat") => {
+                    let (move1, move2, move3) = if strike == current_strike {
+                        match stage_section {
+                            Some(StageSection::Buildup) => (anim_time.powf(0.25), 0.0, 0.0),
+                            Some(StageSection::Action) => (1.0, anim_time.powi(2), 0.0),
+                            Some(StageSection::Recover) => (1.0, 1.0, anim_time.powi(2)),
+                            _ => (0.0, 0.0, 0.0),
+                        }
+                    } else {
+                        (1.0, 1.0, 0.0)
+                    };
+
+                    let move2_pre = move2.min(0.3) * 10.0 / 3.0;
+
+                    next.hand_l.position = Vec3::new(s_a.shl.0, s_a.shl.1, s_a.shl.2);
+                    next.hand_l.orientation =
+                        Quaternion::rotation_x(s_a.shl.3) * Quaternion::rotation_y(s_a.shl.4);
+                    next.hand_r.position =
+                        Vec3::new(-s_a.sc.0 + 6.0 + move1 * -12.0, -4.0 + move1 * 3.0, -2.0);
+                    next.hand_r.orientation = Quaternion::rotation_x(0.9 + move1 * 0.5);
+                    next.control.position = Vec3::new(s_a.sc.0, s_a.sc.1, s_a.sc.2);
+                    next.control.orientation = Quaternion::rotation_x(s_a.sc.3);
+
+                    next.chest.orientation = Quaternion::rotation_z(move1 * 0.7);
+                    next.shorts.orientation = Quaternion::rotation_z(move1 * -0.3);
+                    next.belt.orientation = Quaternion::rotation_z(move1 * -0.1);
+                    next.head.orientation = Quaternion::rotation_z(move1 * -0.4);
+                    next.foot_l.position += Vec3::new(0.0, move1 * -4.0, 0.0);
+
+                    next.chest.orientation.rotate_z(move2 * -1.5);
+                    next.head.orientation.rotate_z(move2 * 0.9);
+                    next.belt.orientation.rotate_z(move2 * 0.4);
+                    next.shorts.orientation.rotate_z(move2 * 1.0);
+                    next.control
+                        .orientation
+                        .rotate_y(move2 * -1.6 + move3 * 1.6);
+                    next.control
+                        .orientation
+                        .rotate_z(move1 * 0.3 + move2 * -1.5 + move3 * -0.4);
+                    next.control.position += Vec3::new(move2 * 12.0 + move3 * -12.0, 0.0, 0.0);
+                },
                 _ => {},
             }
         }
