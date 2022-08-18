cea1eb52f9ca57c066f6e5b10ab78c825bb9dc43
juliancoffee <lightdarkdaughter@gmail.com>
2022-08-19 00:09:39 +0300

    Resurrect convert_utf8_to_ascii

--- voxygen/i18n/src/raw.rs
+++ voxygen/i18n/src/raw.rs
@@ -1,12 +1,15 @@
-use crate::{Fonts, LanguageMetadata};
+use crate::{
+    assets::{loader, StringLoader},
+    Fonts, LanguageMetadata,
+};
 use serde::{Deserialize, Serialize};
-
-use std::str::FromStr;
 
 /// Localization metadata from manifest file
 /// See `Language` for more info on each attributes
 #[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
 pub(crate) struct Manifest {
+    /// Whether to convert the input text encoded in UTF-8
+    /// into a ASCII version by using the `deunicode` crate.
     pub(crate) convert_utf8_to_ascii: bool,
     pub(crate) fonts: Fonts,
     pub(crate) metadata: LanguageMetadata,
@@ -18,19 +21,31 @@ impl crate::assets::Asset for Manifest {
     const EXTENSION: &'static str = "ron";
 }
 
+// Newtype wrapper representing fluent resource.
+//
+// NOTE:
+// We store String, that later converted to FluentResource.
+// We can't do it at load time, because we might want to do ut8 to ascii
+// conversion and we know it only after we've loaded language manifest.
+//
+// Alternative solution is to make it hold Rc/Arc around FluentResource,
+// implement methods that give us mutable control around resource entries,
+// but doing it to eliminate Clone that happens N per programm life seems as
+// overengineering.
+//
+// N is time of fluent files, so about 20 for English and the same for target
+// localisation.
 #[derive(Clone)]
 pub(crate) struct Resource {
     pub(crate) src: String,
 }
 
-impl FromStr for Resource {
-    type Err = std::convert::Infallible;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> { Ok(Self { src: s.to_owned() }) }
+impl From<String> for Resource {
+    fn from(src: String) -> Self { Self { src } }
 }
 
 impl crate::assets::Asset for Resource {
-    type Loader = crate::assets::loader::ParseLoader;
+    type Loader = loader::LoadFrom<String, StringLoader>;
 
     const EXTENSION: &'static str = "ftl";
 }
