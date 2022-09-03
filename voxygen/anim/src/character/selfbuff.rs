8dcff0bb4bca7dc7c72957040508ec1d836e0dbe
Sam <samuelkeiffer@gmail.com>
2022-09-02 20:50:33 -0400

    Animation for heavy fortitude. Tweaked animation for defensive bulwark.

--- voxygen/anim/src/character/selfbuff.rs
+++ voxygen/anim/src/character/selfbuff.rs
@@ -23,8 +23,6 @@ impl Animation for SelfBuffAnimation {
     ) -> Self::Skeleton {
         *rate = 1.0;
         let mut next = (*skeleton).clone();
-        next.main_weapon_trail = true;
-        next.off_weapon_trail = true;
 
         next.main.position = Vec3::new(0.0, 0.0, 0.0);
         next.main.orientation = Quaternion::rotation_z(0.0);
@@ -46,21 +44,55 @@ impl Animation for SelfBuffAnimation {
                 next.hand_r.orientation = Quaternion::rotation_x(0.9 + move1 * 0.5);
                 next.control.position = Vec3::new(s_a.sc.0, s_a.sc.1, s_a.sc.2);
                 next.control.orientation =
-                    Quaternion::rotation_x(s_a.sc.3) * Quaternion::rotation_z(move1 * -PI / 2.0);
+                    Quaternion::rotation_x(s_a.sc.3) * Quaternion::rotation_z(move2 * -0.5);
 
-                next.foot_r.position += Vec3::new(move1 * 2.0, move1 * -3.0, 0.0);
-                next.foot_r.orientation.rotate_z(move1 * -1.5);
-                next.chest.orientation = Quaternion::rotation_z(move1 * -0.6);
+                next.foot_r.position += Vec3::new(move1 * 1.0, move1 * -2.0, 0.0);
+                next.foot_r.orientation.rotate_z(move1 * -0.9);
+                next.chest.orientation = Quaternion::rotation_z(move1 * -0.5);
                 next.head.orientation = Quaternion::rotation_z(move1 * 0.3);
-                next.belt.orientation = Quaternion::rotation_z(move1 * -0.1);
-                next.shorts.orientation = Quaternion::rotation_z(move1 * -0.3);
+                next.shorts.orientation = Quaternion::rotation_z(move1 * 0.1);
                 next.control.orientation.rotate_x(move1 * 0.4);
-                next.control.orientation.rotate_y(move1 * -0.8);
-                next.control.position += Vec3::new(move1 * 12.0, 0.0, 0.0);
-                next.hand_l.orientation.rotate_y(move1 * PI);
-                next.hand_l.position += Vec3::new(0.0, 0.0, move1 * 7.0);
+                next.control.orientation.rotate_z(move1 * 0.5);
+                next.control.position += Vec3::new(0.0, 0.0, move1 * 4.0);
 
-                next.hand_l.position += Vec3::new(0.0, 0.0, move2 * 20.0);
+                next.control.position += Vec3::new(move2 * 8.0, 0.0, move2 * -1.0);
+                next.control.orientation.rotate_x(move2 * -0.6);
+                next.chest.position += Vec3::new(0.0, 0.0, move2 * -2.0);
+                next.belt.position += Vec3::new(0.0, 0.0, move2 * 1.0);
+                next.shorts.position += Vec3::new(0.0, 0.0, move2 * 1.0);
+                next.shorts.orientation.rotate_x(move2 * 0.2);
+                next.control.orientation.rotate_z(move2 * 0.4);
+            },
+            Some("common.abilities.sword.heavy_fortitude") => {
+                let (move1, move2, move3) = match stage_section {
+                    Some(StageSection::Movement) => (anim_time.powf(0.25), 0.0, 0.0),
+                    Some(StageSection::Action) => (1.0, anim_time.powi(2), 0.0),
+                    Some(StageSection::Recover) => (1.0, 1.0, anim_time.powi(4)),
+                    _ => (0.0, 0.0, 0.0),
+                };
+
+                next.hand_l.position = Vec3::new(s_a.shl.0, s_a.shl.1, s_a.shl.2);
+                next.hand_l.orientation =
+                    Quaternion::rotation_x(s_a.shl.3) * Quaternion::rotation_y(s_a.shl.4);
+                next.hand_r.position =
+                    Vec3::new(-s_a.sc.0 + 6.0 + move1 * -12.0, -4.0 + move1 * 3.0, -2.0);
+                next.hand_r.orientation = Quaternion::rotation_x(0.9 + move1 * 0.5);
+                next.control.position = Vec3::new(s_a.sc.0, s_a.sc.1, s_a.sc.2);
+                next.control.orientation = Quaternion::rotation_x(s_a.sc.3);
+
+                next.foot_l.position += Vec3::new(move1 * 1.0, move1 * 2.0, 0.0);
+                next.chest.orientation = Quaternion::rotation_z(move1 * -0.4);
+                next.head.orientation = Quaternion::rotation_z(move1 * 0.2);
+                next.shorts.orientation = Quaternion::rotation_z(move1 * 0.3);
+                next.belt.orientation = Quaternion::rotation_z(move1 * 0.1);
+                next.control.orientation.rotate_x(move1 * 0.4 + move2 * 0.6);
+                next.control.orientation.rotate_z(move1 * 0.4);
+
+                next.foot_r.position += Vec3::new(move2 * -1.0, move2 * -2.0, 0.0);
+                next.control.position += Vec3::new(move2 * 5.0, move2 * 7.0, move2 * 5.0);
+                next.chest.position += Vec3::new(0.0, 0.0, move2 * -1.0);
+                next.shorts.orientation.rotate_x(move2 * 0.2);
+                next.shorts.position += Vec3::new(0.0, move2 * 1.0, 0.0);
             },
             _ => {},
         }
