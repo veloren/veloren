2bf7de8ac036b85939015946daf4baa92d99a6c5
Sam <samuelkeiffer@gmail.com>
2022-09-04 10:45:45 -0400

    Animation for reaching combo

--- voxygen/anim/src/character/combomelee.rs
+++ voxygen/anim/src/character/combomelee.rs
@@ -958,6 +958,73 @@ impl Animation for ComboAnimation {
                         next.control.position += Vec3::new(move2 * 10.0, move2 * 2.0, 0.0);
                     }
                 },
+                Some("common.abilities.sword.reaching_combo") => {
+                    let (move1, move2, move3) = if strike == current_strike {
+                        match stage_section {
+                            Some(StageSection::Buildup) => (anim_time.powf(0.25), 0.0, 0.0),
+                            Some(StageSection::Action) => (1.0, anim_time.powi(2), 0.0),
+                            Some(StageSection::Recover) => (1.0, 1.0, anim_time.powi(4)),
+                            _ => (0.0, 0.0, 0.0),
+                        }
+                    } else {
+                        (1.0, 1.0, 0.0)
+                    };
+
+                    if move2 > 0.0 {
+                        next.main_weapon_trail = true;
+                    } else {
+                        next.main_weapon_trail = false;
+                    }
+
+                    match strike {
+                        0 => {
+                            next.hand_l.position = Vec3::new(s_a.shl.0, s_a.shl.1, s_a.shl.2);
+                            next.hand_l.orientation = Quaternion::rotation_x(s_a.shl.3)
+                                * Quaternion::rotation_y(s_a.shl.4);
+                            next.hand_r.position = Vec3::new(
+                                -s_a.sc.0 + 6.0 + move1 * -12.0,
+                                -4.0 + move1 * 3.0,
+                                -2.0,
+                            );
+                            next.hand_r.orientation = Quaternion::rotation_x(0.9 + move1 * 0.5);
+                            next.control.position = Vec3::new(s_a.sc.0, s_a.sc.1, s_a.sc.2);
+                            next.control.orientation = Quaternion::rotation_x(s_a.sc.3);
+
+                            next.chest.orientation = Quaternion::rotation_z(move1 * -0.6);
+                            next.head.orientation = Quaternion::rotation_z(move1 * 0.3);
+                            next.shorts.orientation = Quaternion::rotation_z(move1 * 0.4);
+                            next.belt.orientation = Quaternion::rotation_z(move1 * 0.2);
+                            next.control.orientation.rotate_x(move1 * -1.05);
+                            next.control.orientation.rotate_z(move1 * 0.6);
+                            next.control.position += Vec3::new(move1 * 12.0, move1 * -2.0, 0.0);
+
+                            next.chest.orientation.rotate_z(move2 * 0.6);
+                            next.head.orientation.rotate_z(move2 * -0.2);
+                            next.shorts.orientation.rotate_z(move2 * -0.4);
+                            next.belt.orientation.rotate_z(move2 * -0.2);
+                            next.control.orientation.rotate_z(move2 * -0.6);
+                            next.control.position +=
+                                Vec3::new(move2 * -2.0, move2 * 12.0, move2 * 5.0);
+                        },
+                        1 => {
+                            next.chest.orientation.rotate_z(move1 * 0.5);
+                            next.head.orientation.rotate_z(move1 * -0.3);
+                            next.shorts.orientation.rotate_z(move1 * -0.3);
+                            next.belt.orientation.rotate_z(move1 * -0.1);
+                            next.control.orientation.rotate_z(move1 * -0.5);
+                            next.control.position +=
+                                Vec3::new(move1 * -7.0, move1 * -12.0, move1 * -4.0);
+
+                            next.chest.orientation.rotate_z(move2 * -1.0);
+                            next.head.orientation.rotate_z(move2 * 0.4);
+                            next.shorts.orientation.rotate_z(move2 * 0.6);
+                            next.belt.orientation.rotate_z(move2 * 0.2);
+                            next.control.orientation.rotate_z(move2 * 1.0);
+                            next.control.position += Vec3::new(0.0, move2 * 11.0, move2 * 3.0);
+                        },
+                        _ => {},
+                    }
+                },
                 _ => {},
             }
         }
