ae40257279b9c53192bbc7c8354810fb0a2f43f4
flo666 <sunflowerstarfishmail@gmail.com>
2022-10-21 09:11:59 +0200

    - make tricksters spawn in groups and play instruments - remove defunct draugr flee

--- common/src/comp/agent.rs
+++ common/src/comp/agent.rs
@@ -228,8 +228,7 @@ impl<'a> From<&'a Body> for Psyche {
                     humanoid::Species::Elf => 0.4,
                     humanoid::Species::Human => 0.4,
                     humanoid::Species::Orc => 0.3,
-                    // set back to 0.3 after halloween event
-                    humanoid::Species::Draugr => 1.0,
+                    humanoid::Species::Draugr => 0.3,
                 },
                 Body::QuadrupedSmall(quadruped_small) => match quadruped_small.species {
                     quadruped_small::Species::Pig => 0.5,
