151f4bb7081f7feac803428146970865709bca59
flo <sunflowerstarfishmail@gmail.com>
2022-11-13 19:14:26 +0000

    add merchants to savannah pits

--- server/src/rtsim/mod.rs
+++ server/src/rtsim/mod.rs
@@ -299,6 +299,35 @@ pub fn init(
                         });
                     }
                 },
+                SiteKind::SavannahPit(site2) => {
+                    for _ in 0..4 {
+                        rtsim.entities.insert(Entity {
+                            is_loaded: false,
+                            pos: site2
+                                .plots()
+                                .filter(|plot| {
+                                    matches!(plot.kind(), world::site2::PlotKind::SavannahPit(_))
+                                })
+                                .choose(&mut thread_rng())
+                                .map_or(site.get_origin(), |plot| {
+                                    site2.tile_center_wpos(
+                                        plot.root_tile()
+                                            + Vec2::new(
+                                                thread_rng().gen_range(-5..5),
+                                                thread_rng().gen_range(-5..5),
+                                            ),
+                                    )
+                                })
+                                .with_z(0)
+                                .map(|e| e as f32),
+                            seed: thread_rng().gen(),
+                            controller: RtSimController::default(),
+                            last_time_ticked: 0.0,
+                            kind: RtSimEntityKind::Merchant,
+                            brain: Brain::merchant(site_id, &mut thread_rng()),
+                        });
+                    }
+                },
                 SiteKind::DesertCity(site2) => {
                     // villagers
                     for _ in 0..(site2.plazas().len() as f32 * 1.5) as usize {
