fba14cefdab2a8cc1a9ac7314fe8af6a0bb08629
Sam <samuelkeiffer@gmail.com>
2023-01-05 21:19:20 -0500

    Temp

--- .gitlab/scripts/env.sh
+++ .gitlab/scripts/env.sh
@@ -4,6 +4,6 @@ export DISABLE_GIT_LFS_CHECK=true
 export VELOREN_ASSETS="assets"
 
 # When updating RUSTFLAGS here, windows-x86_64.sh must also be updated as it sets them independently
-export RUSTFLAGS="-D warnings"
+# export RUSTFLAGS="-D warnings"
 
 export SHADERC_LIB_DIR=/shaderc/combined/
