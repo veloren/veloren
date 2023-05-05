73a247753bbdf321e86339c5c40c845bd636bd39
Marcel Märtens <marcel.cochem@googlemail.com>
2023-05-05 09:53:49 +0200

    we are ignoreing those errors anyway, i think chrono is not using that functionality of time any more and the other 2 dependencies are very hard to get ever rid of in our codebase :/

--- .gitlab/scripts/security.sh
+++ .gitlab/scripts/security.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-time cargo audit
+# RUSTSEC-2020-0071: segfault in time crate which is used by chrono
+# RUSTSEC-2021-0119: out-of-bounds write in nix::unistd::getgrouplist in a old nix version (0.18 and 0.20) that are needed by old winit,
+time cargo audit --ignore RUSTSEC-2020-0071 --ignore RUSTSEC-2021-0119
