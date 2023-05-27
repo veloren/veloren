2dd529ad959fa65eba20fc822a8d786be2881116
Sam <samuelkeiffer@gmail.com>
2023-05-27 19:24:55 -0400

    Capsize

--- voxygen/anim/src/character/finishermelee.rs
+++ voxygen/anim/src/character/finishermelee.rs
@@ -364,6 +364,44 @@ impl Animation for FinisherMeleeAnimation {
                 next.control.orientation.rotate_z(move2 * -4.0);
                 next.control.position += Vec3::new(move2 * 12.0, move2 * -6.0, 0.0);
             },
+            Some("common.abilities.axe.capsize") => {
+                let (move1, move2_raw, move3) = match stage_section {
+                    Some(StageSection::Buildup) => (anim_time, 0.0, 0.0),
+                    Some(StageSection::Action) => (1.0, anim_time, 0.0),
+                    Some(StageSection::Recover) => (1.0, 1.0, anim_time),
+                    _ => (0.0, 0.0, 0.0),
+                };
+                let pullback = 1.0 - move3;
+                let move1 = move1 * pullback;
+                let move2 = move2_raw * pullback;
+
+                next.hand_l.position = Vec3::new(s_a.ahl.0, s_a.ahl.1, s_a.ahl.2);
+                next.hand_l.orientation =
+                    Quaternion::rotation_x(s_a.ahl.3) * Quaternion::rotation_y(s_a.ahl.4);
+                next.hand_r.position = Vec3::new(s_a.ahr.0, s_a.ahr.1, s_a.ahr.2);
+                next.hand_r.orientation =
+                    Quaternion::rotation_x(s_a.ahr.3) * Quaternion::rotation_z(s_a.ahr.5);
+
+                next.control.position = Vec3::new(s_a.ac.0, s_a.ac.1, s_a.ac.2);
+                next.control.orientation = Quaternion::rotation_x(s_a.ac.3)
+                    * Quaternion::rotation_y(s_a.ac.4)
+                    * Quaternion::rotation_z(s_a.ac.5 + move1 * -PI * 0.75 + move2 * PI * 0.25);
+
+                next.chest.orientation.rotate_z(move1 * 1.8);
+                next.head.orientation.rotate_z(move1 * -0.6);
+                next.belt.orientation.rotate_z(move1 * -0.4);
+                next.shorts.orientation.rotate_z(move1 * -1.3);
+                next.control.orientation.rotate_x(move1 * -0.8);
+
+                next.chest.orientation.rotate_z(move2 * -3.8);
+                next.head.orientation.rotate_z(move2 * 1.2);
+                next.belt.orientation.rotate_z(move2 * 0.8);
+                next.shorts.orientation.rotate_z(move2 * 2.1);
+                next.control.orientation.rotate_x(move2 * 0.6);
+                next.control.orientation.rotate_z(move2 * -4.0);
+                next.control.position += Vec3::new(move2 * 12.0, move2 * -6.0, 0.0);
+                next.torso.orientation.rotate_z(move2_raw * -TAU);
+            },
             _ => {},
         }
 
