cdf5cfad386227f3037bc29c339ecac885c1a062
Maxicarlos08 <maxicarlos08@gmail.com>
2023-07-13 17:18:46 +0200

    Miner helmet migrations

---
+++ server/src/migrations/V52_miner_helmets.sql
@@ -0,0 +1,6 @@
+UPDATE item
+SET item_definition_id = 'common.items.armor.miner.helmet_blue' WHERE item_definition_id = 'common.items.armor.miner.helmet';
+UPDATE item
+SET item_definition_id = 'common.items.armor.miner.helmet_orange' WHERE item_definition_id = 'common.items.armor.miner.helmet';
+UPDATE item
+SET item_definition_id = 'common.items.armor.miner.helmet_red' WHERE item_definition_id = 'common.items.armor.miner.helmet';
