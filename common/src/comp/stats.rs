81312132453fa4e214c4338749e1a0908f4f066b
Sam <samuelkeiffer@gmail.com>
2023-07-28 22:24:07 -0400

    Added documentation

--- common/src/comp/stats.rs
+++ common/src/comp/stats.rs
@@ -65,11 +65,12 @@ pub struct Stats {
     pub attack_damage_modifier: f32,
     pub crit_chance_modifier: StatsModifier,
     pub swim_speed_modifier: f32,
-    // This adds effects to any attacks that the entity makes
+    /// This adds effects to any attacks that the entity makes
     pub effects_on_attack: Vec<AttackEffect>,
+    /// This is the fraction of damage reduction (from armor and other buffs) that gets ignored by attacks from this entity
     pub mitigations_penetration: f32,
     pub energy_reward_modifier: f32,
-    // This creates effects when the entity is damaged
+    /// This creates effects when the entity is damaged
     pub effects_on_damaged: Vec<DamagedEffect>,
 }
 
