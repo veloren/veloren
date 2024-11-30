69a7b386a8f4d455497aa5c63d64daf8848996a9
miocore <miocore@proton.me>
2024-11-30 18:01:47 +0300

    fix wallrun-equip cycle when trying to cast abilities

--- common/src/states/wielding.rs
+++ common/src/states/wielding.rs
@@ -25,9 +25,9 @@ impl CharacterBehavior for Data {
         handle_orientation(data, &mut update, 1.0, None);
         handle_move(data, &mut update, if self.is_sneaking { 0.4 } else { 1.0 });
         handle_climb(data, &mut update);
+        handle_wallrun(data, &mut update);
         attempt_input(data, output_events, &mut update);
         handle_jump(data, output_events, &mut update, 1.0);
-        handle_wallrun(data, &mut update);
 
         if self.is_sneaking
             && (data.physics.on_ground.is_none() || data.physics.in_liquid().is_some())
