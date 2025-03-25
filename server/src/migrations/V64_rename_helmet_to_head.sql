bf8af10c6039b25b78ebc2229fa6e334e8dd4d38
horblegorble <horblegorble@gmail.com>
2025-03-25 12:20:28 +1000

    headgear item migration

---
+++ server/src/migrations/V64_rename_helmet_to_head.sql
@@ -0,0 +1,4 @@
+UPDATE item
+SET item_definition_id = 'common.items.armor.ashen.head' WHERE item_definition_id = 'common.items.armor.ashen.helmet';
+UPDATE item
+SET item_definition_id = 'common.items.armor.golemite.head' WHERE item_definition_id = 'common.items.armor.golemite.helmet';
