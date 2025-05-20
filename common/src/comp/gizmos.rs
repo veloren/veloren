675f228d6b182798d0e1fd6e3634c3171d8a6746
isse <git@isse.rs>
2025-05-20 18:05:31 +0200

    add comments

--- common/src/comp/gizmos.rs
+++ common/src/comp/gizmos.rs
@@ -1,3 +1,5 @@
+//! Gizmos are used to visually debug server-side values.
+
 use enum_map::EnumMap;
 use serde::{Deserialize, Serialize};
 use specs::{Component, DenseVecStorage};
@@ -15,6 +17,7 @@ pub enum Shape {
 pub struct Gizmos {
     pub shape: Shape,
     pub color: Rgba<u8>,
+    /// If `None`, this ends when the next batch of gizmos are recieved.
     pub end_time: Option<Time>,
 }
 
@@ -46,6 +49,9 @@ impl Gizmos {
         }
     }
 
+    /// If specified, this shape will be displayed up to `time`.
+    ///
+    /// If not specified this will end the next time a batch of gizmos are sent.
     pub fn with_end_time(mut self, time: Time) -> Self {
         self.end_time = Some(time);
         self
@@ -72,11 +78,14 @@ pub enum GizmoContext {
     #[default]
     Disabled,
     Enabled,
+    /// If applicable, only draw debug information with this specific target.
     EnabledWithTarget(Uid),
 }
 
+/// A component to subscribe to certain visual debug information.
 pub struct GizmoSubscriber {
     pub gizmos: EnumMap<GizmoSubscription, GizmoContext>,
+    /// The range of the debug drawing, in world space.
     pub range: f32,
 }
 
