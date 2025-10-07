48c0825fde6e4d6b99245a6fa5159c54c54ecbab
Youser Nayme <7685106-NeuralModder@users.noreply.gitlab.com>
2025-10-07 10:13:39 +0000

    Allow building outside git repository
    If versioning information hasn't been provided manually and the git
    command to get the version fails, the buildscript prints a warning and
    builds the game with git hash and timestamp set to 0.
    I could've perhaps tried to go into all the places where the version
    information is used and added new checks and warning texts and such but
    that's a lot of work for something people shouldn't really be doing
    anyway. Setting them to 0 is fairly simple and obvious nonsense,
    communicating that something has gone awry; the biggest downside is that
    no warnings will be thrown if both client and server lack real version
    information, even when they may be incompatible. But whatever.
    
    Closes #1950

--- common/build.rs
+++ common/build.rs
@@ -1,45 +1,80 @@
 use std::process::Command;
 
+// Get the current githash+date
+// Note: It will compare commits. As long as the commits do not diverge from the
+// server no version change will be detected.
+fn get_git_version() -> Result<String, String> {
+    let output = Command::new("git")
+        .args(["log", "-n", "1", "--pretty=format:%h/%ct", "--abbrev=8"])
+        .output()
+        .map_err(|e| format!("Git version command couldn't be run with error: {}", e))?;
+
+    if !output.status.success() {
+        return Err(format!(
+            "Git version command was unsuccessful: {}",
+            String::from_utf8_lossy(&output.stderr)
+        ));
+    }
+
+    let version = String::from_utf8(output.stdout)
+        .map_err(|e| format!("Git version command output isn't valid UTF-8: {}", e))?;
+    let hash = version
+        .split('/')
+        .next()
+        .ok_or("Git hash not found".to_string())?;
+    // The non-collision guarantee isn't all that important for our purposes
+    if hash.len() != 8 {
+        Ok(format!(
+            "{}/{}",
+            hash.get(..8)
+                .ok_or("Git hash not long enough".to_string())?,
+            version
+                .split('/')
+                .nth(1)
+                .ok_or("Git timestamp not found".to_string())?
+        ))
+    } else {
+        Ok(version)
+    }
+}
+
+// Get the current gittag
+fn get_git_tag() -> Option<String> {
+    let output = Command::new("git")
+        .args(["describe", "--exact-match", "--tags", "HEAD"])
+        .output()
+        .ok()?;
+
+    if !output.status.success() {
+        return None;
+    }
+
+    String::from_utf8(output.stdout).ok()
+}
+
 fn main() {
     // If this env var exists, it'll be used instead
     if option_env!("VELOREN_GIT_VERSION").is_none() {
-        // Get the current githash+date
-        // Note: It will compare commits. As long as the commits do not diverge from the
-        // server no version change will be detected.
-        let version = match Command::new("git")
-            .args(["log", "-n", "1", "--pretty=format:%h/%ct", "--abbrev=8"])
-            .output()
-        {
-            Ok(output) => match String::from_utf8(output.stdout) {
-                Ok(version) => {
-                    let hash = version.split('/').next().expect("no git hash");
-                    // The non-collision guarantee isn't all that important for our purposes
-                    if hash.len() != 8 {
-                        format!(
-                            "{}/{}",
-                            hash.get(..8).expect("invalid git hash"),
-                            version.split('/').nth(1).expect("no git timestamp")
-                        )
-                    } else {
-                        version
-                    }
-                },
-                Err(e) => panic!("failed to convert git output to UTF-8: {}", e),
+        let version = match get_git_version() {
+            Ok(version) => version,
+            Err(e) => {
+                println!("cargo::warning={}", e);
+                println!(
+                    "cargo::warning=Veloren will be compiled with git hash and timestamp set to \
+                     0, which will cause version mismatch warnings where applicable, whether the \
+                     version is actually mismatched or not. It is highly recommended to build the \
+                     game from the cloned git repository with the git command available in order \
+                     to give Veloren access to proper versioning information."
+                );
+                println!(
+                    "cargo::rustc-env=VELOREN_GIT_VERSION={}/0/0",
+                    env!("CARGO_PKG_VERSION")
+                );
+                return;
             },
-            Err(e) => panic!("failed to retrieve current git commit hash and date: {}", e),
         };
 
-        // Get the current gittag
-        let tag = match Command::new("git")
-            .args(["describe", "--exact-match", "--tags", "HEAD"])
-            .output()
-        {
-            Ok(output) => match String::from_utf8(output.stdout) {
-                Ok(tag) => tag,
-                Err(e) => panic!("failed to convert git output to UTF-8: {}", e),
-            },
-            Err(e) => panic!("failed to retrieve current git tag: {}", e),
-        };
+        let tag = get_git_tag().unwrap_or("".to_string());
 
         println!("cargo::rustc-env=VELOREN_GIT_VERSION={}/{}", &tag, &version);
     }
