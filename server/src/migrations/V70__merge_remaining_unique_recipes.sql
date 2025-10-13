4d0030d35a1161835c3a1c037c5001dc2bd64f40
Uniior <uniior73141@protonmail.com>
2025-10-13 20:21:07 +0000

    Assorted small tweaks/fixes

---
+++ server/src/migrations/V70__merge_remaining_unique_recipes.sql
@@ -0,0 +1,6 @@
+UPDATE item
+    SET item_definition_id = 'common.items.recipes.equipment.moderate'
+    WHERE item_definition_id = 'common.items.recipes.unique.winged_coronet';
+UPDATE item
+    SET item_definition_id = 'common.items.recipes.equipment.advanced'
+    WHERE item_definition_id = 'common.items.recipes.unique.mindflayer_spellbag';
