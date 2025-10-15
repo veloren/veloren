eaf334ad39208d5cf5cf02c32e4429b3573d1e31
Marcel Märtens <marcel.cochem@googlemail.com>
2025-10-15 11:10:35 +0200

    compress the debuginfo in pipeline generated builds.  - This reduces the size for installations on disk (and probably load times)  - The distribution times are probably not improved alot, because we zip anyways.  - we get a 50% reduction in size for binary.  - Note: llvm-dwarfutil crashes with llvm-20 for voxygen, its fixed in llvm-21 though, but the result is a larger binary :/  - This follows: https://kobzol.github.io/rust/2025/09/22/reducing-binary-size-of-rust-programs-with-debuginfo.html / https://www.reddit.com/r/rust/comments/1nnm6di/reducing_binary_size_of_rust_programs_with/

--- .gitlab/scripts/linux-aarch64.sh
+++ .gitlab/scripts/linux-aarch64.sh
@@ -3,3 +3,9 @@ export VELOREN_USERDATA_STRATEGY=executable;
 export PKG_CONFIG="/usr/bin/aarch64-linux-gnu-pkg-config";
 time cargo build --target=aarch64-unknown-linux-gnu --release --no-default-features --features default-publish;
 # evaluate --bin instead, last time i checked (2021-07-14) it was 2 minutes slower on release (but faster on debug)
+
+# compress debuginfos via zlib, which reduces the size by 50% while still beeing supported by most tools
+objcopy --compress-debug-sections=zlib target/release/veloren-server-cli target/release/veloren-server-cli-compressed
+objcopy --compress-debug-sections=zlib target/release/veloren-voxygen target/release/veloren-voxygen-compressed
+mv target/release/veloren-server-cli-compressed target/release/veloren-server-cli
+mv target/release/veloren-voxygen-compressed target/release/veloren-voxygen
--- .gitlab/scripts/windows-x86_64.sh
+++ .gitlab/scripts/windows-x86_64.sh
@@ -9,3 +9,9 @@ export VELOREN_USERDATA_STRATEGY=executable;
 export RUSTFLAGS="-D warnings -C link-arg=-lpsapi";
 
 time cargo build --target=x86_64-pc-windows-gnu --release --no-default-features --features "default-publish";
+
+# compress debuginfos via zlib, which reduces the size by 50% while still beeing supported by most tools
+objcopy --compress-debug-sections=zlib target/release/veloren-server-cli target/release/veloren-server-cli-compressed
+objcopy --compress-debug-sections=zlib target/release/veloren-voxygen target/release/veloren-voxygen-compressed
+mv target/release/veloren-server-cli-compressed target/release/veloren-server-cli
+mv target/release/veloren-voxygen-compressed target/release/veloren-voxygen
