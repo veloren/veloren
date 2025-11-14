047974b1a0efe5583e85e765aa54262313a65ce5
Youser Nayme <7685106-NeuralModder@users.noreply.gitlab.com>
2025-11-14 17:54:39 +0000

    Fix debuginfo compression for linux-aarch64, disable for MacOS and Windows
    Objcopy doesn't seem to support debuginfo compression in PE executables,
    and it doesn't even exist on MacOS. Who knows if it's even possible for
    these formats.

--- .gitlab/scripts/linux-aarch64.sh
+++ .gitlab/scripts/linux-aarch64.sh
@@ -5,7 +5,7 @@ time cargo build --target=aarch64-unknown-linux-gnu --release --no-default-featu
 # evaluate --bin instead, last time i checked (2021-07-14) it was 2 minutes slower on release (but faster on debug)
 
 # compress debuginfos via zlib, which reduces the size by 50% while still beeing supported by most tools
-objcopy --compress-debug-sections=zlib target/release/veloren-server-cli target/release/veloren-server-cli-compressed
-objcopy --compress-debug-sections=zlib target/release/veloren-voxygen target/release/veloren-voxygen-compressed
-mv target/release/veloren-server-cli-compressed target/release/veloren-server-cli
-mv target/release/veloren-voxygen-compressed target/release/veloren-voxygen
+aarch64-linux-gnu-objcopy --compress-debug-sections=zlib target/aarch64-unknown-linux-gnu/release/veloren-server-cli target/aarch64-unknown-linux-gnu/release/veloren-server-cli-compressed
+aarch64-linux-gnu-objcopy --compress-debug-sections=zlib target/aarch64-unknown-linux-gnu/release/veloren-voxygen target/aarch64-unknown-linux-gnu/release/veloren-voxygen-compressed
+mv target/aarch64-unknown-linux-gnu/release/veloren-server-cli-compressed target/aarch64-unknown-linux-gnu/release/veloren-server-cli
+mv target/aarch64-unknown-linux-gnu/release/veloren-voxygen-compressed target/aarch64-unknown-linux-gnu/release/veloren-voxygen
