76c6df48410078dc0ff7a461a78198085876654b
juliancoffee <lightdarkdaughter@gmail.com>
2026-06-22 17:04:25 +0300

    Remove asset_tweak

--- .gitlab/scripts/code-quality.sh
+++ .gitlab/scripts/code-quality.sh
@@ -5,7 +5,7 @@
 time cargo clippy \
     --all-targets \
     --locked \
-    --features="bin_cmd_doc_gen,bin_compression,bin_csv,bin_graphviz,bin_bot,bin_asset_migrate,asset_tweak,bin,stat,cli" \
+    --features="bin_cmd_doc_gen,bin_compression,bin_csv,bin_graphviz,bin_bot,bin_asset_migrate,bin,stat,cli" \
     -- -D warnings &&
 
 # Ensure that the veloren-voxygen default-publish feature builds as it excludes some default features.
